de2922da90dff73f26f2b4a43eff30c1ad92c202
Stijn <stijn.peeters@uva.nl>
2018-09-26 12:04:21 +0200

    Refactor scraper into a worker pool manager
    - Put python files containing worker classes in workers/: if they contain classes descending
      from BasicWorker they will be added to the pool while the manager is running
    - Make keyboard input/control actually work: press q+enter to quit the manager.

--- scraper/lib/scraper.py
+++ scraper/lib/scraper.py
@@ -1,70 +1,23 @@
-import threading
 import requests
 import time
 import json
+import abc
 
-from lib.queue import JobQueue, JobClaimedException
-from lib.logger import Logger
+from lib.worker import BasicWorker
 
 
-class BasicJSONScraper:
+class BasicJSONScraper(BasicWorker, metaclass=abc.ABCMeta):
     """
     Abstract JSON scraper class
 
-    This starts a separate thread in which the job queue is continually checked for
-    jobs of this scraper's type. If any are found, the URL for that job is scraped
-    and the result is parsed as JSON. The parsed JSON is then passed to a processor
-    function for further handling.
+    The job queue is continually checked for jobs of this scraper's type. If any are found,
+    the URL for that job is scraped and the result is parsed as JSON. The parsed JSON is
+    then passed to a processor method for further handling.
     """
-    type = "misc"  # this should match the job type as saved in the database
-    jobdata = {}
-    pause = 1  # time to wait between scrapes
 
-    queue = None
-    log = None
-    looping = True
-    thread = None
-
-    def __init__(self):
+    def work(self):
         """
-        Set up queue and log handlers, and start a thread
-        """
-        super().__init__()
-        self.queue = JobQueue()
-        self.log = Logger()
-
-        self.thread = threading.Thread(target=self.loop, name=self.type)
-        self.thread.start()
-
-    def isAlive(self):
-        """
-        Check if scraper thread is still alive
-
-        If not, it has ended - this *should* happen only when `abort()` is called.
-        :return bool:
-        """
-        return self.thread.isAlive()
-
-    def abort(self):
-        """
-        Stop the scraping loop, and end the thread.
-        """
-        self.looping = False
-        self.thread.join()
-
-    def loop(self):
-        """
-        Loop the scraper
-
-        This simply scrapes continually, with a pause in-between scrapes.
-        """
-        while self.looping:
-            self.scrape()
-            time.sleep(self.pause)
-
-    def scrape(self):
-        """
-        Scrape an URL
+        Scrape a URL
 
         This acquires a job - if none are found, the loop pauses for a while. The job's URL
         is then requested and parsed. If that went well, the parsed data is passed on to the
@@ -72,11 +25,11 @@ class BasicJSONScraper:
         """
         job = self.queue.getJob(self.type)
         if not job:
-            time.sleep(10)
             self.log.info("Scraper (%s) has no jobs, sleeping for 10 seconds" % self.type)
+            time.sleep(10)
             return
 
-        # claim the job - this is needed so multiple scrapers don't do the same job
+        # claim the job - this is needed so multiple workers don't do the same job
         self.jobdata = job
         self.queue.claimJob(job["id"])
 
@@ -103,8 +56,21 @@ class BasicJSONScraper:
         # finally, pass it on
         self.process(jsondata)
 
+    @abc.abstractmethod
     def process(self, data):
-        raise NotImplementedError("A child class needs to implement this method.")
+        """
+        Process scraped data
 
+        :param data:  Parsed JSON data
+        """
+        pass
+
+    @abc.abstractmethod
     def getUrl(self, data):
-        raise NotImplementedError("A child class needs to implement this method.")
+        """
+        Get URL to scrape
+
+        :param data:  Job data
+        :return:
+        """
+        pass
