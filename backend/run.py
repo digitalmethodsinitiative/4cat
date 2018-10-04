2ea6b2d28b100795528f8dc6fc128aaead7172fe
Stijn <stijn.peeters@uva.nl>
2018-10-04 13:42:38 +0200

    Limit direct log output to actually useful info

--- backend/run.py
+++ backend/run.py
@@ -6,6 +6,24 @@ from lib.database import Database
 from lib.manager import WorkerManager
 from lib.logger import Logger
 
+print("""
++---------------------------------------------------------------+
+|                                                               |
+|                           welcome to                          |
+|                                                               |
+|                  j88D   .o88b.  .d8b.  d888888b               |
+|                 j8~88  d8P  Y8 d8' `8b `~~88~~'               |
+|                j8' 88  8P      88ooo88    88                  |
+|                V88888D 8b      88~~~88    88                  |
+|                    88  Y8b  d8 88   88    88                  |
+|                    VP   `Y88P' YP   YP    YP                  |
+|                                                               |
+|               4CAT: Capture and Analysis Toolkit              |
+|                       www.oilab.eu/4cat                       |
+|                                                               |
++---------------------------------------------------------------+    
+""")
+
 # init
 looping = True
 scraper_threads = []
