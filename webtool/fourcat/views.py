cb641578bca32ee445a810e898721d9a999dd40e
Stijn <stijn.peeters@uva.nl>
2018-10-11 11:29:23 +0200

    Fix import path in webtool

--- webtool/fourcat/views.py
+++ webtool/fourcat/views.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__) +  '/../..')
+sys.path.insert(0, os.path.dirname(__file__) +  '/../../backend')
 import lib.queue
 import config
 import pandas as pd
