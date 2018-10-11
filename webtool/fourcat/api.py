d004cb326203df8de1996cd1c521947907c71a92
Stijn <stijn.peeters@uva.nl>
2018-10-11 13:38:51 +0200

    Add rate limiting to API
    Via flask-limiter

--- webtool/fourcat/api.py
+++ webtool/fourcat/api.py
@@ -2,19 +2,35 @@ import sys
 import os
 import psutil
 sys.path.insert(0, os.path.dirname(__file__) +  '/../../backend')
+
+from flask import jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+from fourcat import app
 from lib.database import Database
 from lib.logger import Logger
-from fourcat import app
-from flask import jsonify
 
 log = Logger()
 db = Database(logger=log)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address
+)
 
 API_SUCCESS = 200
 API_FAIL = 404
 
 @app.route('/api/')
+@limiter.limit("30 per minute")
 def api_main():
+    """
+    API Index
+
+    No data here - just a reference to the documentation
+
+    :return: Flask JSON response
+    """
     response = {
         "code": API_SUCCESS,
         "items": [
@@ -25,12 +41,22 @@ def api_main():
     return jsonify(response)
 
 @app.route('/api/status/')
+@limiter.limit("1 per second")
 def api_status():
+    """
+    Get service status
+
+    :return: Flask JSON response
+    """
+
+    # get job stats
     jobs = db.fetchall("SELECT * FROM jobs")
     jobs_count = len(jobs)
     jobs_types = set([job["jobtype"] for job in jobs])
     jobs_sorted = {jobtype: len([job for job in jobs if job["jobtype"] == jobtype]) for jobtype in jobs_types}
+    jobs_sorted["total"] = jobs_count
 
+    # determine if backend is live by checking if the process is running
     lockfile = "../../backend/4cat-backend.lock"
     if os.path.isfile("../../backend/4cat-backend.lock"):
         with open(lockfile) as pidfile:
@@ -44,8 +70,7 @@ def api_status():
         "items": {
             "backend": {
                 "live": backend_live,
-                "queued": jobs_count,
-                "queued_sorted": jobs_sorted
+                "queued": jobs_sorted
             },
             "frontend": {
                 "live": True  # duh
