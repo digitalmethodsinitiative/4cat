abee38d2658e20c38adb78a7b02efd9808631da1
Stijn <stijn.peeters@uva.nl>
2018-10-17 09:03:09 +0200

    Refactor tests
    Put setup/teardown methods in base class

--- test/test_database.py
+++ test/test_database.py
@@ -2,18 +2,10 @@ import unittest
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + '/..')
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + '/../backend')
-import config
-from lib.logger import Logger
-from lib.database import Database
+from basic_testcase import FourcatTestCase
 
 
-class TestJobQueue(unittest.TestCase):
-	db = None
-	log = None
-	root = ""
-
+class TestJobQueue(FourcatTestCase):
 	example_record = {
 		"jobtype": "test",
 		"remote_id": "1234",
@@ -22,37 +14,6 @@ class TestJobQueue(unittest.TestCase):
 		"claim_after": 0,
 		"attempts": 0
 	}
-
-	@classmethod
-	def setUpClass(cls):
-		"""
-		Set up database connection for test database before starting tests
-		"""
-		cls.root = os.path.abspath(os.path.dirname(__file__))
-		cls.log = Logger(output=False)
-		cls.db = Database(logger=cls.log, dbname=config.DB_NAME_TEST)
-		cls.db.rollback()
-		with open(cls.root + "/../backend/database.sql") as dbfile:
-			cls.db.execute(dbfile.read())
-
-	@classmethod
-	def tearDownClass(cls):
-		"""
-		Close database connection after tests are finished
-		"""
-		cls.db.close()
-		del cls.db
-
-	def setUp(self):
-		self.db.commit()
-
-	def tearDown(self):
-		"""
-		Reset database after each test
-		"""
-		self.db.rollback()
-		with open(self.root + "/reset_database.sql") as dbfile:
-			self.db.execute(dbfile.read())
 
 	@unittest.expectedFailure
 	def test_insert_failure(self):
