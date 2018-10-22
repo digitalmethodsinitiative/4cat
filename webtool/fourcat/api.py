898140de32bd8eb2a1769aabcc803e3b9c15b4d6
Stijn <stijn.peeters@uva.nl>
2018-10-22 16:41:44 +0200

    Add API endpoint to emulate 4chan thread API
    - Callable via /api/[board]/thread/[thread].json

--- webtool/fourcat/api.py
+++ webtool/fourcat/api.py
@@ -1,8 +1,11 @@
-import sys
-import os
+import datetime
 import psutil
+import json
+import os
 
-from flask import jsonify
+from collections import OrderedDict
+
+from flask import jsonify, abort
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
@@ -82,5 +85,94 @@ def api_status():
 			}
 		}
 	}
+
+	return jsonify(response)
+
+
+@app.route('/api/<board>/thread/<thread_id>.json')
+@api_ratelimit
+def api_thread(board, thread_id):
+	"""
+	Emulate 4chan thread.json API endpoint
+
+	:param str board:  Board name
+	:param int thread_id:  Thread ID
+	:return: JSONified thread data
+	"""
+	db = Database(logger=log)
+	thread = db.fetchone("SELECT * FROM threads WHERE board = %s AND id = %s", (board, thread_id))
+	if not thread:
+		abort(404)
+
+	posts = db.fetchall("SELECT * FROM posts WHERE thread_id = %s ORDER BY timestamp ASC", (thread_id,))
+	response = {"posts": []}
+
+	first_post = True
+	for post in posts:
+
+		# add data that is present for every single post
+		response_post = OrderedDict({
+			"resto": 0 if first_post else int(thread_id),
+			"no": post["id"],
+			"time": post["timestamp"],
+			"now": datetime.datetime.fromtimestamp(post["timestamp"]).strftime("%m/%d/%y(%a)%H:%I")
+		})
+
+		# first post has some extra data as well that is never present for replies
+		if first_post:
+			if thread["is_sticky"]:
+				response_post["sticky"] = 1
+			if thread["is_closed"]:
+				response_post["closed"] = 1
+
+			response_post["imagelimit"] = 1 if thread["limit_image"] else 0
+			response_post["bumplimit"] = 1 if thread["limit_bump"] else 0
+
+			if thread["timestamp_archived"] > 0:
+				response_post["archived"] = 1
+				response_post["archived_on"] = thread["timestamp_archived"]
+
+			response_post["unique_ips"] = thread["num_unique_ips"]
+			response_post["replies"] = thread["num_replies"] - 1  # OP doesn't count as reply
+			response_post["images"] = thread["num_images"]
+
+		# there are a few fields that are only present if an image was attached
+		if post["image_file"]:
+			response_post["tim"] = int(post["image_4chan"].split(".")[0])
+			response_post["ext"] = "." + post["image_4chan"].split(".").pop()
+			response_post["md5"] = post["image_md5"]
+			response_post["filename"] = ".".join(post["image_file"].split(".")[:-1])
+			response_post["fsize"] = post["image_filesize"]
+
+			dimensions = json.loads(post["image_dimensions"])
+			response_post["w"] = dimensions["w"]
+			response_post["h"] = dimensions["h"]
+			response_post["tn_w"] = dimensions["tw"]
+			response_post["tn_h"] = dimensions["th"]
+
+		# for the rest, just add it if not empty
+		if post["subject"]:
+			response_post["sub"] = post["subject"]
+
+		if post["body"]:
+			response_post["com"] = post["body"]
+
+		if post["author"]:
+			response_post["name"] = post["author"]
+
+		if post["author_trip"]:
+			response_post["trip"] = post["author_trip"]
+
+		if post["author_type"]:
+			response_post["id"] = post["author_type"]
+
+		if post["author_type_id"]:
+			response_post["capcode"] = post["author_type_id"]
+
+		if post["semantic_url"]:
+			response_post["semantic_url"] = post["semantic_url"]
+
+		response["posts"].append(response_post)
+		first_post = False
 
 	return jsonify(response)
