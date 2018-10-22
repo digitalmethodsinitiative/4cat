4ed9fc5e6a2831a4b52d97b114365af1574d02b2
Stijn <stijn.peeters@uva.nl>
2018-10-22 11:59:13 +0200

    Add support for running Flask app via WSGI

--- webtool/fourcat/__init__.py
+++ webtool/fourcat/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+import config
 import fourcat.views
 import fourcat.api
 
-app.config.from_object('config')
+app.config.from_object("config.FlaskConfig")
 
 if __name__ == "__main__":
 	print('Starting server...')
