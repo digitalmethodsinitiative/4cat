7b87e7eb3b0bf09863e0e2e1a5a5c7bf35a81f31
LAPTOP\hagen <hagen.sal@gmail.com>
2018-10-25 19:56:25 +0200

    Integrated query status table with frontend

--- webtool/fourcat/views.py
+++ webtool/fourcat/views.py
@@ -6,10 +6,11 @@ import pickle as p
 from flask import Flask, render_template, url_for
 from fourcat import app
 
+from backend.lib.query import SearchQuery
 from backend.lib.helpers import get_absolute_folder
 from backend.lib.database import Database
 from backend.lib.logger import Logger
-from backend.lib.queue import JobQueue
+from backend.lib.queue import JobQueue, JobAlreadyExistsException
 
 """
 
@@ -37,44 +38,67 @@ def string_query(searchquery, min_timestamp='none', max_timestamp='none'):
 	# for security
 	searchquery = re.escape(searchquery)
 
-	# make connections to database with backend library
+	# make connections to database with backend library - safe enough?
 	log = Logger()
 	db = Database(logger=log)
-	query_queue = JobQueue(logger=log, database=db)
-	
-	# add job with respective query string
-	query_queue.add_job("query", details={"str_query": searchquery, "col_query": "body_vector", "min_timestamp": min_timestamp, "max_timestamp": max_timestamp})
+	queue = JobQueue(log, db)
 
-	return 'success'
+	query = SearchQuery(query=searchquery, parameters={"str_query": searchquery, "col_query": "body_vector", "min_timestamp": min_timestamp, "max_timestamp": max_timestamp}, db=db)
+	try:
+		queue.add_job(jobtype="query", remote_id=query.key)
+	except JobAlreadyExistsException:
+		pass
 
-@app.route('/check_query/<searchquery>', methods=['GET','POST'])
-def check_query(searchquery):
+	print("Query queued: %s" % query.key)
+	return query.key
+
+@app.route('/check_query/<query_key>', methods=['GET','POST'])
+def check_query(query_key):
 	"""
 	AJAX URI to check whether query has been completed.
 	"""
 
 	# load dict with metadata and statuses of already processed queries
-	path_file_status = get_absolute_folder(config.PATH_DATA + '/queries/di_queries.p')
-
-	if os.path.isfile(path_file_status):
-		di_queries = p.load(open(path_file_status, 'rb'))
-		print(di_queries)
-		if searchquery in di_queries:
-			file_status = di_queries[searchquery]
-		else:
-			file_status = 'processing'
-	else: 
-		file_status = 'processing'
 	
-	# check status of query
-	if file_status == 'finished':
-		# returns string with path to the csv
-		csv_path = config.PATH_DATA + '/mentions_' + searchquery + '.csv'
-		return csv_path
+	log = Logger()
+	db = Database(logger=log)
+	query = SearchQuery(key=query_key, db=db)
 
-	elif file_status == 'empty_file':
-		# if the query has already been executed, but no results were shown, return empty file notification
-		return 'empty_file'
 
+	results = query.get_finished_results_path()
+
+	if results:
+
+		# relative file path for debugging
+		if app.debug == True:
+			results = 'http://localhost/fourcat/' + config.PATH_DATA + results.split(config.PATH_DATA,1)[1]
+
+		return(results)
 	else:
-		return 'nofile'
+		return("nofile")
+
+	if 1 == 2:
+		path_file_status = get_absolute_folder(config.PATH_DATA + '/queries/di_queries.p')
+
+		if os.path.isfile(path_file_status):
+			di_queries = p.load(open(path_file_status, 'rb'))
+			
+			if searchquery in di_queries:
+				file_status = di_queries[searchquery]
+			else:
+				file_status = 'processing'
+		else: 
+			file_status = 'processing'
+		
+		# check status of query
+		if file_status == 'finished':
+			# returns string with path to the csv
+			csv_path = config.PATH_DATA + '/mentions_' + searchquery + '.csv'
+			return csv_path
+
+		elif file_status == 'empty_file':
+			# if the query has already been executed, but no results were shown, return empty file notification
+			return 'empty_file'
+
+		else:
+			return 'nofile'
