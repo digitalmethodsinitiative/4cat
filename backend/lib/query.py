c181930d6bb0035b8afcb772e3b2ccb90a328efd
Stijn <stijn.peeters@uva.nl>
2018-10-25 16:50:03 +0200

    Add SearchQuery class, tests and database table

---
+++ backend/lib/query.py
@@ -0,0 +1,138 @@
+import hashlib
+import json
+import time
+import re
+import os
+
+import config
+from backend.lib.helpers import get_absolute_folder
+
+
+class SearchQuery:
+	"""
+	Provide interface to safely register and run search queries
+	"""
+	db = None
+	data = None
+	folder = None
+
+	def __init__(self, query=None, parameters=None, key=None, db=None):
+		"""
+		Create new query object
+
+		If the query is not in the database yet, it is added.
+
+		:param str query:  Search query
+		:param parameters:  Parameters, e.g. date limits, et cetera
+		:param db:  Database connection
+		"""
+		self.db = db
+		self.folder = get_absolute_folder(config.PATH_DATA)
+
+		if key is not None:
+			self.key = key
+			current = self.db.fetchone("SELECT * FROM queries WHERE key = %s", (self.key,))
+			if not current:
+				raise TypeError("SearchQuery() requires a valid query key for its 'key' argument")
+		else:
+			if query is None or parameters is None:
+				raise TypeError("SearchQuery() requires either 'key', or 'parameters' and 'query' to be given")
+
+			self.key = self.get_key(query, parameters)
+			current = self.db.fetchone("SELECT * FROM queries WHERE key = %s AND query = %s", (self.key, query))
+
+		if current:
+			self.data = current
+		else:
+			self.data = {
+				"key": self.key,
+				"query": query,
+				"parameters": json.dumps(parameters),
+				"result_file": "",
+				"timestamp": int(time.time()),
+				"is_finished": False
+			}
+			self.db.insert("queries", data=self.data)
+
+	def get_finished_results_path(self):
+		"""
+		Get path to results file
+
+		Only returns a path if the query is finished. In other words, if this
+		method returns a path, a file with the complete results for this query
+		will exist at that location.
+
+		:return: A path to the results file, or `None`
+		"""
+		if self.data["is_finished"] and self.data["result_file"] and os.path.isfile(
+				self.folder + "/" + self.data["result_file"]):
+			return self.folder + "/" + self.data["result_file"]
+		else:
+			return None
+
+	def get_results_path(self):
+		"""
+		Get path to results file
+
+		Always returns a path, that will at some point contain the query
+		results, but may not do so yet. Use this to get the location to write
+		generated results to.
+
+		:return str:  A path to the results file
+		"""
+		return self.folder + "/" + self.data["result_file"]
+
+	def finish(self):
+		"""
+		Declare the query finished
+		"""
+		if self.data["is_finished"]:
+			raise RuntimeError("Cannot finish a finished query again")
+
+		self.db.update("queries", where={"query": self.data["query"], "key": self.data["key"]},
+					   data={"is_finished": True})
+		self.data["is_finished"] = True
+
+	def reserve_result_file(self, extension="csv"):
+		"""
+		Generate a unique path to the results file for this query
+
+		This generates a file name for the result of this query, and makes sure
+		no file exists or will exist at that location other than the file we
+		expect (i.e. the results file for this particular query).
+
+		:param str extension: File extension, "csv" by default
+		:return bool:  Whether the file path was successfully reserved
+		"""
+		if self.data["is_finished"]:
+			raise RuntimeError("Cannot reserve results file for a finished query")
+
+		query_bit = self.data["query"].replace(" ", "_").lower()
+		query_bit = re.sub(r"[^a-z0-9]", "", query_bit)
+		file = query_bit + "-" + self.data["key"]
+
+		path = self.folder + "/" + file + "." + extension.lower()
+		index = 1
+		while os.path.isfile(path):
+			path = self.folder + "/" + file + "-" + str(index) + "." + extension.lower()
+			index += 1
+
+		file = path.split("/").pop()
+		updated = self.db.update("queries", where={"query": self.data["query"], "key": self.data["key"]},
+								 data={"result_file": file})
+		self.data["result_file"] = file
+		return updated > 0
+
+	def get_key(self, query, parameters):
+		"""
+		Generate a unique key for this query that can be used to identify it
+
+		The key is a hash of a combination of the query string and parameters.
+		You never need to call this, really: it's used internally.
+
+		:param str query:  Query string
+		:param parameters:  Query parameters
+		:return str:  Query key
+		"""
+		plain_key = repr(parameters) + query
+		return hashlib.md5(plain_key.encode("utf-8")).hexdigest()
---
+++ test/test_query.py
@@ -0,0 +1,150 @@
+import unittest
+import json
+import os
+
+from test.basic_testcase import FourcatTestCase
+from backend.lib.query import SearchQuery
+
+
+class TestQuery(FourcatTestCase):
+	"""
+	Test the SearchQuery class
+	"""
+	default_parameters = {"before": 1540477901, "after": 0}
+	default_key = "291391f1636c710e00f9a136e54d770a"
+
+	def test_create_key(self):
+		"""
+		Test creating a unique key for a query
+
+		Expected: the key is the expected MD5 Hash
+		"""
+		query = SearchQuery(query="obama", parameters=self.default_parameters, db=self.db)
+		self.assertEqual(query.key, self.default_key)
+
+	def test_create_query(self):
+		"""
+		Test creating a query
+
+		Expected: data is properly inserted into database
+		"""
+		SearchQuery(query="obama", parameters=self.default_parameters, db=self.db)
+
+		result = self.db.fetchone("SELECT * FROM queries")
+		self.assertIsNotNone(result)
+
+		expected = {
+			"key": self.default_key,
+			"query": "obama",
+			"parameters": json.dumps(self.default_parameters),
+			"is_finished": False
+		}
+
+		for key in expected:
+			with self.subTest(key=key):
+				self.assertEqual(expected[key], result[key])
+
+	def test_key_not_match_query(self):
+		"""
+		Test if hash changes if query changes
+
+		Expected: hash changes
+		"""
+		query = SearchQuery(query="trump", parameters=self.default_parameters, db=self.db)
+		self.assertNotEqual(query.key, self.default_key)
+
+	def test_key_not_match_parameters(self):
+		"""
+		Test if hash changes if parameters change
+
+		Expected: hash changes
+		"""
+		query = SearchQuery(query="trump", parameters={"president": True}, db=self.db)
+		self.assertNotEqual(query.key, self.default_key)
+
+	def test_reserve_file(self):
+		"""
+		Test reserving a query's result file
+
+		Expected:
+		"""
+		query = SearchQuery(query="obama", parameters=self.default_parameters, db=self.db)
+
+		self.assertEqual(query.reserve_result_file(), True)
+		self.assertNotEqual("", query.data["result_file"])
+
+		path = query.get_results_path()
+		self.assertFalse(os.path.isfile(path))
+		self.assertIsNone(query.get_finished_results_path())
+
+		touch = open(query.get_results_path(), "w")
+		touch.close()
+		query.finish()
+
+		self.assertIsNotNone(query.get_finished_results_path())
+		self.assertTrue(os.path.isfile(query.get_finished_results_path()))
+
+	def test_state(self):
+		"""
+		Test performing preparation methods on a finished query
+
+		Expected: RuntimeErrors are raised
+		"""
+		query = SearchQuery(query="obama", parameters=self.default_parameters, db=self.db)
+		query.finish()
+
+		with self.subTest("Reserving a finished query's result file"):
+			with self.assertRaises(RuntimeError):
+				query.reserve_result_file()
+
+		with self.subTest("Finishing a finished query"):
+			with self.assertRaises(RuntimeError):
+				query.finish()
+
+	def test_instantiate_key(self):
+		"""
+		Test instantiating a SearchQuery by key
+
+		Expected: the same record is used in both cases
+		"""
+		query = SearchQuery(query="obama", parameters=self.default_parameters, db=self.db)
+		other_query = SearchQuery(key=self.default_key, db=self.db)
+
+		self.assertEqual(other_query.data["query"], "obama")
+		self.assertEqual(query.key, other_query.key)
+
+	def test_instantiate_queryparam(self):
+		"""
+		Test instantiating a SearchQuery by query/param combination
+
+		Expected: the same record is used in both cases
+		"""
+		query = SearchQuery(query="obama", parameters=self.default_parameters, db=self.db)
+		other_query = SearchQuery(query="obama", parameters=self.default_parameters, db=self.db)
+
+		self.assertEqual(query.key, other_query.key)
+
+		amount = self.db.fetchone("SELECT COUNT(*) AS num FROM queries")["num"]
+		self.assertEqual(amount, 1)
+
+	def test_instantiate_key_fail(self):
+		"""
+		Test instantiating a search query with a non-existent key
+
+		Expected: a TypeError is raised
+		"""
+		with self.assertRaises(TypeError):
+			SearchQuery(key="fake-key", db=self.db)
+
+	def test_instantiate_queryparam_fail(self):
+		"""
+		Test instantiating a search query with a lack of parameters
+
+		Expected: a TypeError is raised
+		"""
+		with self.assertRaises(TypeError):
+			SearchQuery(query="", db=self.db)
+
+
+if __name__ == '__main__':
+	unittest.main()
