613b81a88ff561a93f3fb79ed7df619f6ca8d1ea
Stijn <stijn.peeters@uva.nl>
2018-11-06 17:58:06 +0100

    Don’t spam logs with log init message

--- backend/lib/logger.py
+++ backend/lib/logger.py
@@ -62,7 +62,7 @@ class Logger:
 		frame = sys._getframe(1)
 		location = frame.f_code.co_filename.split("/").pop() + ":" + str(frame.f_lineno)
 
-		self.info("Logging from %s to %s" % (location, self.log_path))
+		# self.info("Logging from %s to %s" % (location, self.log_path))
 
 		if db:
 			self.db = db
