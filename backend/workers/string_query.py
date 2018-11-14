0342e102eb66b24bfce463e4f04a0177c443012d
LAPTOP\hagen <hagen.sal@gmail.com>
2018-11-14 18:58:55 +0100

    Attempt at integrating Sphinx db and mysql queries without testing :D

--- backend/workers/string_query.py
+++ backend/workers/string_query.py
@@ -4,6 +4,7 @@ import csv
 import os
 import pickle as p
 import re
+import mysql
 
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -14,7 +15,6 @@ from backend.lib.query import SearchQuery
 from backend.lib.queue import JobClaimedException
 from backend.lib.helpers import get_absolute_folder
 from backend.lib.worker import BasicWorker
-
 
 class stringQuery(BasicWorker):
 	"""
@@ -27,15 +27,24 @@ class stringQuery(BasicWorker):
 	pause = 2
 	max_workers = 3
 
-	# Columns to return in csv.
+	# Columns to return in csv
 	# Mandatory columns: ['thread_id', 'body', 'subject', 'timestamp']
 	li_return_cols = ['thread_id', 'id', 'timestamp', 'body', 'subject', 'author', 'image_file', 'image_md5', 'country_name']
 
 	def __init__(self, logger, manager):
 		"""
 		Set up database connection - we need one to perform the query
+
 		"""
 		super().__init__(logger=logger, manager=manager)
+
+		# Connect to the Sphinx database
+		sphinx_db = mysql.connector.connect(
+			host = "localhost",
+			user = config.DB_USER,
+			passwd = config.DB_PASSWORD,
+			port = 9306
+		)
 
 	def work(self):
 
@@ -103,6 +112,9 @@ class stringQuery(BasicWorker):
 		
 		"""
 		
+		# Connect to the Sphinx database
+		sphinx_cursor =  self.sphinx_db.cursor()
+
 		board = query_parameters["board"]
 		body_query = query_parameters["body_query"]
 		subject_query = query_parameters["subject_query"]
@@ -123,46 +135,38 @@ class stringQuery(BasicWorker):
 		# Set SQL statements depending on job parameters
 		replacements = []
 		sql_board = ''
-		sql_body = ''
-		sql_subject = ''
+		sql_text = ''
 		sql_min_date = ''
 		sql_max_date = ''
 		sql_columns = ', '.join(self.li_return_cols)
 		sql_log = 'Starting substring query where '
-
-
-		# Mandatory that thread appears in threads.id for the respective board
-		board_threads = self.db.fetchall("SELECT id FROM threads WHERE board = '" + board + "'")
-		board_threads = list([thread["id"] for thread in board_threads])
-		board_threads = [str(thread) for thread in board_threads]
 		
-		board_threads = ','.join(board_threads)
-		#sql_board = ' AND thread_id IN (' + board_threads + ')'
 		sql_log = sql_log + "posts are on " + board + ", "
 		
-		# Generate SQL query string
-		if body_query != 'empty':
-			# Primary use if FTS search, so set tsvector matching first
-			sql_body = sql_body + " AND body_vector @@ plainto_tsquery('" + body_query + "')"
-			sql_log = sql_log + "'" + body_query + "' is in body, "
-			# If there are exact string matches between "quotation marks", loop through all entries and add to SQL query
-			if li_exact_body:
-				for exact_body in li_exact_body:
-					sql_body = sql_body + " AND lower(body) SIMILAR TO '%" + exact_body + "%'"
-					sql_log = sql_log + "body exactly matches '" + exact_body + "', "
-			replacements.append(sql_body)
-		
-		if subject_query != 'empty':
-			# Primary use if FTS search, so set tsvector matching first
-			# sql_subject = sql_subject + " AND subject_vector @@ plainto_tsquery('" + subject_query + "')"
-			sql_subject = sql_subject + " AND lower(subject) SIMILAR TO '%" + subject_query + "%'"
-			sql_log = sql_log + "'" + subject_query + "' is in subject, "
-			# If there are exact string matches between "quotation marks", loop through all entries and add to SQL query
-			# if li_exact_subject:
-			# 	for exact_subject in li_exact_subject:
-			# 		sql_subject = sql_subject + " AND lower(subject) SIMILAR TO '%" + exact_subject + "%'"
-			# 		sql_log = sql_log + "subject exactly matches '" + exact_subject + "', "
-			replacements.append(sql_body)
+		# Generate query string MATCH() with Sphinx
+		if body_query != "empty" or subject_query != "empty":
+			sql_text = " AND MATCH('"
+			if body_query != "empty":
+				# Sphinx search
+				sql_text = sql_text + " @body " + body_query
+				sql_log = sql_log + "'" + body_query + "' is in body, "
+				# If there are exact string matches between "quotation marks", loop through all entries and add
+				if li_exact_body:
+					for exact_body in li_exact_body:
+						sql_text = sql_text + " @body \"" + exact_body + "\""
+						sql_log = sql_log + "body exactly matches '" + exact_body + "', "
+				replacements.append(sql_text)
+			
+			if subject_query != 'empty':
+				sql_text = sql_text + " @subject " + subject_query
+				sql_log = sql_log + "'" + subject_query + "' is in subject, "
+				# If there are exact string matches between "quotation marks", loop through all entries and add
+				if li_exact_subject:
+					for exact_subject in li_exact_subject:
+						sql_text = sql_text + " @subject \"" + exact_subject + "\""
+						sql_log = sql_log + "subject exactly matches '" + exact_subject + "', "
+				replacements.append(sql_text)
+			sql_text = sql_text + "')"
 
 		if min_date != 0:
 			sql_min_date = " AND timestamp >= " + str(min_date)
@@ -182,38 +186,69 @@ class stringQuery(BasicWorker):
 
 			# Log SQL query
 			self.log.info(sql_log)
-			self.log.info("SELECT " + sql_columns + " FROM posts WHERE true AND thread_id IN (...)" + sql_body + sql_subject + sql_min_date + sql_max_date)
+			self.log.info('First fetching matching post ids')
+			self.log.info("SELECT post_id FROM 4cat_posts WHERE true AND " + sql_text + sql_min_date + sql_max_date)
 
+			# Get the post ids for all posts that match the sphinx query
 			try:
-				li_matches = self.db.fetchall("SELECT " + sql_columns + " FROM posts WHERE true" + sql_board + sql_body + sql_subject + sql_min_date + sql_max_date)
+				li_matches = sphinx_cursor.fetchall("SELECT post_id FROM 4cat_posts WHERE true" + sql_board + sql_text + sql_min_date + sql_max_date)
 			except Exception as error:
 				return str(error)
+
+			# Convert matching ids to tuple
+			li_ids = tuple([post["post_id"] for post in li_matches])
+
+			# If there are no posts matching the string query, return an empty list
+			if len(li_ids) == 0:
+				return li_ids
+
+			# Now fetch the real posts with the ids
+			self.log.info("Fetching posts that match the query")
+			try:
+				li_matches = self.db.fetchall("SELECT " + sql_columns + " FROM posts WHERE id IN %s ORDER BY thread_id, timestamp", (li_ids,))
+			except Exception as error:
+				return str(error)
+
+			return li_matches
 
 		# Fetch full thread data
 		elif dense_threads != False or (full_thread and subject_query != 'empty'):
 			
 			# Log SQL query
 			self.log.info("Getting full thread data, but first: " + sql_log)
-			self.log.info("SELECT " + sql_columns + " FROM posts WHERE true AND thread_in IN (...)" + sql_body + sql_subject + sql_min_date + sql_max_date)
+			self.log.info("SELECT " + sql_columns + " FROM 4cat_posts WHERE true AND " + sql_text + sql_min_date + sql_max_date)
 
-			# Get the IDs of the matching threads
+			# Get the IDs of the matching posts
 			li_thread_ids = []
+
+			# If this function is used for dense threads, skip the thread_id lookup
 			if dense_threads != False:
 				li_thread_ids = dense_threads
 			else:
+
+				# First, get the post ids (Sphinx table doesn't have thread_ids)
 				try:
-					li_thread_ids = self.db.fetchall("SELECT thread_id FROM posts WHERE true" + sql_board + sql_body + sql_subject + sql_min_date + sql_max_date)
+					li_matches = sphinx_cursor.fetchall("SELECT post_id FROM 4cat_posts WHERE true" + sql_text + sql_min_date + sql_max_date)
 				except Exception as error:
 					return str(error)
 				
-				# Convert matching OP ids to tuple
+				# Convert matching ids to tuple
+				li_ids = tuple([post["post_id"] for post in li_matches])
+
+				# With the post ids, thread ids from the 'regular' `posts` table
+				try:
+					li_thread_ids = self.db.fetchall("SELECT thread_id FROM posts WHERE id in %s ", (li_ids,))
+				except Exception as error:
+					return str(error)
+				
+				# Convert matching thread_ids to tuple
 				li_thread_ids = tuple([thread["thread_id"] for thread in li_thread_ids])
 
 				# If there are no threads with the requested string in the title, return the empty list
 				if len(li_thread_ids) == 0:
 					return li_thread_ids
-								
-			# Fetch posts within matching thread IDs
+			
+			# Fetch all posts with matching thread_ids
 			try:
 				li_matches = self.db.fetchall("SELECT " + sql_columns + " FROM posts WHERE thread_id IN %s ORDER BY thread_id, timestamp", (li_thread_ids,))
 			except Exception as error:
@@ -232,6 +267,9 @@ class stringQuery(BasicWorker):
 		Returns a list of thread IDs that match the keyboard-density parameters
 
 		"""
+
+		# Connect to the Sphinx database
+		sphinx_cursor =  self.sphinx_db.cursor()
 				
 		# Get relevant parameter values
 		board = parameters["board"]
@@ -241,24 +279,19 @@ class stringQuery(BasicWorker):
 		min_date = parameters["min_date"]
 		max_date = parameters["max_date"]
 
-		# Mandatory that thread appears in threads.id for the respective board
-		board_threads = self.db.fetchall("SELECT id FROM threads WHERE board = '" + board + "'")
-		board_threads = list([thread["id"] for thread in board_threads])
-		board_threads = [str(thread) for thread in board_threads]
-		board_threads = ','.join(board_threads)
-
 		# Set body query. Check if there's anything in quotation marks for LIKE operations.
 		pattern = "\"(.*?)\""
 		li_exact_body = re.findall(pattern, body_query)
-		sql_body = " AND posts.body_vector @@ plainto_tsquery('" + body_query + "')"
+		sql_body = " AND MATCH('@body " + body_query
 		if li_exact_body:
 			for exact_body in li_exact_body:
-				sql_body = sql_body + " AND posts.body LIKE '%" + exact_body + "%'"
+				sql_body = sql_body + " @body \"" + exact_body + "\""
+		sql_body = sql_body + "')"
+
 		body_query = body_query.replace("\"", "")
 		
 		# Indicate which boards to query
 		sql_board = ''
-		#sql_board = ' AND thread_id IN (' + board_threads + ')'
 
 		# Set timestamp parameters. Currently checks timestamp of all posts with keyword within paramaters.
 		# Should perhaps change to OP timestamp.
@@ -269,21 +302,34 @@ class stringQuery(BasicWorker):
 		if max_date != 0:
 			sql_max_date = " AND posts.timestamp <= " + str(max_date)
 
-		self.log.info("Getting keyword-dense threads on " + board + " for " + body_query + " with a minimum thread length of " + str(dense_length) + " and a keyword density of " + str(dense_percentage) + ".")
-		
-		matching_threads = self.db.fetchall("""
-			SELECT thread_id, num_replies, keyword_count, keyword_density::real FROM (
-				SELECT thread_id, num_replies, keyword_count, ((keyword_count::real / num_replies::real) * 100) AS keyword_density FROM (
-					SELECT posts.thread_id, threads.num_replies, count(*) as keyword_count FROM posts
-					INNER JOIN threads ON posts.thread_id = threads.id
-					WHERE true """ + sql_board + sql_body + sql_min_date + sql_max_date + """
-					GROUP BY posts.thread_id, threads.num_replies
-				) AS thread_matches
-			) AS thread_meta
-			WHERE num_replies >= """ + str(dense_length) + """
-			AND keyword_density >= """ + str(dense_percentage) + """
+		# First, get all the posts that match the string query with Sphinx
+		self.log.info("Fetching ids from posts matching " + body_query)
+		try:
+			li_ids = sphinx_cursor.fetchall("SELECT post_id FROM 4cat_posts WHERE true" + sql_text + sql_min_date + sql_max_date)
+		except Exception as error:
+			return str(error)
 
-			""")
+		# Convert matching ids to tuple
+		li_ids = tuple([post["post_id"] for post in li_matches])
+
+		# With the post ids, get all the thread ids and metadata for the dense threads
+		self.log.info("Getting keyword-dense threads on " + board + " for " + body_query + " with a minimum thread length of " + str(dense_length) + " and a keyword density of " + str(dense_percentage) + ".")		
+		try:
+			matching_threads = self.db.fetchall("""
+				SELECT thread_id, num_replies, keyword_count, keyword_density::real FROM (
+					SELECT thread_id, num_replies, keyword_count, ((keyword_count::real / num_replies::real) * 100) AS keyword_density FROM (
+						SELECT posts.thread_id, threads.num_replies, count(*) as keyword_count FROM posts
+						INNER JOIN threads ON posts.thread_id = threads.id
+						WHERE posts.id IN(""" + li_ids + """)
+						GROUP BY posts.thread_id, threads.num_replies
+					) AS thread_matches
+				) AS thread_meta
+				WHERE num_replies >= """ + str(dense_length) + """
+				AND keyword_density >= """ + str(dense_percentage) + """
+
+				""")
+		except Exception as error:
+			return str(error)
 
 		# Convert threads to tuple
 		self.log.info("Found " + str(len(matching_threads)) + " " + body_query + "-dense threads.")
@@ -302,7 +348,7 @@ class stringQuery(BasicWorker):
 
 		"""
 
-		# Sme error handling
+		# Error handling
 		if type(sql_results) != list:
 			self.log.error('Please use a list instead of ' +  str(type(sql_results)) + ' to convert to csv')
 			self.log.error(sql_results)
