cf7133a9a028ebede1fc646ed867982ad714cb2b
Stijn <stijn.peeters@uva.nl>
2018-11-26 10:01:16 +0100

    Limit retries for HTTPError in image downloader

--- backend/workers/download_images.py
+++ backend/workers/download_images.py
@@ -37,10 +37,15 @@ class ImageDownloader(BasicWorker):
 		try:
 			url = "http://i.4cdn.org/%s/%s%s" % (job["details"]["board"], job["details"]["tim"], job["details"]["ext"])
 			image = requests.get(url)
-		except requests.HTTPError:
+		except requests.HTTPError as e:
 			# something wrong with our internet connection? or blocked by 4chan?
 			# try again in a minute
-			self.queue.release_job(job, delay=random.choice(range(15,45)))
+			if job["attempts"] > 2:
+				self.log.error("Could not download image %s after 2 retries (%s), aborting" % (job["details"]["tim"], e))
+				self.queue.finish_job(job)
+			else:
+				self.log.info("HTTP Error %s while downloading image, retrying later" % e)
+				self.queue.release_job(job, delay=random.choice(range(15,45)))
 			return
 
 		if image.status_code == 404:
@@ -51,7 +56,7 @@ class ImageDownloader(BasicWorker):
 		if image.status_code != 200:
 			# try again in 30 seconds
 			if job["attempts"] > 2:
-				self.log.warning("Could not download image %s after x retries (last response code %s), aborting",
+				self.log.error("Could not download image %s after 2 retries (last response code %s), aborting",
 								 (url, image.status_code))
 				self.queue.finish_job(job)
 			else:
