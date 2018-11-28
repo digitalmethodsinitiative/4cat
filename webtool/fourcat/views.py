723b6bd5dec038031a87f605043960c9601ee7e9
Stijn <stijn.peeters@uva.nl>
2018-11-28 15:43:18 +0100

    Fix results page route with trailing slash

--- webtool/fourcat/views.py
+++ webtool/fourcat/views.py
@@ -102,7 +102,7 @@ def string_query(board, body_query, subject_query, full_thread=0, dense_threads=
 	print("Query queued: %s" % query.key)
 	return query.key
 
-@app.route('/results')
+@app.route('/results/')
 def show_results():
 	queries = db.fetchall("SELECT * FROM queries WHERE is_finished = TRUE ORDER BY timestamp DESC LIMIT 20")
 
