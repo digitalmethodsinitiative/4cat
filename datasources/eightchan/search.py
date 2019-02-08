9580f878080c8e63b34a1084d61a997136593c45
Stijn <stijn.peeters@uva.nl>
2019-02-08 12:22:18 +0100

    Fix 8chan full thread query

--- datasources/eightchan/search.py
+++ datasources/eightchan/search.py
@@ -13,3 +13,17 @@ class Search8Chan(Search4Chan):
 	type = "8chan-search"
 	sphinx_index = "8chan"
 	prefix = "8chan"
+
+	def fetch_threads(self, thread_ids):
+		"""
+		Fetch post from database for given threads
+
+		:param list thread_ids: List of thread IDs to return post data for
+		:return list: List of posts, with a dictionary representing the database record for each post
+		"""
+		columns = ", ".join(self.return_cols)
+
+		thread_ids = tuple([str(thread_id) for thread_id in thread_ids])
+		return self.db.fetchall(
+			"SELECT " + columns + " FROM posts_" + self.prefix + " WHERE thread_id IN %s ORDER BY thread_id ASC, id ASC",
+			(thread_ids,))
