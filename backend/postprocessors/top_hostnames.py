349f6f152acb95100bb2ba102fc79804d07e56ed
Stijn <stijn.peeters@uva.nl>
2019-02-12 10:20:27 +0100

    “Top domains” post-processor

--- backend/postprocessors/top_hostnames.py
+++ backend/postprocessors/top_hostnames.py
@@ -1,5 +1,5 @@
 """
-Extract URLs from results
+Extract domains from results
 """
 import re
 
@@ -8,20 +8,20 @@ from csv import DictReader, DictWriter
 from backend.abstract.postprocessor import BasicPostProcessor
 
 
-class URLExtractor(BasicPostProcessor):
+class DomainExtractor(BasicPostProcessor):
 	"""
-	Rank URLs by most mentioned
+	Rank domains by most mentioned
 	"""
-	type = "url-extractor"  # job type ID
-	title = "Top URLs"  # title displayed in UI
-	description = "Extract URLs mentioned in the posts and sort them by most-mentioned. Note that archive and URL shortener links are not expanded."  # description displayed in UI
+	type = "top-domains"  # job type ID
+	title = "Top Domains"  # title displayed in UI
+	description = "Extract domain names mentioned in the posts and sort them by most-mentioned. Note that archive and URL shortener links are not expanded."  # description displayed in UI
 	extension = "csv"  # extension of result file, used internally and in UI
 
 	def process(self):
 		"""
 		This takes a 4CAT results file as input, and outputs a new CSV file
 		with one column containing URLs and the other containing how often
-		the URL was mentioned
+		the domain was mentioned
 		"""
 		link_regex = re.compile(r"https?://[^\s]+")
 		links = {}
@@ -33,6 +33,7 @@ class URLExtractor(BasicPostProcessor):
 				post_links = link_regex.findall(post["body"])
 				if post_links:
 					for link in post_links:
+						link = link.split("/")[2]
 						if link not in links:
 							links[link] = 0
 						links[link] += 1
