e161ec6d55fe54cfa431ead322db40b4730c80c2
Stijn <stijn.peeters@uva.nl>
2019-05-10 16:22:30 +0200

    Misc

---
+++ backend/extras/delete_snapshots.py
@@ -0,0 +1,20 @@
+"""
+Delete all snapshot queries
+"""
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + "/../..")
+from backend.lib.database import Database
+from backend.lib.logger import Logger
+from backend.lib.query import DataSet
+
+logger = Logger()
+database = Database(logger=logger, appname="snapshot-deleter")
+
+snapshots = database.fetchall("SELECT key FROM queries WHERE parameters::json->>'user' = 'daily-snapshot'")
+
+for snapshot in snapshots:
+	print("Deleting %s" % snapshot["key"])
+	query = DataSet(key=snapshot["key"], db=database)
+	query.delete()
