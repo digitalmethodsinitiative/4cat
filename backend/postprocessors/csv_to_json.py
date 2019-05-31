31cb43ccae17be29db91204b12d0221739d9e968
Stijn <stijn.peeters@uva.nl>
2019-05-31 13:19:50 +0200

    Slice frequencies after sorting instead of before

--- backend/postprocessors/csv_to_json.py
+++ backend/postprocessors/csv_to_json.py
@@ -21,7 +21,7 @@ class ConvertCSVToJSON(BasicPostProcessor):
 	# all post-processors with CSV output
 	accepts = ["search", "collocations", "debate_metrics", "quote-ranker", "tfidf", "thread-metadata",
 			   "count-countries", "top-images", "url-extractor", "extract-usernames", "vector-ranker",
-			   "count-words", "youtube-metadata", "rank-attribute", "count-posts"]
+			   "count-words", "youtube-metadata", "attribute-frequencies", "count-posts"]
 
 	def process(self):
 		"""
