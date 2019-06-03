5102fd20ab3895a882b1a59b56f3f9d54c690920
Stijn <stijn.peeters@uva.nl>
2019-06-03 10:01:04 +0200

    Refuse fetching full thread data if > 100k threads

--- backend/abstract/string_query.py
+++ backend/abstract/string_query.py
@@ -239,6 +239,13 @@ class StringQuery(BasicWorker, metaclass=abc.ABCMeta):
 				if not thread_ids:
 					return []
 
+			if len(posts) > 100000:
+				self.log.info("Aborting full-thread query - too many OPs found")
+				self.query.update_status(
+					"Your query returned more than 100000 posts to fetch full thread data for - this is too much data to handle. Consider splitting your query into smaller periods of time or using a more precise query.")
+				self.job.finish()
+				return None
+
 			posts = self.fetch_threads(thread_ids)
 
 			self.query.update_status("Post data collected")
