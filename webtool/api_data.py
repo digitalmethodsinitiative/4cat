9c71c6a467d6405aa5bc5df87455c0042c6e4f87
Stijn <stijn.peeters@uva.nl>
2019-07-03 17:39:06 +0200

    Backwards compatibility

--- webtool/api_data.py
+++ webtool/api_data.py
@@ -321,6 +321,7 @@ def get_thread(datasource, board, thread, db, limit=0):
 
 
 @app.route('/api/image/<img_file>')
+@app.route('/api/imagefile/<img_file>')
 @api_ratelimit
 def get_image_file(img_file, limit=0):
 	"""
