41cb11ac7ffade0615a855a3913922903dd41d80
Stijn <stijn.peeters@uva.nl>
2019-08-29 20:28:07 +0200

    Instagram datasource

---
+++ datasources/instagram/__init__.py
@@ -0,0 +1,11 @@
+"""
+Initialize Instagram data source
+"""
+
+# An init_datasource function is expected to be available to initialize this
+# data source. A default function that does this is available from the
+# backend helpers library.
+from backend.lib.helpers import init_datasource
+
+# Internal identifier for this data source
+DATASOURCE = "instagram"
