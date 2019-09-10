9f63879368ced7e93b4a99e8d0271178677d3d2a
Stijn <stijn.peeters@uva.nl>
2019-09-10 15:39:56 +0200

    Clarify neologism template

--- backend/processors/presets/neologisms.py
+++ backend/processors/presets/neologisms.py
@@ -1,20 +1,23 @@
 """
-Generate co-link network of URLs in posts
+Extract neologisms
 """
 from backend.abstract.processor import BasicProcessor
 
 from backend.lib.dataset import DataSet
 from backend.lib.helpers import UserInput
 
-class URLCoLinker(BasicProcessor):
+
+class NeologismExtractor(BasicProcessor):
 	"""
-	Generate URL co-link network
+	Run post-processor chain to extract neologisms
 	"""
 	type = "preset-neologisms"  # job type ID
-	category = "Presets"  # category
+	category = "Presets"  # category. 'Presets' are always listed first in the UI.
 	title = "Extract neologisms"  # title displayed in UI
-	description = "Follows Van Soest (2019)'s protocol to extract neologisms - hitherto unused words - from the dataset. Assumes English-language data. Uses stopwords-iso as its stopword filter."  # description displayed in UI
-	extension = "csv"  # extension of result file, used internally and in UI
+	description = "Follows Van Soest (2019)'s protocol to extract neologisms - hitherto unused words - " \
+				  "from the dataset. Assumes English-language data. Uses stopwords-iso as its stopword " \
+				  "filter."
+	extension = "csv"
 
 	options = {
 		"timeframe": {
@@ -27,13 +30,16 @@ class URLCoLinker(BasicProcessor):
 
 	def process(self):
 		"""
-		This takes a 4CAT results file as input, and outputs a new CSV file
-		with all posts containing the original query exactly, ignoring any
-		* or " in the query
+		This queues a series of post-processors to extract neologisms from a
+		dataset through Van Soest (2019)'s protocol. The resulting top vector
+		ranking is used as the result of this processor, once available.
 		"""
 		timeframe = self.parameters.get("timeframe", self.options["timeframe"]["default"])
 
-		analysis = DataSet(parameters={
+		# processors are chained through the "next" parameter, which takes a
+		# list of dataset parameters (i.e. the parameters= argument for
+		# DataSet()). These may be nested at will
+		analysis_chain = DataSet(parameters={
 			"next": [{
 				"type": "vectorise-tokens",
 				"parameters": {
@@ -42,19 +48,20 @@ class URLCoLinker(BasicProcessor):
 						"parameters": {
 							"amount": True,
 							"top": 15,
-							"copy_to": self.dataset.key
+							"copy_to": self.dataset.key  # copy to the dataset with this key
 						}
 					}]
 				}
 			}],
-			"echobrackets": False,
 			"stem": False,
 			"strip_symbols": True,
 			"lemmatise": False,
 			"timeframe": timeframe,
 			"filter": ["wordlist-googlebooks-min40", "stopwords-iso-all"]
 		}, type="tokenise-posts", db=self.db, parent=self.dataset.key_parent)
-		self.queue.add_job("tokenise-posts", remote_id=analysis.key)
+
+		# this starts the chain
+		self.queue.add_job("tokenise-posts", remote_id=analysis_chain.key)
 
 	def after_process(self):
 		"""
