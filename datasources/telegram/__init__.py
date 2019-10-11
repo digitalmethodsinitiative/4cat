6941095a043ec123bb2eb2e940f13a9e13fda59f
Stijn <stijn.peeters@uva.nl>
2019-10-11 10:26:27 +0200

    Add Telegram data source

---
+++ datasources/telegram/__init__.py
@@ -0,0 +1,14 @@
+"""
+Initialize The Guardian data source
+"""
+
+# An init_datasource function is expected to be available to initialize this
+# data source. A default function that does this is available from the
+# backend helpers library.
+from backend.lib.helpers import init_datasource
+
+# Internal identifier for this data source
+DATASOURCE = "telegram"
+NAME = "Telegram"
+
+USER_SETTINGS = ["api_id", "api_key", "phone", "security_code"]
