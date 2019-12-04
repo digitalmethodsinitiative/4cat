c71f71209421d08ba56eea908c61d5ead0cdd6fa
Stijn <stijn.peeters@uva.nl>
2019-12-04 12:52:21 +0100

    Allow stripping URLs in 'merge post texts'

--- processors/stringify.py
+++ processors/stringify.py
@@ -5,7 +5,7 @@ import re
 from csv import DictReader
 
 from backend.abstract.processor import BasicProcessor
-
+from backend.lib.helpers import UserInput
 
 class Stringify(BasicProcessor):
 	"""
@@ -17,12 +17,27 @@ class Stringify(BasicProcessor):
 	description = "Collapses all posts in the results into one plain text string. The result can be used for word clouds, word trees, et cetera."  # description displayed in UI
 	extension = "txt"  # extension of result file, used internally and in UI
 
+	options = {
+		"strip-urls": {
+			"type": UserInput.OPTION_TOGGLE,
+			"default": True,
+			"help": "Remove URLs"
+		}
+	}
+
+	input = "csv:body"
+	output = "txt"
+
 	def process(self):
 		"""
 		This takes a 4CAT results file as input, and outputs a plain text file
 		containing all post bodies as one continuous string, sanitized.
 		"""
+
+		link_regex = re.compile(r"https?://[^\s]+")
 		delete_regex = re.compile(r"[^a-zA-Z)(.,\n -]")
+
+		strip_urls = self.parameters.get("strip-urls", self.options["strip-urls"]["default"])
 
 		posts = 0
 		self.dataset.update_status("Processing posts")
@@ -31,6 +46,8 @@ class Stringify(BasicProcessor):
 				csv = DictReader(source)
 				for post in csv:
 					posts += 1
+					if strip_urls:
+						post["body"] = link_regex.sub("", post["body"])
 					results.write(re.sub(r"\s+", " ", delete_regex.sub(" ", post["body"])).strip() + " ")
 
 
