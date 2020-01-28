3eae40a85d112b4dc7c2a8734cdcf9580aa43345
Stijn <stijn.peeters@uva.nl>
2020-01-28 11:55:48 +0100

    Allow random sample to deal with generators

--- backend/abstract/search.py
+++ backend/abstract/search.py
@@ -177,8 +177,9 @@ class Search(BasicProcessor, ABC):
 			# for simple queries, this is handled in get_posts_simple
 			if query.get("search_scope", None) == "random-sample":
 				try:
+					self.dataset.update_status("Creating random sample")
 					sample_size = int(query.get("sample_size", 5000))
-					random.shuffle(posts)
+					random.shuffle(list(posts))
 					return posts[0:sample_size]
 				except ValueError:
 					pass
