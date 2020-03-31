880424316573b151875075c0ecab8f74bb6e7cf5
LAPTOP\hagen <hagen.sal@gmail.com>
2020-03-31 10:56:33 +0200

    better update status if no parent dataset

--- backend/abstract/processor.py
+++ backend/abstract/processor.py
@@ -101,11 +101,11 @@ class BasicProcessor(BasicWorker, metaclass=abc.ABCMeta):
 				
 				# Not all datasets have parent keys
 				if self.dataset.get_genealogy():
-					parent_key = self.dataset.get_genealogy()[0].key
+					parent_key = " (via " + self.dataset.get_genealogy()[0].key + ")"
 				else:
 					parent_key = ""
 
-				raise ProcessorException("Processor %s raised %s while processing dataset %s (via %s) in %s:\n   %s\n" % (self.type, e.__class__.__name__, self.dataset.key, parent_key, location, str(e)))
+				raise ProcessorException("Processor %s raised %s while processing dataset %s %sin %s:\n   %s\n" % (self.type, e.__class__.__name__, self.dataset.key, parent_key, location, str(e)))
 
 
 	def after_process(self):
