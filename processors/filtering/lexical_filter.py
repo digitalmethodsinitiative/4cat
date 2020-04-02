af949ab2b2501b3a36b5adb6556e3a4855c9291f
Stijn <stijn.peeters@uva.nl>
2020-04-02 16:39:32 +0200

    Allow processors to create new standalone datasets. Fix #79.
    To do this, mark the processor as a filter and override after_process
    to copy and detach the processor's dataset

--- processors/filtering/lexical_filter.py
+++ processors/filtering/lexical_filter.py
@@ -1,14 +1,15 @@
 """
 Filter posts by lexicon
 """
-import shutil
 import pickle
 import re
+import os
 
 from csv import DictReader, DictWriter
 from pathlib import Path
 
 from backend.abstract.processor import BasicProcessor
+from backend.lib.dataset import DataSet
 from backend.lib.helpers import UserInput, convert_to_int
 
 import config
@@ -26,21 +27,15 @@ class LexicalFilter(BasicProcessor):
 	type = "lexical-filter"  # job type ID
 	category = "Filtering"  # category
 	title = "Filter by lexicon"  # title displayed in UI
-	description = "Copies the dataset, retaining only posts that match any selected lexicon of words or phrases"  # description displayed in UI
-	extension = "csv"  # extension of result file, used internally and in UI
+	description = "Copies the dataset, retaining only posts that match any selected lexicon of words or phrases. This creates a new, separate dataset you can run analyses on."  # description displayed in UI
+	extension = "dataset"  # extension of result file, used internally and in UI
 
 	input = "csv:body"
-	output = "csv:body"
+	output = "dataset"
 
 	# the following determines the options available to the user via the 4CAT
 	# interface.
 	options = {
-		"replace-parent": {
-			"type": UserInput.OPTION_TOGGLE,
-			"default": False,
-			"help": "Replace original dataset",
-			"tooltip": "If this is enabled, the result of this processor will replace the original, parent dataset. This allows you to run other analyses on the filtered data."
-		},
 		"lexicon": {
 			"type": UserInput.OPTION_MULTI,
 			"default": [],
@@ -76,9 +71,6 @@ class LexicalFilter(BasicProcessor):
 		and aggregates the results per chosen time frame
 		"""
 
-		# replace parent or save as new file?
-		replace = bool(self.parameters.get("replace-parent", self.options["replace-parent"]["default"]))
-
 		# load lexicons from word lists
 		lexicons = {}
 		for lexicon_id in self.parameters.get("lexicon", []):
@@ -97,7 +89,8 @@ class LexicalFilter(BasicProcessor):
 		if custom_id not in lexicons:
 			lexicons[custom_id] = set()
 
-		custom_lexicon = set([word.strip() for word in self.parameters.get("lexicon-custom", "").split(",") if word.strip()])
+		custom_lexicon = set(
+			[word.strip() for word in self.parameters.get("lexicon-custom", "").split(",") if word.strip()])
 		lexicons[custom_id] |= custom_lexicon
 
 		# compile into regex for quick matching
@@ -106,7 +99,8 @@ class LexicalFilter(BasicProcessor):
 			if not lexicons[lexicon_id]:
 				continue
 			lexicon_regexes[lexicon_id] = re.compile(
-				r"\b(" + "|".join([re.escape(term) for term in lexicons[lexicon_id] if term]) + r")\b", flags=re.IGNORECASE)
+				r"\b(" + "|".join([re.escape(term) for term in lexicons[lexicon_id] if term]) + r")\b",
+				flags=re.IGNORECASE)
 
 		# now for the real deal
 		self.dataset.update_status("Reading source file")
@@ -160,13 +154,33 @@ class LexicalFilter(BasicProcessor):
 				writer.writerow(post)
 
 				matching_items += 1
+		self.dataset.finish(matching_items)
 
-		# file has been written, finish
-		if replace:
-			parent = self.dataset.get_genealogy()[0]
-			shutil.move(self.dataset.get_results_path(), parent.get_results_path())
-			self.dataset.update_status("Parent dataset updated.")
-			parent.num_rows = matching_items
-			self.dataset.finish(-1)
-		else:
-			self.dataset.finish(matching_items)
+	def after_process(self):
+		super().after_process()
+
+		# copy this dataset - the filtered version - and make that copy standalone
+		# this has the benefit of allowing for all analyses that can be run on
+		# full datasets on the new, filtered copy as well
+		top_parent = self.dataset.get_genealogy()[0]
+
+		standalone = self.dataset.copy(shallow=False)
+		standalone.body_match = "(Filtered) " + top_parent.query
+		standalone.datasource = top_parent.parameters.get("datasource", "custom")
+
+		try:
+			standalone.board = top_parent.board
+		except KeyError:
+			standalone.board = self.type
+
+		standalone.type = "search"
+
+		standalone.detach()
+		standalone.delete_parameter("key_parent")
+
+		self.dataset.copied_to = standalone.key
+
+		# we don't need this file anymore - it has been copied to the new
+		# standalone dataset, and this one is not accessible via the interface
+		# except as a link to the copied standalone dataset
+		os.unlink(self.dataset.get_results_path())
