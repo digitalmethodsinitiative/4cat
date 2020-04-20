b6bff73667b4fcd7f55ef07e1a81da0c1e998c62
Stijn <stijn.peeters@uva.nl>
2020-04-20 12:24:43 +0200

    Allow no password or e-mail via create-user.py

--- helper-scripts/create_user.py
+++ helper-scripts/create_user.py
@@ -19,13 +19,15 @@ db = Database(logger=log, appname="create-user")
 
 cli = argparse.ArgumentParser()
 cli.add_argument("-u", "--username", required=True, help="Name of user (must be unique)")
+cli.add_argument("-p", "--password", help="Password (if left empty an e-mail will be sent to the user to reset it)")
+cli.add_argument("-e", "--noemail", help="Do not force an e-mail as username", action="store_true")
 
 args = cli.parse_args()
 
 if __name__ != "__main__":
 	sys.exit(1)
 
-if not re.match(r"[^@]+\@.*?\.[a-zA-Z]+", args.username):
+if not args.noemail and not re.match(r"[^@]+\@.*?\.[a-zA-Z]+", args.username):
 	print("Please provide an e-mail address as username.")
 	sys.exit(1)
 
@@ -40,13 +42,22 @@ if user is None:
 	print("Warning: User not created properly. No password reset e-mail sent.")
 	sys.exit(1)
 
-try:
-	user.email_token(new=True)
-	print("An e-mail containing a link through which the registration can be completed has been sent to %s." % args.username)
-except RuntimeError as e:
-	print("""
-WARNING: User registered but no e-mail was sent. The following exception was raised:
+if args.password:
+	user.set_password(args.password)
+	print("User created and password set!")
+else:
+	if args.noemail:
+		user.generate_token()
+		print("User created! No registration e-mail will be sent.")
+		print("%s can complete their registration via the following token:\n%s" % (args.username, user.get_token()))
+	else:
+		try:
+			user.email_token(new=True)
+			print("An e-mail containing a link through which the registration can be completed has been sent to %s." % args.username)
+		except RuntimeError as e:
+			print("""
+WARNING: User created but no e-mail was sent. The following exception was raised:
    %s
-   
+	   
 %s can complete their registration via the following token:
    %s""" % (e, args.username, user.get_token()))
