c07fb39e2a18705ff564ee52301bc9c10fe10cd1
Stijn <stijn.peeters@uva.nl>
2020-04-22 11:39:06 +0200

    Don't needlessly install chromium via pyppeteer

--- helper-scripts/migrate/migrate-1.10-1.11.py
+++ helper-scripts/migrate/migrate-1.10-1.11.py
@@ -1,10 +1,2 @@
-# all this does is make sure pyppeteer installs chromium
-import asyncio
-from pyppeteer import launch
-
-print("  Triggering Chromium download...")
-async def trigger_chromium_install():
-	browser = await launch()
-	await browser.close()
-
-asyncio.get_event_loop().run_until_complete(trigger_chromium_install())
+# this is now a dummy file as the updates in this migration were made obsolete
+# by the next one
