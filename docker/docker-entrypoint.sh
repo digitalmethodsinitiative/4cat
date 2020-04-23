0a74a82d9de278b9aba847da448aba422677347a
niczem <nic@sea-watch.org>
2020-04-23 11:26:36 +0200

    docker-entrypoint updated

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 set -e
 
+#set pg password for seeding
 export PGPASSWORD=test
 
+#seed db
 cd /usr/src/app && psql --host=db --port=5432 --user=test --dbname=4cat < backend/database.sql
+
+#generate password for admin user
+admin_password=$(openssl rand -base64 12)
+
+echo 'your admin password:'
+echo $admin_password
+
+python3 /usr/src/app/helper-scripts/create_user.py -u admin@admin.com -p $admin_password
+
+
 # flask db upgrade
 gunicorn --bind 0.0.0.0:5000 webtool:app
