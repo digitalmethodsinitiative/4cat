5566b4d49aa468eab623cb3b442a37bfdea41afc
Stijn <stijn.peeters@uva.nl>
2020-06-23 18:09:48 +0200

    Remove redundant 8chan datasource files

--- datasources/eightchan-qresearch/__init__.py
+++
@@ -1,7 +0,0 @@
-# Use default data source init function
-from backend.lib.helpers import init_datasource
-from datasources.fourchan import activity
-
-# Internal identifier for this data source
-DATASOURCE = "8chan"
-NAME = "8chan/8kun"
