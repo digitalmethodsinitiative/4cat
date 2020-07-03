f9aa3b3418ec8774b788e3ec8e1af9c7ac28925f
Stijn <stijn.peeters@uva.nl>
2020-07-03 12:00:13 +0200

    Make flowchart compatible with weeks

--- processors/visualisation/flowchart.py
+++ processors/visualisation/flowchart.py
@@ -81,44 +81,67 @@ class FlowChart(BasicProcessor):
 			date_key = "time" if "time" in reader.fieldnames else "date"
 			value_key = "value" if "value" in reader.fieldnames else "frequency"
 
-		for post in self.iterate_csv_items(self.source_file):
-			# Set label names
-			if post[item_key] not in labels:
-				labels.append(post[item_key])
-				label_key = str(len(result["labels"]))
-				result["labels"][label_key] = {
-					"n": post[item_key]
-				}
+			# Make sure we know what time format we're dealing with.
+			time_format = False
+			guess_time_format = False
+			if "timeframe" in self.parent.parameters:
+				time_format = self.parent.parameters["timeframe"]
+
+			if not time_format:
+				guess_time_format = True
+			elif time_format.startswith("year"):
+				time_format = "%Y"
+			elif time_format.startswith("month"):
+				time_format = "%Y-%m"
+			elif time_format.startswith("week"):
+				time_format = "%Y-%U"
+			elif time_format.startswith("day"):
+				time_format = "%Y-%m-%d"
 			else:
-				label_key = labels.index(post[item_key])
+				guess_time_format = True
 
-			# Make a bucket when a new timestamp appears
-			if post[date_key] == "overall":
-				timestamp = "overall"
-			else:
-				if len(post[date_key]) == 4: # years
-					time_format = "%Y"
-				elif 6 <= len(post[date_key]) <= 7: # months (2018-1 or 2018-01)
-					time_format = "%Y-%m"
-				elif 8 <= len(post[date_key]) <= 10: # days (2018-1-1, 2018-01-1, 2018-1-01 or 2018-01-01)
-					time_format = "%Y-%m-%d"
-				timestamp = int(datetime.datetime.strptime(post[date_key], time_format).timestamp())
+			for post in self.iterate_csv_items(self.source_file):
+				# Set label names
+				if post[item_key] not in labels:
+					labels.append(post[item_key])
+					label_key = str(len(result["labels"]))
+					result["labels"][label_key] = {
+						"n": post[item_key]
+					}
+				else:
+					label_key = labels.index(post[item_key])
 
-			# Multiply small floats so they can be converted to ints (necessary for tf-idf scores)
-			value = float(post.get(value_key))
-			if value % 1 != 0:
-				value = value * 100
-			value = int(value)
+				# If the parent dataset has no valid date parameter,
+				# we're going to guess what time format we're dealing with.
+				if guess_time_format:
+					if len(post[date_key]) == 4:  # years
+						time_format = "%Y"
+					elif 6 <= len(post[date_key]) <= 7:  # Assume months (2018-1 or 2018-01)
+						time_format = "%Y-%m"
+					elif 8 <= len(post[date_key]) <= 10:  # days (2018-1-1, 2018-01-1, 2018-1-01 or 2018-01-01)
+						time_format = "%Y-%m-%d"
 
-			# Add values to results dict
-			if timestamp not in timestamps:
-				result["buckets"].append({"d":timestamp, "i": [[label_key, value]]})
-				timestamps.append(timestamp)
-			else:
-				result["buckets"][len(result["buckets"]) - 1]["i"].append([label_key, value])
+				# Make a bucket when a new timestamp appears
+				if post[date_key] == "overall":
+					timestamp = "overall"
+				else:
+					timestamp = int(datetime.datetime.strptime(post[date_key], time_format).timestamp())
 
-			# Set max value
-			if value > result["max"]:
-				result["max"] = value
+				# Multiply small floats so they can be converted to ints (necessary for tf-idf scores)
+				value = float(post.get(value_key))
+				if value % 1 != 0:
+					value = value * 100
+				value = int(value)
+
+				# Add values to results dict
+				if timestamp not in timestamps:
+					result["buckets"].append({"d": timestamp, "i": [[label_key, value]]})
+					timestamps.append(timestamp)
+				else:
+					result["buckets"][len(result["buckets"]) - 1]["i"].append([label_key, value])
+
+				# Set max value
+				if value > result["max"]:
+					result["max"] = value
 
 		return result
