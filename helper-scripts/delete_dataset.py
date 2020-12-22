f09136826d15c6de796852a88ed1ecb38f6349c7
Stijn Peeters <git@stijnpeeters.nl>
2020-12-22 17:12:45 +0100

    Datasets, not queries

--- helper-scripts/delete_dataset.py
+++ helper-scripts/delete_dataset.py
@@ -1,5 +1,5 @@
 """
-Delete a query
+Delete a dataset
 """
 import argparse
 import sys
@@ -10,30 +10,28 @@ from backend.lib.database import Database
 from backend.lib.logger import Logger
 from backend.lib.dataset import DataSet
 
-import config
-
 # parse parameters
-cli = argparse.ArgumentParser(description="Deletes a query, the corresponding job, and any sub-queries.")
-cli.add_argument("-k", "--key", required=True, help="Query key to delete.")
+cli = argparse.ArgumentParser(description="Deletes a dataset, the corresponding job, and any child datasets.")
+cli.add_argument("-k", "--key", required=True, help="Dataset key to delete.")
 cli.add_argument("-q", "--quiet", type=bool, default=False,
 				 help="Whether to skip asking for confirmation. Defaults to false.")
 args = cli.parse_args()
 
 if not args.quiet:
-	confirm = input("This will delete the query, and any sub-queries. Are you sure? (y/n)")
+	confirm = input("This will delete the dataset and any child datasets. Are you sure? (y/n)")
 	if confirm.strip().lower() != "y":
 		sys.exit(0)
 
 logger = Logger()
-database = Database(logger=logger, appname="delete-query")
+database = Database(logger=logger, appname="delete-dataset")
 
 # Initialize query
 try:
 	parent = DataSet(key=args.key, db=database)
 except TypeError:
-	print("No query found with that key.")
+	print("No dataset found with that key.")
 	sys.exit(1)
 
 
 parent.delete()
-print("Done. Note that running jobs for the queries above are not stopped; you will have to wait for them to finish on their own.")
+print("Done. Note that running jobs for the datasets above are not stopped; you will have to wait for them to finish on their own.")
