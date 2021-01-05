b6a0d2d721e5616d083a42c8cf545c32f7f834d0
Stijn Peeters <git@stijnpeeters.nl>
2021-01-05 20:09:59 +0100

    Add BitChute and Parler datasources

---
+++ datasources/bitchute/search_bitchute.py
@@ -0,0 +1,223 @@
+"""
+Search Bitchute
+
+Scrape Bitchute videos via the Bitchute web API
+"""
+import dateparser
+import requests
+import time
+import re
+
+from bs4 import BeautifulSoup
+
+from backend.abstract.search import Search
+from backend.lib.exceptions import QueryParametersException, ProcessorInterruptedException
+
+
+class SearchBitChute(Search):
+    """
+    BitChute scraper
+    """
+    type = "bitchute-search"  # job ID
+    category = "Search"  # category
+    title = "Search BitChute"  # title displayed in UI
+    description = "Retrieve BitChute videos"  # description displayed in UI
+    extension = "csv"  # extension of result file, used internally and in UI
+
+    # not available as a processor for existing datasets
+    accepts = [None]
+
+    # let's not get rate limited
+    max_workers = 1
+
+    def get_posts_simple(self, query):
+        """
+        Run custom search
+
+        Fetches data from BitChute
+        """
+
+        # ready our parameters
+        parameters = self.dataset.get_parameters()
+        max_items = parameters.get("items", 100)
+        queries = [query.strip() for query in parameters.get("query", "").split(",")]
+        num_query = 0
+        detailed = parameters.get("scope") == "detail"
+
+        # bitchute uses a CSRF cookie that needs to be included on each request. The only way to obtain it is by
+        # visiting the site, so do just that and extract the CSRF token from the page:
+        session = requests.Session()
+        session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
+        request = session.get("https://www.bitchute.com/search")
+        csrftoken = BeautifulSoup(request.text, 'html.parser').findAll("input", {"name": "csrfmiddlewaretoken"})[0].get("value")
+        time.sleep(1)
+
+        for query in queries:
+            num_query += 1
+            query = query.strip()
+
+            page = 0
+            num_results = 0
+            while True:
+                if self.interrupted:
+                    raise ProcessorInterruptedException("Interrupted while scraping Parler")
+
+                # prepare the request - the CSRF param *must* be the first or the request will fail
+                post_data = {"csrfmiddlewaretoken": csrftoken, "query": query, "kind": "video", "duration": "",
+                             "sort": "", "page": str(page)}
+                headers = {'Referer': "https://www.bitchute.com/search", 'Origin': "https://www.bitchute.com/search"}
+                request = session.post("https://www.bitchute.com/api/search/list/", data=post_data, headers=headers)
+                response = request.json()
+
+                if response["count"] == 0 or num_results >= max_items:
+                    break
+
+                for video_data in response["results"]:
+                    # this is only included as '5 months ago' and so forth, not exact date
+                    # so use dateparser to at least approximate the date
+                    dt = dateparser.parse(video_data["published"])
+
+                    video = {
+                        "id": video_data["id"],
+                        "thread_id": video_data["id"],
+                        "subject": video_data["name"],
+                        "body": video_data["description"],
+                        "author": video_data["channel_name"],
+                        "author_id": video_data["channel_path"].split("/")[2],
+                        "timestamp": int(dt.timestamp()),
+                        "url": "https://www.bitchute.com" + video_data["path"],
+                        "views": video_data["views"],
+                        "length": video_data["duration"],
+                        "thumbnail": video_data["images"]["thumbnail"],
+                        "sensitivity": video_data["sensitivity"]
+                    }
+
+                    if detailed:
+                        # to get more details per video, we need to request the actual video detail page
+                        # start a new session, to not interfer with the CSRF token from the search session
+                        video_session = requests.session()
+                        video_page = video_session.get(video["url"])
+                        soup = BeautifulSoup(video_page.text, 'html.parser')
+                        video_csfrtoken = soup.findAll("input", {"name": "csrfmiddlewaretoken"})[0].get("value")
+
+                        # again, no structured info available for the publication date, but at least we can extract the
+                        # exact day it was uploaded
+                        published = dateparser.parse(
+                            soup.find(class_="video-publish-date").text.split("published at")[1].strip()[:1])
+                        category = re.findall(r'<a href="/category/([^/]+)/"', video_page.text)[0]
+
+                        # we need *two more requests* to get the comment count and like/dislike counts
+                        # this seems to be because bitchute uses a third-party comment widget
+                        video_session.headers = {'Referer': video["url"], 'Origin': video["url"]}
+                        counts = video_session.post("https://www.bitchute.com/video/%s/counts/" % video["id"],
+                                              data={"csrfmiddlewaretoken": video_csfrtoken}).json()
+                        comment_count = video_session.post("https://commentfreely.bitchute.com/api/get_comment_count/",
+                                                    data={"csrfmiddlewaretoken": video_csfrtoken, "cf_thread": "bc_" + video["id"]}).json()
+
+                        # merge data
+                        video = {
+                            **video,
+                            "category": category,
+                            "likes": counts["like_count"],
+                            "dislikes": counts["dislike_count"],
+                            "channel_subscribers": counts["subscriber_count"],
+                            "comments": comment_count["commentCount"]
+                        }
+
+                        if published:
+                            video["timestamp"] = int(published.timestamp())
+
+                        # may need to be increased? bitchute doesn't seem particularly strict
+                        time.sleep(0.5)
+
+                    num_results += 1
+                    yield video
+
+                page += 1
+                self.dataset.update_status(
+                    "Retrieved %i videos for query '%s' (%i/%i)" % (num_results, query, num_query, len(queries)))
+
+    def validate_query(query, request, user):
+        """
+        Validate BitChute query input
+
+        :param dict query:  Query parameters, from client-side.
+        :param request:  Flask request
+        :param User user:  User object of user who has submitted the query
+        :return dict:  Safe query parameters
+        """
+        # no query 4 u
+        if not query.get("query", "").strip():
+            raise QueryParametersException("You must provide a search query.")
+
+        if query.get("search_scope", "") not in ("basic", "detail"):
+            raise QueryParametersException("Invalid search scope: must be basic or detail")
+
+        # 500 is mostly arbitrary - may need tweaking
+        max_posts = 2500
+        if query.get("max_posts", ""):
+            try:
+                max_posts = min(abs(int(query.get("max_posts"))), max_posts)
+            except TypeError:
+                raise QueryParametersException("Provide a valid number of videos to query.")
+
+        # reformat queries to be a comma-separated list with no wrapping
+        # whitespace
+        whitespace = re.compile(r"\s+")
+        items = whitespace.sub("", query.get("query").replace("\n", ","))
+        if len(items.split(",")) > 10:
+            raise QueryParametersException("You cannot query more than 10 items at a time.")
+
+        # simple!
+        return {
+            "items": max_posts,
+            "query": items,
+            "scope": query.get("search_scope")
+        }
+
+    def get_search_mode(self, query):
+        """
+        BitChute searches are always simple
+
+        :return str:
+        """
+        return "simple"
+
+    def get_posts_complex(self, query):
+        """
+        Complex post fetching is not used by the BitChute datasource
+
+        :param query:
+        :return:
+        """
+        pass
+
+    def fetch_posts(self, post_ids, where=None, replacements=None):
+        """
+        Posts are fetched via the BitChute API for this datasource
+        :param post_ids:
+        :param where:
+        :param replacements:
+        :return:
+        """
+        pass
+
+    def fetch_threads(self, thread_ids):
+        """
+        Thread filtering is not a toggle for BitChute datasets
+
+        :param thread_ids:
+        :return:
+        """
+        pass
+
+    def get_thread_sizes(self, thread_ids, min_length):
+        """
+        Thread filtering is not a toggle for BitChute datasets
+
+        :param tuple thread_ids:
+        :param int min_length:
+        results
+        :return dict:
+        """
+        pass
---
+++ datasources/parler/search_parler.py
@@ -0,0 +1,221 @@
+"""
+Search Parler
+
+Scrape parler posts via the Parler web API
+"""
+import requests
+import datetime
+import json
+import time
+import re
+
+from backend.abstract.search import Search
+from backend.lib.exceptions import QueryParametersException, ProcessorInterruptedException
+
+
+class SearchParler(Search):
+    """
+    Parler scraper
+    """
+    type = "parler-search"  # job ID
+    category = "Search"  # category
+    title = "Search Parler"  # title displayed in UI
+    description = "Retrieve Parler posts by user or hashtag, in chronological order"  # description displayed in UI
+    extension = "csv"  # extension of result file, used internally and in UI
+
+    # not available as a processor for existing datasets
+    accepts = [None]
+
+    # let's not get rate limited
+    max_workers = 1
+
+    def get_posts_simple(self, query):
+        """
+        Run custom search
+
+        Fetches data from Parler via instaloader.
+        """
+
+        # ready our parameters
+        parameters = self.dataset.get_parameters()
+        max_posts = parameters.get("items", 100)
+        queries = [query.strip() for query in parameters.get("query", "").split(",")]
+        num_query = 0
+
+        # start a HTTP session. Parler uses two session 'cookies' that are required on each request, else no response
+        # will be given. These can only be obtained by logging in. Logging in via 4CAT is not preferred, because it will
+        # lead to quick rate limiting and requires people to share their passwords. Instead, ask users to obtain these
+        # values by logging in themselves.
+        session = requests.Session()
+        session.cookies.set("mst", parameters.get("mst", ""))
+        session.cookies.set("jst", parameters.get("jst", ""))
+        session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
+
+        for query in queries:
+            num_query += 1
+            query = query.strip()
+            is_hashtag = (query[0] == "#")
+
+            if is_hashtag:
+                params = {"tag": query[1:], "limit": 100}
+                url = "https://api.parler.com/v1/post/hashtag"
+            else:
+                # for user queries, we need the user ID, which is *not* the username and can only be obtained
+                # via the API
+                user_id = session.get("https://api.parler.com/v1/profile", params={"username": query}).json()["_id"]
+                params = {"id": user_id, "limit": 100}
+                url = "https://api.parler.com/v1/post/creator"
+
+            cursor = ""
+            num_posts = 0
+            while True:
+                if self.interrupted:
+                    raise ProcessorInterruptedException("Interrupted while scraping Parler")
+
+                if cursor:
+                    # handles pagination
+                    params["startkey"] = cursor
+
+                try:
+                    chunk_posts = session.get(url, params=params)
+
+                    if chunk_posts.status_code != 200:
+                        # this can happen when the login is incorrect, or a 404 is returned
+                        self.dataset.update_status(
+                            "Error scraping %s. Are the JST and MST values correct? Does the query exist?" % query,
+                            is_final=True)
+                        return
+
+                    chunk_posts = chunk_posts.json()
+                except json.JSONDecodeError:
+                    # this would be weird
+                    self.dataset.update_status("Got unexpected response from Parler API - cannot parse data, halting.",
+                                               is_final=True)
+                    return
+                except (requests.RequestException, ConnectionError):
+                    # this would be weird
+                    self.dataset.update_status("Error connecting to Parler - halting.", is_final=True)
+                    return
+
+
+
+                for post in chunk_posts["posts"]:
+                    # fairly straighforward - most of the API response maps 1-on-1 to 4CAT data fields
+                    num_posts += 1
+                    dt = datetime.datetime.strptime(post["createdAt"], "%Y%m%d%H%M%S")
+                    post = {
+                        "id": post["_id"],
+                        "thread_id": post["_id"],
+                        "subject": "",
+                        "body": post["body"],
+                        "author": query,
+                        "timestamp": dt.timestamp(),
+                        "comments": post["comments"],
+                        "urls": ",".join([("https://api.parler.com/l/" + link) for link in post["links"]]),
+                        "hashtags": ",".join(post["hashtags"]),
+                        "impressions": post["impressions"],
+                        "reposts": post["reposts"],
+                        "upvotes": post["upvotes"],
+                        "permalink": post.get("shareLink", "")
+                    }
+
+                    yield post
+
+                    if num_posts >= max_posts:
+                        return
+
+                self.dataset.update_status(
+                    "Retrieved %i posts for query '%s' (%i/%i)" % (num_posts, query, num_query, len(queries)))
+
+                # paginate, if needed
+                if not chunk_posts["last"]:
+                    cursor = chunk_posts["next"]
+                    time.sleep(1.5)
+                else:
+                    break
+
+    def validate_query(query, request, user):
+        """
+        Validate Parler query input
+
+        :param dict query:  Query parameters, from client-side.
+        :param request:  Flask request
+        :param User user:  User object of user who has submitted the query
+        :return dict:  Safe query parameters
+        """
+        # no query 4 u
+        if not query.get("query", "").strip():
+            raise QueryParametersException("You must provide a search query.")
+
+        if not query.get("jst") or not query.get("mst"):
+            raise QueryParametersException("You must provide the 'JST' and 'MST' values")
+
+        # 500 is mostly arbitrary - may need tweaking
+        max_posts = 2500
+        if query.get("max_posts", ""):
+            try:
+                max_posts = min(abs(int(query.get("max_posts"))), max_posts)
+            except TypeError:
+                raise QueryParametersException("Provide a valid number of posts to query.")
+
+        # reformat queries to be a comma-separated list with no wrapping
+        # whitespace
+        whitespace = re.compile(r"\s+")
+        items = whitespace.sub("", query.get("query").replace("\n", ","))
+        if len(items.split(",")) > 15:
+            raise QueryParametersException("You cannot query more than 15 items at a time.")
+
+        # simple!
+        return {
+            "items": max_posts,
+            "query": items,
+            "jst": query.get("jst"),
+            "mst": query.get("mst")
+        }
+
+    def get_search_mode(self, query):
+        """
+        Parler searches are always simple
+
+        :return str:
+        """
+        return "simple"
+
+    def get_posts_complex(self, query):
+        """
+        Complex post fetching is not used by the Parler datasource
+
+        :param query:
+        :return:
+        """
+        pass
+
+    def fetch_posts(self, post_ids, where=None, replacements=None):
+        """
+        Posts are fetched via the Parler API for this datasource
+        :param post_ids:
+        :param where:
+        :param replacements:
+        :return:
+        """
+        pass
+
+    def fetch_threads(self, thread_ids):
+        """
+        Thread filtering is not a toggle for Parler datasets
+
+        :param thread_ids:
+        :return:
+        """
+        pass
+
+    def get_thread_sizes(self, thread_ids, min_length):
+        """
+        Thread filtering is not a toggle for Parler datasets
+
+        :param tuple thread_ids:
+        :param int min_length:
+        results
+        :return dict:
+        """
+        pass
