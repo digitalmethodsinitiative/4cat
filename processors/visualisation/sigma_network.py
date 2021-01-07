566be796e78cc3a88476eb626dacfaca94199227
Stijn Peeters <git@stijnpeeters.nl>
2021-01-07 12:48:16 +0100

    Allow SigmaJS viz for bipartite user-tag graph

--- processors/visualisation/sigma_network.py
+++ processors/visualisation/sigma_network.py
@@ -34,7 +34,7 @@ class SigmaNetwork(BasicProcessor):
 	title = "Sigma js network"  # title displayed in UI
 	description = "Visualise a network in the browser with sigma js."  # description displayed in UI
 	extension = "html"  # extension of result file, used internally and in UI
-	accepts = ["word-embeddings-neighbours", "url-network", "cotag-network", "quote-network", "wiki-category-network", "collocations"]  # query types this post-processor accepts as input
+	accepts = ["bipartite-user-tag-network", "word-embeddings-neighbours", "url-network", "cotag-network", "quote-network", "wiki-category-network", "collocations"]  # query types this post-processor accepts as input
 	preview_allowed = False # Will slow down the page too much
 
 	input = "csv"
