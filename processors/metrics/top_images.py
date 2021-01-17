de1a421f28920577924cdb0efb3914d4040361b3
sal-phd-desktop <s.h.hagen@uva.nl>
2021-01-17 15:51:44 +0100

    Make top_images and download_images modules generic - can be used for any datasource now.

--- processors/metrics/top_images.py
+++ processors/metrics/top_images.py
@@ -3,9 +3,14 @@ Extract most-used images from corpus
 """
 import hashlib
 import base64
+import re
 import config
+import csv
+import shutil
 
+from collections import Counter
 from backend.abstract.processor import BasicProcessor
+from backend.lib.helpers import UserInput
 
 __author__ = "Stijn Peeters"
 __credits__ = ["Stijn Peeters"]
@@ -17,17 +22,25 @@ class TopImageCounter(BasicProcessor):
 	"""
 	Top Image listing
 
-	Collects all images used in a data set, and sorts by most-used
+	Collects all images used in a data set, and sorts by most-used.
 	"""
 	type = "top-images"  # job type ID
 	category = "Post metrics"  # category
 	title = "Top images"  # title displayed in UI
-	description = "Collect all images used in the data set, and sort by most used. Contains URLs through which the images may potentially be downloaded."  # description displayed in UI
+	description = "Collect all images used in the data set, and sort by most used. Contains URLs through which the images may be downloaded."  # description displayed in UI
 	extension = "csv"  # extension of result file, used internally and in UI
-	datasources = ["4chan"]
 
-	input = "csv:image_file, image_md5"
-	output = "csv:md5_hash,filename,num_posts,url_4cat,url_4plebs,url_fireden"
+	input = "csv"
+	output = "csv"
+
+	options = {
+		"overwrite": {
+			"type": UserInput.OPTION_TOGGLE,
+			"default": False,
+			"help": "Add extracted image URLs to source csv",
+			"tooltip": "This will add a new column, \"img_url\", to the dataset's csv file."
+		}
+	}
 
 	def process(self):
 		"""
@@ -37,38 +50,157 @@ class TopImageCounter(BasicProcessor):
 		"""
 		images = {}
 
-		self.dataset.update_status("Reading source file")
-		for post in self.iterate_csv_items(self.source_file):
-			if not post["image_file"]:
-				continue
+		restuls = None
 
-			if post["image_md5"] not in images:
-				# md5 is stored encoded; make it normal ascii
-				md5 = hashlib.md5()
-				md5.update(base64.b64decode(post["image_md5"]))
+		# 4chan data has specific columns for image information, so treat this a bit differently.
+		if self.parent.parameters["datasource"] == "4chan":
+			
+			self.dataset.update_status("Extracting image links from source file")
+			
+			for post in self.iterate_csv_items(self.source_file):
 
-				images[post["image_md5"]] = {
-					"filename": post["image_file"],
-					"md5": md5.hexdigest(),
-					"hash": post["image_md5"],
-					"count": 0
-				}
+				if not post["image_file"]:
+					continue
 
-			images[post["image_md5"]]["count"] += 1
+				if post["image_md5"] not in images:
+					# md5 is stored encoded; make it normal ascii
+					md5 = hashlib.md5()
+					md5.update(base64.b64decode(post["image_md5"]))
 
-		top_images = {id: images[id] for id in sorted(images, key=lambda id: images[id]["count"], reverse=True)}
+					images[post["image_md5"]] = {
+						"filename": post["image_file"],
+						"md5": md5.hexdigest(),
+						"hash": post["image_md5"],
+						"count": 0
+					}
 
-		results = [{
-			"md5_hash": images[id]["md5"],
-			"filename": images[id]["filename"],
-			"num_posts": images[id]["count"],
-			"url_4cat": ("s" if config.FlaskConfig.SERVER_HTTPS else "") + "://" + config.FlaskConfig.SERVER_NAME + "/api/image/" + images[id]["md5"] + "." + images[id]["filename"].split(".")[
-				-1],
-			"url_4plebs": "https://archive.4plebs.org/_/search/image/" + images[id]["hash"].replace("/", "_"),
-			"url_fireden": "https://boards.fireden.net/_/search/image/" + images[id]["hash"].replace("/", "_")
-		} for id in top_images]
+				images[post["image_md5"]]["count"] += 1
+
+			top_images = {id: images[id] for id in sorted(images, key=lambda id: images[id]["count"], reverse=True)}
+
+			results = [{
+				"md5_hash": images[id]["md5"],
+				"filename": images[id]["filename"],
+				"num_posts": images[id]["count"],
+				"url_4cat": ("s" if config.FlaskConfig.SERVER_HTTPS else "") + "://" + config.FlaskConfig.SERVER_NAME + "/api/image/" + images[id]["md5"] + "." + images[id]["filename"].split(".")[
+					-1],
+				"url_4plebs": "https://archive.4plebs.org/_/search/image/" + images[id]["hash"].replace("/", "_"),
+				"url_fireden": "https://boards.fireden.net/_/search/image/" + images[id]["hash"].replace("/", "_")
+			} for id in top_images]
+
+		else:
+			
+			# Important: image link regex.
+			# Makes sure that it gets "http://site.com/img.jpg", but also
+			# more complicated ones like
+			# https://preview.redd.it/3thfhsrsykb61.gif?format=mp4&s=afc1e4568789d2a0095bd1c91c5010860ff76834
+			img_link_regex = re.compile(r"(?:www\.|https?:\/\/)[^\s\(\)\]\[]*\.(?:png|jpg|jpeg|gif|gifv)[^\s\(\)\]\[]*", re.IGNORECASE)
+
+			# Imgur and gfycat links that do not end in an extention are also accepted.
+			# These can later be downloaded by adding an extention.
+			img_domain_regex = re.compile(r"(?:https:\/\/gfycat\.com\/|https:\/\/imgur\.com\/)[^\s\(\)\]\[]*", re.IGNORECASE)
+
+			self.dataset.update_status("Extracting image links from source file")
+			
+			all_links = [] # Used for overwrite
+			img_links = []
+			
+			for post in self.iterate_csv_items(self.source_file):
+
+				post_img_links = []
+
+				if "url" in post and (img_link_regex.match(post["url"]) or img_domain_regex.match(post["url"])):
+					# some datasources may provide a specific URL per post
+					# as a separate attribute.
+					post_img_links.append(post["url"])
+
+				if "urls" in post and (img_link_regex.search(post["urls"]) or img_domain_regex.search(post["url"])):
+					
+					# some datasources may provide a specific URL per post
+					# as a separate attribute.
+					urls = post["urls"].split(",")
+					urls = list(set(urls)) # to prevent spamming
+					for url in urls:
+						if url.strip() and (img_link_regex.match(post["urls"]) or img_domain_regex.match(post["url"])):
+							post_img_links.append(url)
+
+				# We also take into account urls in the text body itself.
+				# URLs need some processing because there may be multiple per post.
+				body_links = img_link_regex.findall(post["body"]) + img_domain_regex.findall(post["body"])
+				
+				if body_links:
+					post_img_links += body_links
+
+				# Only keep unique links per post, to prevent spamming.
+				post_img_links = list(set(post_img_links))
+
+				# Always add to all_links, so we can easily add to the source file if needed.
+				all_links.append(post_img_links)
+				
+				# Only add valid links to img_links.
+				if post_img_links:
+					img_links += post_img_links
+
+			img_ranked = Counter(img_links)
+			
+			results = [{
+				"img_url": k,
+				"num_posts": v
+			} for k, v in img_ranked.items()]
 
 		if not results:
+			self.dataset.update_status("Zero image URLs detected.")
 			return
 
+		# Also add the data to the original csv file, if indicated.
+		if self.parameters.get("overwrite"):
+			self.update_parent(all_links)
+
 		self.write_csv_items_and_finish(results)
+
+	def update_parent(self, li_urls):
+		"""
+		Update the original dataset with a column detailing the image urls.
+
+		"""
+
+		self.dataset.update_status("Adding image urls to the source file")
+
+		# Get the source file data path
+		genealogy = self.dataset.get_genealogy()
+		parent = genealogy[0]
+		parent_path = parent.get_results_path()
+
+		# Get a temporary path where we can store the data
+		tmp_path = self.dataset.get_staging_area()
+		tmp_file_path = tmp_path.joinpath(parent_path.name)
+
+		count = 0
+
+		# Get field names
+		with parent_path.open(encoding="utf-8") as input:
+			reader = csv.DictReader(input)
+			fieldnames = reader.fieldnames
+			fieldnames.append("img_url")
+
+		# Iterate through the original dataset and add values to a new img_link column
+		self.dataset.update_status("Writing csv with nouns.")
+		with tmp_file_path.open("w", encoding="utf-8", newline="") as output:
+
+			writer = csv.DictWriter(output, fieldnames=fieldnames)
+			writer.writeheader()
+
+			count = 0
+
+			for post in self.iterate_csv_items(parent_path):
+				post["img_url"] = ", ".join(li_urls[count])
+				writer.writerow(post)
+				count += 1
+
+		# Replace the source file path with the new file
+		shutil.copy(str(tmp_file_path), str(parent_path))
+
+		# delete temporary files and folder
+		shutil.rmtree(tmp_path)
+
+		self.dataset.update_status("Parent dataset updated.")
--- processors/visualisation/download_images.py
+++ processors/visualisation/download_images.py
@@ -7,9 +7,11 @@ import hashlib
 import base64
 import time
 import re
+import csv
+import shutil
 
 from pathlib import Path
-from PIL import Image, ImageFile, ImageOps
+from PIL import Image, ImageFile, ImageOps, UnidentifiedImageError
 
 from lxml import etree
 from lxml.cssselect import CSSSelector as css
@@ -34,11 +36,11 @@ class ImageDownloader(BasicProcessor):
 	type = "image-downloader"  # job type ID
 	category = "Visual"  # category
 	title = "Download images"  # title displayed in UI
-	description = "Download top images and compress as a zip file. May take a while to complete as images are sourced externally."  # description displayed in UI
+	description = "Download top images and compress as a zip file. May take a while to complete as images are sourced externally. Note that not always all images can be retrieved. For imgur galleries, only the first image is saved. For imgur gifv files, only the first frame is saved. Use the \"Add download status\" option to see what downloads succeeded"  # description displayed in UI
 	extension = "zip"  # extension of result file, used internally and in UI
 	accepts = ["top-images"]  # query types this post-processor accepts as input
 
-	input = "csv:filename,url_4cat"
+	input = "csv"
 	output = "zip"
 
 	options = {
@@ -48,6 +50,12 @@ class ImageDownloader(BasicProcessor):
 			"default": 100,
 			"min": 0,
 			"max": 5000
+		},
+		"overwrite": {
+			"type": UserInput.OPTION_TOGGLE,
+			"default": True,
+			"help": "Add download status to Top images",
+			"tooltip": "This will add two columns, \"download_status\" and \"img_name\", to above csv file so you can check whether downloading succeeded and what the image's filename is."
 		}
 	}
 
@@ -59,12 +67,12 @@ class ImageDownloader(BasicProcessor):
 		"""
 		images = {}
 
-		self.dataset.update_status("Reading source file")
-		parent = self.dataset.get_genealogy()[0]
-		external = "fireden" if parent.parameters["board"] == "v" else "4plebs"
-		rate_limit = 1 if external == "fireden" else 16
-
 		urls = []
+
+		# Get the source file data path
+		genealogy = self.dataset.get_genealogy()
+		parent = genealogy[0]
+		datasource = parent.parameters["datasource"]
 
 		try:
 			amount = max(0, min(1000, int(self.parameters.get("amount", 0))))
@@ -72,80 +80,210 @@ class ImageDownloader(BasicProcessor):
 			amount = 100
 
 		extensions = {}
+		
+		# 4chan is the odd one out (images are traced to and scraped from archives), so treat differently.
+		if datasource == "4chan":
+			self.dataset.update_status("Reading source file")
+			parent = self.dataset.get_genealogy()[0]
+			external = "fireden" if parent.parameters["board"] == "v" else "4plebs"
+			rate_limit = 1 if external == "fireden" else 16
 
-		for post in self.iterate_csv_items(self.source_file):
-			# stop processing if worker has been asked to stop
-			if self.interrupted:
-				raise ProcessorInterruptedException("Interrupted while extracting image URLs")
 
-			if len(urls) >= amount:
-				break
+			for post in self.iterate_csv_items(self.source_file):
+				# stop processing if worker has been asked to stop
+				if self.interrupted:
+					raise ProcessorInterruptedException("Interrupted while extracting image URLs")
 
-			extension = post["filename"].split(".")[1].lower()
-			if extension not in ("jpg", "jpeg", "png", "gif"):
-				continue
+				if len(urls) >= amount:
+					break
 
-			local_file = post["url_4cat"].split("/")[-1]
-			local_path = Path(config.PATH_IMAGES, local_file)
-			if local_path.exists():
-				url = local_path
-			else:
-				url = post["url_" + external]
+				extension = post["filename"].split(".")[1].lower()
+				if extension not in ("jpg", "jpeg", "png", "gif"):
+					continue
 
-			urls.append(url)
-			extensions[url] = extension
+				local_file = post["url_4cat"].split("/")[-1]
+				local_path = Path(config.PATH_IMAGES, local_file)
+				if local_path.exists():
+					url = local_path
+				else:
+					url = post["url_" + external]
+
+				urls.append(url)
+				extensions[url] = extension
+
+		# With other sources, just use the generic set of image URLs.
+		else:
+			for row in self.iterate_csv_items(self.source_file):
+
+				if len(urls) >= amount:
+					break
+
+				img_url = row["img_url"]
+				extension = img_url.split(".")[-1].lower()
+				extensions[img_url] = extension
+				urls.append(img_url)
 
 		# prepare staging area
 		results_path = self.dataset.get_staging_area()
 		counter = 0
 
+		# Used to overwrite top-images csv file with download status
+		success = []
+
 		# loop through images and copy them onto the wall
 		for path in urls:
+
 			# stop processing if worker has been asked to stop
 			if self.interrupted:
 				raise ProcessorInterruptedException("Interrupted while downloading images.")
 
 			counter += 1
 			self.dataset.update_status("Downloading image %i of %i" % (counter, len(urls)))
+			success.append({"download_status": "failed", "img_name": ""})
 
 			# acquire and resize image
 			try:
-				picture = self.get_image(path, rate_limit=rate_limit)
-			except (requests.RequestException, IndexError, FileNotFoundError):
+				if datasource == "4chan":
+					picture = self.get_4chan_image(path, rate_limit=rate_limit)
+				else:
+					picture, image_name = self.get_image(path)
+			except (requests.RequestException, IndexError, FileNotFoundError) as e:
 				continue
 
 			# hash needs to be hexified if it's a 4chan hash
 			if not isinstance(path, Path) and path[-2:] == "==":
 				md5 = hashlib.md5()
 				b64hash = base64.b64decode(path.split("/")[-1].split(".")[0].replace("_", "/"))
+				
 				try:
 					md5.update(b64hash)
 				except binascii.Error:
 					self.log.warning("Invalid base64 hash %s, skipping" % b64hash)
 					continue
 
-				hash = md5.hexdigest()
-			else:
-				# if we're using an already-saved image the hash is good as it is
-				hash = path.stem
+				file_id = md5.hexdigest()
+				
+				# determine file name
+				image_name = file_id + "." + extensions[path]
 
-			# determine file name
-			imagepath = str(results_path.joinpath(hash)) + "." + extensions[path]
+				# determine where to save
+				imagepath = str(results_path.joinpath(file_id)) + "." + extensions[path]
+
+			else:
+				# if we're using an already-saved image the image filename is good as it is
+				imagepath = str(results_path.joinpath(image_name))
 
 			# save file
 			try:
 				picture.save(imagepath)
-			except OSError:
+			except (OSError, ValueError):
 				self.log.warning("Could not save image %s to disk - invalid format" % path)
 				continue
+
+			# If this all succeeded, we update the download status and the filename.
+			success[counter - 1]["download_status"] = "succeeeded"
+			success[counter - 1]["img_name"] = image_name
+
+		# Also add the data to the original csv file, if indicated.
+		if self.parameters.get("overwrite"):
+			self.update_parent(success)
 
 		# finish up
 		self.dataset.update_status("Compressing images")
 		self.write_archive_and_finish(results_path)
 
-	def get_image(self, path, rate_limit=0):
+	def get_image(self, path):
 		"""
-		Create image from path
+		Get image from a generic URL.
+	
+		Images from URLs ending in image extensions are attempted to download.
+		For imgur and gfycat image URLs, which often do not end in an extension name,
+		we try to extrapolate the image file type and the corresponding URL directly to the image.
+
+		:param path:  Path to image, either a local path or a URL
+		:return Image:  Image object, or nothing if loading it failed
+		:returm str:  	Filename of the image
+		"""
+
+		image_url = None
+
+		# gfycat and imgur images have to handled a bit differently, so check for these first.
+		img_domain_regex = re.compile(r"(?:https:\/\/gfycat\.com\/|imgur\.com\/)[^\s\(\)\]\[]*", re.IGNORECASE)
+
+		# Determine whether the URL ends with a valid image extension.
+		exts = ["png", "jpg", "jpeg", "gif", "gifv"]
+		url_ext = path.split("/")[-1].lower().split(".")[-1]
+		if url_ext not in exts:
+			url_ext = None
+
+		# Treat imgur and gfycat links a bit differently.
+		if (img_domain_regex.search(path)):
+
+			# Imgur images can appear in different formats, so we have to process this a bit.
+			if "imgur.com/" in path:
+				
+				# gifv files on imgur are actually small mp4 files.
+				# Since downloading videos complicates this and follow-up processors,
+				# just safe the first frame that imgur also hosts as a .gif file. 
+				if url_ext == "gifv":
+					image_url = path[:-1]
+
+				# Other urls ending in file extensions we just attempt to download straight away.
+				elif any([ext == url_ext for ext in exts]):
+					image_url = path
+
+				# If there's not file extention at the end of the url,
+				# and the link is a so-called "gallery",
+				# use the image's .json endpoint imgur so graciously provides :)
+				# We only save the first image of the gallery.
+				elif "gallery" in path:
+					path += ".json"
+					page = requests.get(path)
+					imgur_images = []
+					imgur_data = page.json()
+					
+					try:
+						image = imgur_data["data"]["image"]["album_images"]["images"][0]
+					except KeyError as e:
+						raise FileNotFoundError
+
+					image_url = "https://imgur.com/" + image["hash"] + image["ext"]
+
+			elif "gfycat.com/" in path:
+
+				if not url_ext:
+					# For gfycat.com links, just add .gif and download
+					image_url = path + ".gif"
+				else:
+					image_url = path
+
+		# If the URL ends with an image file extension and it's not from imgur or gfycat, just attempt to download it.
+		else:
+			image_url = path
+
+		# Get the image!
+		if image_url:
+			image = requests.get(image_url, stream=True, timeout=20)
+		else:
+			raise FileNotFoundError
+		
+		# Use this for local saving.
+		image_name = image_url.split("/")[-1]
+
+		# Check if we succeededimage.headers["content-type"]
+		if image.status_code != 200 or image.headers["content-type"][:5] != "image":
+			raise FileNotFoundError
+		
+		try:
+			picture = Image.open(image.raw)
+		except UnidentifiedImageError:
+			raise FileNotFoundError
+
+		return picture, image_name
+
+	def get_4chan_image(self, path, rate_limit=0):
+		"""
+		Get image from a 4chan archive and save.
 
 		If the path is local, simply read the local path and return an Image
 		representing it. If not, attempt to download the image from elsewhere,
@@ -154,7 +292,6 @@ class ImageDownloader(BasicProcessor):
 
 		:param path:  Path to image, either a local path or a URL
 		:param rate_limit:  Seconds to wait after downloading, if downloading
-
 		:return Image:  Image object, or nothing if loading it failed
 		"""
 		rate_regex = re.compile(r"Search limit exceeded. Please wait ([0-9]+) seconds before attempting again.")
@@ -219,3 +356,52 @@ class ImageDownloader(BasicProcessor):
 			local_path.unlink()
 
 		return picture
+
+	def update_parent(self, success):
+		"""
+		Update the original dataset with a nouns column
+
+		"""
+
+		self.dataset.update_status("Adding image urls to the source file")
+
+		# Get the source file data path
+		genealogy = self.dataset.get_genealogy()
+		parent = genealogy[1]
+		parent_path = parent.get_results_path()
+
+		# Get a temporary path where we can store the data
+		tmp_path = self.dataset.get_staging_area()
+		tmp_file_path = tmp_path.joinpath(parent_path.name)
+
+		count = 0
+
+		# Get field names
+		with parent_path.open(encoding="utf-8") as input:
+			reader = csv.DictReader(input)
+			fieldnames = reader.fieldnames
+			fieldnames += ["download_status", "img_name"]
+
+		# Iterate through the original dataset and add values to a new img_link column
+		self.dataset.update_status("Writing download status to Top images csv.")
+		with tmp_file_path.open("w", encoding="utf-8", newline="") as output:
+
+			writer = csv.DictWriter(output, fieldnames=fieldnames)
+			writer.writeheader()
+
+			for post in self.iterate_csv_items(parent_path):
+		
+				if count < len(success):
+					post["download_status"] = success[count]["download_status"]
+					post["img_name"] = success[count]["img_name"]
+		
+				writer.writerow(post)
+				count += 1
+
+		# Replace the source file path with the new file
+		shutil.copy(str(tmp_file_path), str(parent_path))
+
+		# delete temporary files and folder
+		shutil.rmtree(tmp_path)
+
+		self.dataset.update_status("Parent dataset updated.")
