10a7101a8bbff0f0df94a194d5e44cc884c2fe1c
sal-phd-desktop <s.h.hagen@uva.nl>
2021-01-18 22:11:04 +0100

    download_images: mask requests

--- processors/visualisation/download_images.py
+++ processors/visualisation/download_images.py
@@ -269,7 +269,7 @@ class ImageDownloader(BasicProcessor):
 
 		# Get the image!
 		if image_url:
-			image = requests.get(image_url, stream=True, timeout=20)
+			image = requests.get(image_url, stream=True, timeout=20, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15"})
 		else:
 			raise FileNotFoundError
 		
