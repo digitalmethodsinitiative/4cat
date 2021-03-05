201488ad41c546b5079195a5dfdf36a1c3dfef13
stijn-uva <stijn.peeters@uva.nl>
2021-03-05 19:00:26 +0100

    Reworked image wall processor

--- processors/visualisation/image_wall.py
+++ processors/visualisation/image_wall.py
@@ -1,34 +1,23 @@
 """
 Create an image wall of the most-used images
 """
-import requests
-import hashlib
+import colorsys
 import random
 import shutil
-import base64
 import math
-import time
 import csv
-import re
 
-from pathlib import Path
-from PIL import Image, ImageFile, ImageOps
+from PIL import Image, ImageFile, ImageOps, ImageDraw, UnidentifiedImageError
+from sklearn.cluster import KMeans
 
-from lxml import etree
-from lxml.cssselect import CSSSelector as css
-from io import StringIO
-
-import config
-from backend.lib.helpers import UserInput
+from backend.lib.helpers import UserInput, convert_to_int
 from backend.abstract.processor import BasicProcessor
-
+from backend.lib.exceptions import ProcessorInterruptedException
 
 __author__ = "Stijn Peeters"
 __credits__ = ["Stijn Peeters"]
 __maintainer__ = "Stijn Peeters"
 __email__ = "4cat@oilab.eu"
-
-csv.field_size_limit(1024 * 1024 * 1024)
 
 class ImageWallGenerator(BasicProcessor):
 	"""
@@ -39,12 +28,11 @@ class ImageWallGenerator(BasicProcessor):
 	type = "image-wall"  # job type ID
 	category = "Visual"  # category
 	title = "Image wall"  # title displayed in UI
-	description = "Download top images and create an image wall. The amount of images used can be configured; the more images, the longer it takes to create the image wall. May take a while to complete as images need to be downloaded externally."  # description displayed in UI
+	description = "Put all images in an archive into a single combined image, optionally sorting and resizing them"
 	extension = "png"  # extension of result file, used internally and in UI
-	accepts = ["top-images", "tiktok-search", "instagram-search"]  # query types this post-processor accepts as input
-	datasources = ["4chan", "tiktok", "instagram"]
+	accepts = ["image-downloader"]  # query types this post-processor accepts as input
 
-	input = "csv:filename,url_4cat"
+	input = "zip"
 	output = "png"
 
 	options = {
@@ -54,11 +42,34 @@ class ImageWallGenerator(BasicProcessor):
 			"default": 112,
 			"min": 0,
 			"max": 200
+		},
+		"tile_size": {
+			"type": UserInput.OPTION_CHOICE,
+			"options": {
+				"square": "Square",
+				"average": "Average image in set",
+				"fit-height": "Fit height"
+			},
+			"default": "square",
+			"help": "Image tile size",
+			"tooltip": "'Fit height' retains image ratios but resizes them all to be the same height"
+		},
+		"sort_mode": {
+			"type": UserInput.OPTION_CHOICE,
+			"help": "Sort images by",
+			"options": {
+				"": "Do not sort",
+				"random": "Random",
+				"dominant": "Dominant colour (decent, faster)",
+				"kmeans-dominant": "Dominant K-means (precise, slow)",
+				"kmeans-average": "Weighted K-means average (precise, slow)",
+				"average-rgb": "Average colour (RGB; imprecise, fastest)",
+				"average-hsv": "Average colour (HSV; imprecise, fastest)",
+			},
+			"tooltip": "If you're unsure what sorting method to use, dominant colour usually gives decent results",
+			"default": ""
 		}
 	}
-
-	# to help with rate-limiting
-	previous_download = 0
 
 	# images will be arranged and resized to fit these image wall dimensions
 	# note that image aspect ratio may not allow for a precise fit
@@ -71,205 +82,250 @@ class ImageWallGenerator(BasicProcessor):
 		with one column with image hashes, one with the first file name used
 		for the image, and one with the amount of times the image was used
 		"""
-		images = {}
-
 		self.dataset.update_status("Reading source file")
-		parent = self.dataset.get_genealogy()[0]
-
-		urls = []
-		# determine where and how to get the image
-		# this is different between 4chan and other data sources since for
-		# 4chan the file may be stored locally
-		if parent.parameters["datasource"] == "4chan":
-			column_attempt = []
-			column_attempt.append("url_fireden" if parent.parameters["board"] == "v" else "url_4plebs")
-			rate_limit = 1 if parent.parameters["board"] == "v" else 16
-			image_download_method = self.get_image_4chan
-			tile_width = 100
-			tile_height = 100
-
-		else:
-			column_attempt = ["thumbnail_url", "url"]
-			image_download_method = self.get_image_generic
-			rate_limit = 1
-			tile_width = 200
-			tile_height = None
-
-		try:
-			amount = max(0, min(200, int(self.parameters.get("amount", 0))))
-		except ValueError:
-			amount = 100
-
-
-		# prepare staging area
-		tmp_path = self.dataset.get_staging_area()
-		if not tmp_path.exists():
-			tmp_path.mkdir()
-
-		for post in self.iterate_items(self.source_file):
-			if len(urls) >= amount:
-				break
-
-			for field in column_attempt:
-				if field in post:
-					urls.append(post[field])
-					break
-
-		# randomize it
-		random.shuffle(urls)
 
 		# prepare
 		ImageFile.LOAD_TRUNCATED_IMAGES = True
-		counter = 0
-		wall = None
+		sample_max = 75
 
-		# loop through images and copy them onto the wall
-		for path in urls:
-			counter += 1
-			self.dataset.update_status("Downloading image %i of %i" % (counter, len(urls)))
+		def numpy_to_rgb(numpy_array):
+			"""
+			Helper function to go from numpy array to list of RGB strings
 
-			# acquire and resize image
+			Used in the K-Means clustering part
+			"""
+			return ",".join([str(int(value)) for value in numpy_array])
+
+		max_images = convert_to_int(self.parameters.get("amount"), 12)
+		sizing_mode = self.parameters.get("tile_size", self.options["tile_size"]["default"])
+		sort_mode = self.parameters.get("sort_mode")
+
+		# we loop through the images twice - once to reduce them to a value
+		# that can be sorted, and another time to actually copy them to the
+		# canvas for the image wall
+
+		# we create a staging area manually here, so it is not automatically
+		# deleted after one loop, since we need two
+		staging_area = self.dataset.get_staging_area()
+
+		# first, extract and reduce, and store the sortable value in a
+		# dictionary with the image file name as key
+		image_colours = {}
+		dimensions = {}  # used to calculate optimal tile size later
+		index = 0
+		random_values = list(range(0, len(self.parent.num_rows)))
+		random.shuffle(random_values)
+
+		for path in self.iterate_archive_contents(self.source_file, staging_area):
+			if self.interrupted:
+				raise ProcessorInterruptedException("Interrupted while determining image wall order")
+
 			try:
-				picture = image_download_method(path, tmp_path, rate_limit=rate_limit)
-			except (requests.RequestException, IndexError, FileNotFoundError, OSError) as e:
-				# increase the counter here to leave an empty space for the
-				# missing image, since that itself can be significant
+				picture = Image.open(str(path))
+			except UnidentifiedImageError:
+				self.dataset.update_status("Image %s could not be parsed. Skipping." % path)
 				continue
 
-			if not wall:
-				# we can only initialise the wall here, since before we
-				# download an image we don't know what the best dimensions are
-				if not tile_width:
-					tile_width = int(picture.width)
-				if not tile_height:
-					tile_height = int((tile_width / picture.width) * picture.height)
+			self.dataset.update_status("Analyzing %s" % path.filename)
 
-				ratio = self.TARGET_WIDTH / self.TARGET_HEIGHT
-				tiles_y = math.ceil(math.sqrt((tile_width * len(urls)) / (ratio * tile_height)))
-				tiles_x = math.ceil(len(urls) / tiles_y)
-				wall = Image.new('RGB', (tiles_x * tile_width, tiles_y * tile_height))
+			# these calculations can take ages for huge images, so resize if it is
+			# larger than the threshold
+			dimensions[path.name] = (picture.width, picture.height)
+			if sort_mode not in ("", "random") and (picture.height > sample_max or picture.width > sample_max):
+				sample_width = int(sample_max * picture.width / max(picture.width, picture.height))
+				sample_height = int(sample_max * picture.height / max(picture.width, picture.height))
+				picture = ImageOps.fit(picture, (sample_width, sample_height))
 
-			picture = ImageOps.fit(picture, (tile_width, tile_height), method=Image.BILINEAR)
+			if sort_mode not in ("random"):
+				# ensure we get RGB values for pixels
+				picture = picture.convert("RGB")
 
-			# put image on wall
-			index = counter - 1
-			x = index % tiles_x
-			y = math.floor(index / tiles_x)
-			wall.paste(picture, (x * tile_width, y * tile_height))
+			# determine a 'representative colour'
+			if sort_mode == "random":
+				# just randomly sort it, don't even look at the colours
+				value = random_values.pop()
 
+			elif sort_mode in ("average-rgb", "average-hsv"):
+				# average colour, as RGB or HSV
+				pixels = picture.getdata()
+				if sort_mode == "average-hsv":
+					pixels = [colorsys.rgb_to_hsv(*pixel) for pixel in pixels]
 
-		if not wall:
-			self.dataset.update_status("No images could be downloaded", is_final=True)
-			shutil.rmtree(tmp_path)
-			self.dataset.finish(0)
-			return
+				sum_colour = (sum([p[0] for p in pixels]), sum([p[1] for p in pixels]), sum([p[2] for p in pixels]))
+				avg_colour = (sum_colour[0] / len(pixels), sum_colour[1] / len(pixels), sum_colour[2] / len(pixels))
 
-		if wall.width > self.TARGET_WIDTH:
-			self.dataset.update_status("Resizing image wall")
-			new_height = math.ceil((self.TARGET_WIDTH / wall.width) * wall.height)
-			wall = ImageOps.fit(wall, (self.TARGET_WIDTH, new_height), method=Image.BILINEAR)
+				# this is a bit dumb, but since all the other modes return rgb...
+				if sort_mode == "average-hsv":
+					avg_colour = colorsys.hsv_to_rgb(*avg_colour)
+
+				value = avg_colour
+
+			elif sort_mode == "dominant":
+				# most-occurring colour
+				colours = picture.getcolors(picture.width * picture.height)
+				colours = sorted(colours, key=lambda x: x[0], reverse=True)
+				value = colours[0][1]
+
+			elif sort_mode in ("kmeans-dominant", "kmeans-average"):
+				# use k-means clusters to determine the representative colour
+				# this is more computationally expensive but gives far better
+				# results.
+
+				# determine k-means clusters for this image, i.e. the n most
+				# dominant "average" colours, in this case n=3 (make parameter?)
+				pixels = picture.getdata()
+				clusters = KMeans(n_clusters=3, random_state=0)  # 0 so it is deterministic
+				predicted_centroids = clusters.fit_predict(pixels).tolist()
+
+				# now we have two options -
+				if sort_mode == "kmeans-dominant":
+					# the colour of the single most dominant k-means centroid
+					ranked_centroids = {}
+					for index in range(0, len(clusters.cluster_centers_)):
+						ranked_centroids[numpy_to_rgb(clusters.cluster_centers_[index])] = predicted_centroids.count(
+							index)
+
+					value = [int(v) for v in
+							 sorted(ranked_centroids, key=lambda k: ranked_centroids[k], reverse=True)[0].split(",")]
+
+				elif sort_mode == "kmeans-average":
+					# average colour of all k-means centroids, weighted by the
+					# dominance of each centroid
+					value = [0, 0, 0]
+					for index in clusters.labels_:
+						value[0] += clusters.cluster_centers_[index][0]
+						value[1] += clusters.cluster_centers_[index][1]
+						value[2] += clusters.cluster_centers_[index][2]
+
+					value[0] /= len(clusters.labels_)
+					value[1] /= len(clusters.labels_)
+					value[2] /= len(clusters.labels_)
+
+			# converted to HSV, because RGB does not sort nicely
+			image_colours[path.name] = colorsys.rgb_to_hsv(*value)
+			index += 1
+
+		# only retain the top n of the sorted list of images - this gives us
+		# our final image set
+		sorted_image_files = [path for path in sorted(image_colours, key=lambda k: image_colours[k])[:max_images]]
+		dimensions = {path: dimensions[path] for path in sorted_image_files}
+		average_size = (
+			sum([k[0] for k in dimensions.values()]) / len(dimensions),
+			sum([k[1] for k in dimensions.values()]) / len(dimensions))
+
+		# calculate 'tile sizes' (a tile is an image) and also the size of the
+		# canvas we will need to fit them all. The canvas can never be larger than
+		# this:
+		max_pixels = self.TARGET_WIDTH * self.TARGET_HEIGHT
+
+		if sizing_mode == "fit-height":
+			# assuming every image has the overall average height, how wide would
+			# the canvas need to be (if everything is on a single row)?
+			full_width = 0
+			for dimension in dimensions.values():
+				# ideally, we make everything the average height
+				optimal_ratio = average_size[1] / dimension[1]
+				full_width += dimension[0] * optimal_ratio
+
+			# now we can calculate the total amount of pixels needed
+			fitted_pixels = full_width * average_size[1]
+			if fitted_pixels > max_pixels:
+				# try again with a lower height
+				optimal_height = math.floor(average_size[1] * math.sqrt(max_pixels / fitted_pixels))
+				fitted_pixels = max_pixels
+			else:
+				optimal_height = average_size[1]
+
+			# find the canvas size that can fit this amount of pixels at the
+			# required proportions, provided that y = multiple of avg height
+			ideal_height = math.sqrt(fitted_pixels / (self.TARGET_WIDTH / self.TARGET_HEIGHT))
+			size_y = math.ceil(ideal_height / optimal_height) * optimal_height
+			size_x = fitted_pixels / size_y
+
+			tile_y = optimal_height
+			tile_x = -1  # varies
+
+		elif sizing_mode == "square":
+			# assuming each image is square, find a canvas with the right
+			# proportions that would fit all of them
+			# assume the average dimensions
+			tile_size = int(sum(average_size) / 2)
+
+			# this is how many pixels we need
+			fitted_pixels = tile_size * tile_size * len(sorted_image_files)
+
+			# does that fit our canvas?
+			if fitted_pixels > max_pixels:
+				tile_size = math.floor(math.sqrt(max_pixels / len(sorted_image_files)))
+				fitted_pixels = tile_size * tile_size * len(sorted_image_files)
+
+			ideal_width = math.sqrt(fitted_pixels / (self.TARGET_HEIGHT / self.TARGET_WIDTH))
+			size_x = math.ceil(ideal_width / tile_size) * tile_size
+			size_y = math.ceil(fitted_pixels / size_x / tile_size) * tile_size
+
+			tile_x = tile_y = tile_size
+
+		elif sizing_mode == "average":
+			tile_x = int(average_size[0])
+			tile_y = int(average_size[1])
+
+			fitted_pixels = tile_x * tile_y * len(sorted_image_files)
+			if fitted_pixels > max_pixels:
+				surplus = fitted_pixels - max_pixels
+				tile_x -= int(math.sqrt(surplus))
+				tile_y -= int(math.sqrt(surplus))
+				fitted_pixels = tile_x * tile_y * len(sorted_image_files)
+
+			ideal_width = math.sqrt(fitted_pixels / (self.TARGET_HEIGHT / self.TARGET_WIDTH))
+			size_x = math.ceil(ideal_width / tile_x) * tile_x
+			size_y = math.ceil(fitted_pixels / size_x / tile_y) * tile_y
+
+		else:
+			raise NotImplementedError("Sizing mode '%s' not implemented" % sizing_mode)
+
+		wall = Image.new("RGBA", (int(size_x), int(size_y)))
+		ImageDraw.floodfill(wall, (0, 0), (255, 255, 255, 0))  # transparent background
+		counter = 0
+		offset_x = 0
+		offset_y = 0
+
+		tile_x = int(tile_x)
+		tile_y = int(tile_y)
+
+		# now actually putting the images on a wall is relatively trivial
+		self.dataset.update_status("Rendering %i images to wall" % len(sorted_image_files))
+		for path in sorted_image_files:
+			counter += 1
+			picture = Image.open("temp-images/%s" % path)
+
+			if tile_x == -1:
+				picture_x = int(picture.width * (tile_y / picture.height))
+				picture = ImageOps.fit(picture, (picture_x, tile_y), method=Image.BILINEAR)
+			else:
+				picture = ImageOps.fit(picture, (tile_x, tile_y), method=Image.BILINEAR)
+
+			# simply put them side by side until the right edge is reached,
+			# then move to a new row
+			if offset_x + picture.width > wall.width:
+				offset_x = 0
+				offset_y += picture.height
+
+			# this can happen in some edge cases: there is an extra row of
+			# images we hadn't accounted for. In that case, simply enlarge the
+			# canvas.
+			if offset_y + picture.height > wall.height:
+				new_wall = Image.new("RGBA", wall.width, offset_y + picture.height)
+				ImageDraw.floodfill(new_wall, (0, 0), (255, 255, 255, 0))  # transparent background
+				new_wall.paste(wall, (0, 0))
+				wall = new_wall
+
+			wall.paste(picture, (offset_x, offset_y))
+			offset_x += picture.width
 
 		# finish up
 		self.dataset.update_status("Saving result")
 		wall.save(str(self.dataset.get_results_path()))
-		shutil.rmtree(tmp_path)
+		shutil.rmtree(staging_area)
 
 		self.dataset.update_status("Finished")
 		self.dataset.finish(counter)
-
-	def get_image_generic(self, path, staging, rate_limit = 0):
-		while self.previous_download > time.time() - rate_limit:
-			time.sleep(0.1)
-
-		self.previous_download = time.time()
-		image = requests.get(path, stream=True)
-		image.raw.decode_content = True
-
-		staging = staging.joinpath("temp-image")
-
-		with staging.open("wb") as output:
-			for chunk in image.iter_content(1024):
-				output.write(chunk)
-
-		picture = Image.open(str(staging))
-		picture.load()
-		staging.unlink()
-
-		return picture
-
-	def get_image_4chan(self, path, staging, rate_limit=0):
-		"""
-		Create image from path
-
-		If the path is local, simply read the local path and return an Image
-		representing it. If not, attempt to download the image from elsewhere,
-		and cache the downloaded result if possible, else discard the file
-		afterwards.
-
-		:param path:  Path to image, either a local path or a URL
-		:param rate_limit:  Seconds to wait after downloading, if downloading
-
-		:return Image:  Image object, or nothing if loading it failed
-		"""
-		# do we have the file locally?
-		filename = Path(config.PATH_IMAGES, path.split("/")[-1])
-		if filename.exists():
-			return Image.open(str(filename))
-
-
-		while self.previous_download > time.time() - rate_limit:
-			time.sleep(0.1)
-
-		self.previous_download = time.time()
-
-		rate_regex = re.compile(r"Search limit exceeded. Please wait ([0-9]+) seconds before attempting again.")
-
-		# get link to image from external HTML search results
-		# detect rate limiting and wait until we're good to go again
-		page = requests.get(path)
-		rate_limited = rate_regex.search(page.content.decode("utf-8"))
-
-		while rate_limited:
-			self.log.debug("Rate-limited by external source. Waiting %s seconds." % rate_limited[1])
-			time.sleep(int(rate_limited[1]))
-			page = requests.get(path)
-			rate_limited = rate_regex.search(page.content.decode("utf-8"))
-
-		# get link to image file from HTML returned
-		parser = etree.HTMLParser()
-		tree = etree.parse(StringIO(page.content.decode("utf-8")), parser)
-		image_url = css("a.thread_image_link")(tree)[0].get("href")
-
-		# download image itself
-		image = requests.get(image_url, stream=True)
-		if image.status_code != 200:
-			raise FileNotFoundError
-
-		# cache the image for later, if needed
-		if config.PATH_IMAGES and Path(config.PATH_ROOT, config.PATH_IMAGES).exists():
-			md5 = hashlib.md5()
-
-			based_hash = path.split("/")[-1].split(".")[0].replace("_", "/")
-			extension = image_url.split(".")[-1].lower()
-			md5.update(base64.b64decode(based_hash))
-
-			local_path = Path(config.PATH_IMAGES, md5.hexdigest() + "." + extension)
-			delete_after = False
-		else:
-			local_path = staging.joinpath("temp-image")
-			delete_after = True
-
-		# save file, somewhere
-		with local_path.open('wb') as file:
-			for chunk in image.iter_content(1024):
-				file.write(chunk)
-
-		# avoid getting rate-limited by image source
-		time.sleep(rate_limit)
-		picture = Image.open(local_path)
-
-		# if no image folder is configured, delete the temporary file
-		if delete_after:
-			local_path.unlink()
-
-		return picture
