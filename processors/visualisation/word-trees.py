fee8ae08eaac27fa11a9d29e55e026e9b2a6aab1
stijn-uva <stijn.peeters@uva.nl>
2021-03-07 17:44:45 +0100

    Bit too search-and-replace-happy

--- processors/visualisation/word-trees.py
+++ processors/visualisation/word-trees.py
@@ -411,7 +411,7 @@ class MakeWordtree(BasicProcessor):
 				x += text_width
 				block_position = (block_position[0] + text_width, block_position[1])
 
-			# draw the line connecting this node to the source_dataset node
+			# draw the line connecting this node to the parent node
 			if origin:
 				destination = (x - self.step, y + text_offset_y)
 
@@ -448,21 +448,21 @@ class MakeWordtree(BasicProcessor):
 
 	def merge_upwards(self, node):
 		"""
-		Merge a node with the source_dataset node if it has no siblings
+		Merge a node with the parent node if it has no siblings
 
 		Used to string together tokens into one longer text string
 
 		:param Node node:
 		"""
-		if not node or not node.source_dataset:
+		if not node or not node.parent:
 			return
 
-		parent = node.source_dataset
+		parent = node.parent
 		if len(node.siblings) == 0:
-			node.source_dataset.name += " " + node.name
+			node.parent.name += " " + node.name
 			# we need a reference because after the next line the node will
-			# have no source_dataset
-			node.source_dataset.children = node.children
+			# have no parent
+			node.parent.children = node.children
 
 		self.merge_upwards(parent)
 
