99e7bdb30ac694797229c0c5191d0b40d1045bad
guidoajansen <guido.a.jansen@gmail.com>
2021-04-06 10:55:36 +0200

    - Docker Fixes (#113)
    - Improvements on Docker
    - Advanced entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian:buster
 
-RUN apt update && apt install -y python3-pip libpq-dev python3-dev postgresql-server-dev-all
+RUN apt update && apt install -y python3-pip gawk curl libpq-dev python3-dev postgresql-server-dev-all unzip wget xvfb
 
 WORKDIR /usr/src/app
 
@@ -8,6 +8,8 @@ WORKDIR /usr/src/app
 RUN pip3 install --upgrade pip
 COPY ./requirements.txt /usr/src/app/requirements.txt
 COPY ./setup.py /usr/src/app/setup.py
+COPY ./docker-config.py /usr/src/app/config.py
+COPY ./VERSION /usr/src/app/VERSION
 COPY ./README.md /usr/src/app/README.md
 RUN mkdir /usr/src/app/backend
 RUN mkdir /usr/src/app/webtool
@@ -17,7 +19,25 @@ RUN pip3 install -r requirements.txt
 RUN pip3 install gunicorn
 RUN apt install -y postgresql-client
 
-RUN export PGPASSWORD=test
+# Install Netcat
+RUN apt-get update
+RUN apt install -y netcat
+
+# Install google chrome
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
+
+ENV DISPLAY=:99
+ENV CHROME_BIN=/usr/bin/google-chrome
+
+# install chromedriver
+RUN apt-get install -yqq unzip
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+# Set PG Password
+RUN export PGPASSWORD=supers3cr3t
+
 # copy project
 COPY . /usr/src/app/
 
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,27 +1,65 @@
 #!/bin/sh
 set -e
 
-if ! test -f "/usr/src/app/.tcat_created"; then
+version() { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+
+echo "Waiting for postgres..."
+
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+#seed db
+if psql --host=db --port=5432 --user=fourcat --dbname=fourcat -tAc "SELECT 1 FROM users WHERE name='admin@admin.com'"; then echo 'Seed present'; else
+echo 'Generating admin user'
 
 #generate password for admin user
 admin_password=$(openssl rand -base64 12)
 
-echo 'your admin password:'
-echo $admin_password
+echo 'Your admin password:'
+echo "$admin_password"
 
 #seed db
-cd /usr/src/app && psql --host=db --port=5432 --user=test --dbname=4cat < backend/database.sql
+cd /usr/src/app && psql --host=db --port=5432 --user=fourcat --dbname=fourcat < backend/database.sql
 
-
-python3 /usr/src/app/helper-scripts/create_user.py -u admin@admin.com -p $admin_password
-echo $admin_password > /usr/src/app/.tcat_created
+python3 /usr/src/app/helper-scripts/create_user.py -u admin@admin.com -p "$admin_password"
+echo "$admin_password" > /usr/src/app/.tcat_created
 
 fi
 
+echo 'Starting app'
+cd /usr/src/app
+
+# Check for Migrations
+CURRENT="1.9"
+CURRENT_FILE=".current-version"
+
+if test -f "$CURRENT_FILE"; then
+    CURRENT=$(head -n 1 $CURRENT_FILE)
+fi
+
+TARGET="1.9"
+TARGET_FILE="VERSION"
+
+if test -f "$TARGET_FILE"; then
+    TARGET=$(head -n 1 $TARGET_FILE)
+fi
+
+if [ "$(version "$CURRENT")" -ge "$(version "$TARGET")" ]; then
+    echo "Version is up to date"
+else
+    echo "Running migrations"
+    python3 helper-scripts/migrate.py
+fi
+
+python3 4cat-daemon.py start
+
+gunicorn --reload --bind 0.0.0.0:5000 webtool:app
 
 
-#seed db
-#set pg password for seeding
-export PGPASSWORD=test
 
-cd /usr/src/app && gunicorn --bind 0.0.0.0:5000 webtool:app
+
+
+
