3a5e04bb361c668987292b325b50530d1678c427
Dale Wahl <dalewahl@gmail.com>
2021-05-18 13:56:44 +0200

    Dynamic API host

--- backend/workers/api.py
+++ backend/workers/api.py
@@ -14,6 +14,7 @@ class InternalAPI(BasicWorker):
 	type = "api"
 	max_workers = 1
 
+	host = config.API_HOST
 	port = config.API_PORT
 
 	def work(self):
@@ -22,7 +23,7 @@ class InternalAPI(BasicWorker):
 
 		Opens a socket that continuously listens for requests, and passes a
 		client object on to a handling method if a connection is established
-		
+
 		:return:
 		"""
 		if self.port == 0:
@@ -45,7 +46,7 @@ class InternalAPI(BasicWorker):
 		while has_time:
 			has_time = start_trying > time.time() - 300  # stop trying after 5 minutes
 			try:
-				server.bind(("localhost", self.port))
+				server.bind((self.host, self.port))
 				break
 			except OSError as e:
 				if has_time and not self.interrupted:
@@ -60,7 +61,7 @@ class InternalAPI(BasicWorker):
 
 		server.listen(5)
 		server.settimeout(5)
-		self.manager.log.info("Local API listening for requests at localhost:%s" % self.port)
+		self.manager.log.info("Local API listening for requests at %s:%s" % (self.host, self.port))
 
 		# continually listen for new connections
 		while not self.interrupted:
