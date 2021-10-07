d07b1645f7341d540b21bf964ee54d7c1bd00c0b
Sal Hagen <s.h.hagen@uva.nl>
2021-10-07 10:47:19 +0200

    Make the Guardian not local

--- datasources/theguardian/__init__.py
+++ datasources/theguardian/__init__.py
@@ -10,5 +10,4 @@ from common.lib.helpers import init_datasource
 # Internal identifier for this data source
 DATASOURCE = "theguardian"
 NAME = "The Guardian Climate Change"
-IS_LOCAL = True
 IS_STATIC = True
