cec805d58757649709f25728140db47f804547a9
sal-phd-desktop <s.h.hagen@uva.nl>
2021-10-14 11:08:16 +0200

    Remove id_seq field in import 4CAT csv script

--- helper-scripts/import_4chan_csv.py
+++ helper-scripts/import_4chan_csv.py
@@ -39,7 +39,7 @@ db = Database(logger=logger, appname="queue-dump")
 csvnone = re.compile(r"^N$")
 
 safe = False
-if args.skip_duplicates:
+if args.skip_duplicates.lower() == "true":
 	print("Skipping duplicate rows (ON CONFLICT DO NOTHING).")
 	safe = True
 
@@ -53,7 +53,13 @@ with open(args.input, encoding="utf-8") as inputfile:
 
 	posts = 0
 	threads_added = 0
+	posts_added = 0
+	dups = 0
 	threads = {}
+
+	# We keep count of what threads we have last encounterd.
+	# This is done to prevent RAM hogging: we're inserting threads
+	# we haven't seen in a while to the db and removing them from this dict. 
 	threads_last_seen = {}
 
 	# Show status
@@ -62,7 +68,15 @@ with open(args.input, encoding="utf-8") as inputfile:
 
 	for post in reader:
 
-		# We collect thread data first, even though we might skip this post
+		posts += 1
+
+		# Skip rows if needed. Can be useful when importing didn't go correctly.
+		if args.offset and posts < args.offset:
+			if posts % 1000000 == 0:
+				print("Skipped %i posts." % posts)
+			continue
+
+		# We collect thread data first
 		if post["thread_id"] not in threads:
 			threads_added += 1
 			threads[post["thread_id"]] = {
@@ -100,18 +114,11 @@ with open(args.input, encoding="utf-8") as inputfile:
 		for k, v in threads_last_seen.items():
 			threads_last_seen[k] += 1
 
-		posts += 1
-
-		# Skip rows if needed. Can be useful when importing didn't go correctly.
-		if args.offset and posts < args.offset:
-			continue
-
 		# Collect post data
 		post = {k: csvnone.sub("", post[k]) if post[k] else None for k in post}
 
 		post_data = {
 			"id": post["id"],
-			"id_seq": post.get("id_seq", ""),
 			"board": args.board,
 			"thread_id": post["thread_id"],
 			"timestamp": post.get("timestamp", ""),
@@ -133,33 +140,35 @@ with open(args.input, encoding="utf-8") as inputfile:
 			"unsorted_data": post.get("unsorted_data", "")
 		}
 
-		db.insert("posts_4chan", post_data, commit=False, safe=safe)
+		new_post = db.insert("posts_4chan", post_data, commit=False, safe=safe, constraints=["id", "board"])
+
+		posts_added += new_post
 
 		# Insert per every 10000 posts
-		if posts > 0 and posts % 10000 == 0:
-			
-			print("Committing posts %i - %i. " % (posts - 10000, posts), end="")
+		if posts % 10000 == 0:
 
+			print("Committing posts %i - %i. %i new posts added. " % (posts - 10000, posts, posts_added), end="")
+			
 			# We're commiting the threads we didn't encounter in the last 100.000 posts. We're assuming they're complete and won't be seen in this archive anymore.
 			# This is semi-necessary to prevent RAM hogging.
 			threads_committed = 0
 			for thread_seen, last_seen in threads_last_seen.items():
 				if last_seen > 10000:
-					db.upsert("threads_4chan", data=threads[thread_seen], commit=False, constraints=["id", "board"])
+					db.insert("threads_4chan", data=threads[thread_seen], commit=False, safe=safe, constraints=["id", "board"])
 					threads.pop(thread_seen)
 					threads_committed += 1
 
 			# Remove committed threads from the last seen list
 			threads_last_seen = {k: v for k, v in threads_last_seen.items() if v < 10000}
 
-			print("Committing threads %i - %i (%i still updating)." % (threads_added - threads_committed, threads_added, threads_committed, len(threads)))
+			print("Committing threads %i - %i (%i still updating)." % (threads_added - threads_committed, threads_added, len(threads)))
 			
 			db.commit()
 
 	# Add the last posts and threads as well
 	print("Commiting leftover threads")
 	for thread in threads.values():
-		db.upsert("threads_4chan", data=thread, commit=False, constraints=["id", "board"])
+		db.insert("threads_4chan", data=thread, commit=False, safe=safe, constraints=["id", "board"])
 
 	db.commit()
 
