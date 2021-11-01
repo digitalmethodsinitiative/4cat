e204e1c90d654635317c9f56d3694bf26dd7c06b
Stijn Peeters <stijn.peeters@uva.nl>
2021-11-01 18:54:57 +0100

    Fix Parler scraper

--- datasources/parler/search_parler.py
+++ datasources/parler/search_parler.py
@@ -34,9 +34,15 @@ class SearchParler(Search):
         "intro": {
             "type": UserInput.OPTION_INFO,
             "help": "Posts are scraped in reverse chronological order; the most recent post for a given query will be "
-                    "scraped first. Note that you will need a Parler account to use this data source.\n\nYou can "
-                    "scrape up to **fifteen** items at a time. Separate the items with commas or blank lines. Enter "
-                    "hashtags and/or user names; make sure to include the `#` prefix for hashtags!"
+                    "scraped first. Note that you will need a Parler account to use this data source. Unfortunately "
+                    "Parler only provides approximate timestamps in the format of '1 week ago' et cetera, so scraped "
+                    "timestamps are estimates that get less accurate for older posts."
+        },
+        "intro-2": {
+            "type": UserInput.OPTION_INFO,
+            "help": "You can scrape up to **fifteen** items at a time. Separate the items with commas or blank lines. "
+                    "Enter hashtags and/or user names; make sure to include the `#` prefix for hashtags! Note that for "
+                    "hashtags, only the 20 most recent posts can be scraped."
         },
         "query": {
             "type": UserInput.OPTION_TEXT_LARGE,
@@ -56,28 +62,19 @@ class SearchParler(Search):
             "tooltip": "Echoes are Parler's equivalent to Twitter's retweets",
             "default": False
         },
-        "daterange": {
-            "type": UserInput.OPTION_DATERANGE,
-            "help": "Date range"
-        },
         "divider": {
             "type": UserInput.OPTION_DIVIDER
         },
-        "intro-jst-mst": {
+        "intro-token": {
             "type": UserInput.OPTION_INFO,
-            "help": "The following values can be obtained by logging in on Parler, then (in Chrome or Firefox) "
-                    "right-clicking on the page, and choosing 'Inspect Element'. In the panel that pops up, navigate "
-                    "to the 'Storage' tab. You will find 'jst' and 'mst' listed there; copy their values below."
+            "help": "The following values must be obtained from your browser after logging in on Parler. Then press "
+                    "F12 to open the Inspector, go to the 'Storage' tab in the panel that appears, and then open the "
+                    "entry for 'parler.com' under 'Cookies' in the Storage panel. Copy the value for "
+                    "'parler_auth_token', which looks like a long string of letters and numbers."
         },
-        "jst": {
+        "token": {
             "type": UserInput.OPTION_TEXT,
-            "help": "JST",
-            "cache": True,
-            "sensitive": True
-        },
-        "mst": {
-            "type": UserInput.OPTION_TEXT,
-            "help": "MST",
+            "help": "Auth token",
             "cache": True,
             "sensitive": True
         },
@@ -94,8 +91,6 @@ class SearchParler(Search):
         # ready our parameters
         parameters = self.dataset.get_parameters()
         max_posts = parameters.get("items", 100)
-        min_timestamp = parameters.get("min_date", 0)
-        max_timestamp = parameters.get("max_date", time.time())
         queries = [query.strip() for query in parameters.get("query", "").split(",")]
         scrape_echoes = parameters.get("scrape_echoes", False)
         num_query = 0
@@ -105,13 +100,8 @@ class SearchParler(Search):
         # lead to quick rate limiting and requires people to share their passwords. Instead, ask users to obtain these
         # values by logging in themselves.
         session = requests.Session()
-        session.cookies.set("mst", parameters.get("mst", ""))
-        session.cookies.set("jst", parameters.get("jst", ""))
+        session.cookies.set("parler_auth_token", parameters.get("token", ""))
         session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
-
-        user_map = {}
-        ref_map = {}
-        seen_parleys = set()
 
         for query in queries:
             if not query.strip():
@@ -122,35 +112,24 @@ class SearchParler(Search):
             is_hashtag = (query[0] == "#")
 
             if is_hashtag:
-                params = {"tag": query[1:], "limit": 100}
-                url = "https://api.parler.com/v1/post/hashtag"
+                params = {"tag": query[1:]}
+                url = "https://parler.com/pages/hashtags.php"
             else:
-                # for user queries, we need the user ID, which is *not* the username and can only be obtained
-                # via the API
-                try:
-                    user_id_src = self.request_from_parler(session, "GET", "https://api.parler.com/v1/profile", data={"username": query})
-                    user_id = user_id_src["_id"]
-                except KeyError:
-                    # user does not exist or no results
-                    continue
-                except json.JSONDecodeError as e:
-                    self.log.warning("%s:\n\n%s" % (e, user_id_src.text))
-                    continue
-                params = {"id": user_id, "limit": 100}
-                url = "https://api.parler.com/v1/post/creator"
+                params = {"user": query}
+                url = "https://parler.com/pages/feed.php"
 
-            cursor = ""
             num_posts = 0
-            while True:
+            page = 1
+            known_posts = set()
+            fetching = True
+            while fetching:
                 if self.interrupted:
                     raise ProcessorInterruptedException("Interrupted while scraping Parler")
 
-                if cursor:
-                    # handles pagination
-                    params["startkey"] = cursor
-
                 try:
-                    chunk_posts = self.request_from_parler(session, "GET", url, data=params)
+                    params[page] = page
+                    chunk_posts = session.post(url, data=params)
+                    page += 1
 
                     if chunk_posts.status_code in (404, 400):
                         # no results
@@ -174,98 +153,77 @@ class SearchParler(Search):
                     self.dataset.update_status("Error connecting to Parler - halting.", is_final=True)
                     return
 
-                if "posts" not in chunk_posts:
-                    self.log.warning(repr(chunk_posts))
+                if not chunk_posts:
+                    fetching = False
                     break
 
-                for user in chunk_posts.get("users", {}):
-                    user_map[user["id"]] = user["username"]
-
-                for ref in chunk_posts.get("postRefs", {}):
-                    ref_map[ref["_id"]] = ref
-
-
-                done = False
-                for post in chunk_posts["posts"]:
-                    # fairly straighforward - most of the API response maps 1-on-1 to 4CAT data fields
-                    # in case of reposts (echoes), use the original data and mark it as a repost
-                    if post.get("source_dataset") and int(post.get("depth", 0)) == 1:
-                        if not scrape_echoes:
-                            continue
-
-                        reposted_by = user_map.get(post["creator"])
-                        post_src = ref_map[post.get("source_dataset")]
-                    else:
-                        reposted_by = ""
-                        post_src = post
-
-                    if post_src["_id"] in seen_parleys:
-                        # items may be scraped twice e.g. when querying two
-                        # separate hashtags that are both used in a single
-                        # parley - so keep track of seen parleys and skip
+                for post in chunk_posts:
+                    if post.get("ad"):
+                        # skip ads
                         continue
 
-                    seen_parleys.add(post_src["_id"])
-
-                    dt = datetime.datetime.strptime(post["createdAt"], "%Y%m%d%H%M%S")
-                    post = {
-                        "id": post_src["_id"],
-                        "thread_id": post_src["_id"],
-                        "subject": "",
-                        "body": post_src["body"],
-                        "author": user_map.get(post_src["creator"], ""),
-                        "timestamp": int(dt.timestamp()),
-                        "comments": self.expand_number(post_src["comments"]),
-                        "urls": ",".join([("https://api.parler.com/l/" + link) for link in post_src["links"]]),
-                        "hashtags": ",".join(post_src["hashtags"]),
-                        "impressions": self.expand_number(post_src["impressions"]),
-                        "reposts": self.expand_number(post_src["reposts"]),
-                        "upvotes": self.expand_number(post_src["upvotes"]),
-                        "permalink": post_src.get("shareLink", ""),
-                        "reposted_by": reposted_by
-                    }
-
-                    if min_timestamp and dt.timestamp() < min_timestamp:
-                        done = True
+                    if post["primary"]["id"] in known_posts:
+                        # if we see repeating posts we're at the end
+                        fetching = False
                         break
 
-                    if max_timestamp and dt.timestamp() >= max_timestamp:
+                    known_posts.add(post["primary"]["id"])
+
+                    # for echoes, the main post is mostly empty, and the
+                    # content of the echoed post is enclosed - we mostly use
+                    # the content of the echoed post
+                    is_echo = False
+                    if post["primary"].get("is_echo") and not scrape_echoes:
                         continue
+                    elif post["primary"].get("is_echo"):
+                        is_echo = True
+                        post_src = post["parent"]
+                    else:
+                        post_src = post["primary"]
+
+                    post_src["badges"] = json.dumps(post_src["badges"])
+
+                    mapped_post = {
+                        "id": post_src["id"],
+                        "thread_id": post_src["id"],
+                        "subject": post_src.get("title", "") if post_src.get("title") else "",
+                        "body": post_src.get("full_body", post_src.get("body", "")),
+                        "author": post_src["username"],
+                        "author_fullname": post_src["name"],
+                        "timestamp": self.get_timedelta(post["primary"]),  # for echoes, time at which it was echoed
+                        "timestamp_ago": post["primary"]["time_ago"],
+                        "comments": post_src["commentCount"],
+                        "echoes": post_src["echoCount"],
+                        "votes": post_src["voteCount"],
+                        "image_url": post_src.get("image", ""),
+                        "video_url": post_src.get["video_data"]["videoSrc"] if post_src["has_video"] else "",
+                        "linked_urls": post_src.get("long_link"),
+                        "is_echo": "yes" if is_echo else "no",
+                        "is_sensitive": "yes" if bool(post_src["sensitive"]) else "no",
+                        "is_video": "yes" if bool(post_src["has_video"]) else "no",
+                        "echoed_by": "",
+                        "echoed_timestamp": "",
+                        "echoed_timestamp_ago": "",
+                    }
+
+                    if is_echo:
+                        # for echoes, add some extra metadata: the echo'ing
+                        # user and the timestamp of the echoed post
+                        mapped_post["echoed_by"] = post["primary"]["username"]
+                        mapped_post["echoed_timestamp"] = self.get_timedelta(post["parent"])
+                        mapped_post["echoed_timestamp_ago"] = post["parent"]["time_ago"]
 
                     num_posts += 1
-                    yield post
+                    yield mapped_post
 
                     if num_posts >= max_posts:
+                        fetching = False
                         break
 
                 self.dataset.update_status(
                     "Retrieved %i posts for query '%s' (%i/%i)" % (num_posts, query, num_query, len(queries)))
 
-                # paginate, if needed
-                if not done and num_posts < max_posts and not chunk_posts["last"]:
-                    cursor = chunk_posts["next"]
-                    time.sleep(1.5)
-                else:
-                    break
-
-            time.sleep(1)
-
-    def expand_number(self, num_str):
-        """
-        Expand '3.3k' to 3300, etc
-
-        :param str num_str:  Number string
-        :return int:  Expanded number
-        """
-        num_str = str(num_str)
-        if "k" in num_str:
-            return float(num_str.replace("k", "")) * 1000
-        if "m" in num_str:
-            return float(num_str.replace("m", "")) * 1000000
-        if "b" in num_str:
-            return float(num_str.replace("b", "")) * 1000000000
-        else:
-            return int(num_str)
+                time.sleep(1.5)
 
     def validate_query(query, request, user):
         """
@@ -280,10 +238,10 @@ class SearchParler(Search):
         if not query.get("query", "").strip():
             raise QueryParametersException("You must provide a search query.")
 
-        if not query.get("jst") or not query.get("mst"):
-            raise QueryParametersException("You must provide the 'JST' and 'MST' values")
+        if not query.get("token"):
+            raise QueryParametersException("You must provide an access token.")
 
-        # 500 is mostly arbitrary - may need tweaking
+        # mostly arbitrary - may need tweaking
         max_posts = 2500
         if query.get("max_posts", ""):
             try:
@@ -298,64 +256,38 @@ class SearchParler(Search):
         if len(items.split(",")) > 15:
             raise QueryParametersException("You cannot query more than 15 items at a time.")
 
-        # the dates need to make sense as a range to search within
-        after, before = query.get("daterange")
-        if before and after and before < after:
-            raise QueryParametersException("Date range must start before it ends")
-
-        query["min_date"], query["max_date"] = (after, before)
-
         # simple!
         return {
             "items": max_posts,
             "query": items,
-            "min_date": query.get("min_date", None),
-            "max_date": query.get("max_date", None),
-            "jst": query.get("jst"),
-            "mst": query.get("mst"),
+            "token": query.get("token"),
             "scrape_echoes": bool(query.get("scrape_echoes", False))
         }
 
-    def request_from_parler(self, session, method, url, headers=None, data=None):
+    def get_timedelta(self, post):
         """
-        Request something via the Parler API (or non-API)
+        Get timestamp based on delta time
 
-        To avoid having to write the same error-checking everywhere, this takes
-        care of retrying on failure, et cetera
+        Parler doesn't return precise timestamps, only e.g. "1 week ago".
+        This function returns a precise timestamp based on that on a best
+        effort basis, e.g. '1 week ago' would give the current timestamp
+        minus one week.
 
-        :param session:  Requests session
-        :param str method: GET or POST
-        :param str url:  URL to fetch
-        :param dict header:  Headers to pass with the request
-        :param dict data:  Data/params to send with the request
-
-        :return:  Requests response
+        :param dict post:  Post to get timestamp from
+        :return int:  Extrapolated unix timestamp
         """
-        retries = 0
-        response = None
-        while retries < 3:
-            try:
-                if method.lower() == "post":
-                    request = session.post(url, headers=headers, data=data)
-                elif method.lower() == "get":
-                    request = session.get(url, headers=headers, params=data)
-                else:
-                    raise NotImplemented()
+        unit = re.sub(r"[0-9]", "", post["time_ago"]).lower()
+        amount = int(re.sub(r"[^0-9]", "", post["time_ago"]))
 
-                if request.status_code >= 500:
-                    raise ConnectionError()
+        if unit == "d":
+            delta = datetime.timedelta(days=amount)
+        elif unit == "w":
+            delta = datetime.timedelta(weeks=amount)
+        elif unit == "mo":
+            delta = datetime.timedelta(days=amount * 30.5)
+        elif unit == "y":
+            delta = datetime.timedelta(days=amount * 365.25)
+        else:
+            delta = datetime.timedelta()
 
-                return request
-
-            except (ConnectionError, requests.RequestException) as e:
-                retries += 1
-                time.sleep(retries * 3)
-
-            except json.JSONDecodeError as e:
-                self.log.warning("Error decoding JSON: %s\n\n%s" % (e, request.text))
-
-        if not response:
-            self.log.warning("Failed Parler request to %s %i times, aborting" % (url, retries))
-            raise RuntimeError()
-
-        return response
+        return int((datetime.datetime.now() - delta).timestamp())
