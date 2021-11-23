cd644c627fd75cc6c4eecb9aac66218c306255b2
dj-urg <62430885+dj-urg@users.noreply.github.com>
2021-11-23 16:17:07 +0100

    Update search_youtube.py

--- datasources/youtube/search_youtube.py
+++ datasources/youtube/search_youtube.py
@@ -26,7 +26,7 @@ class SearchYouTube(Search):
         """
         Run custom search for video ID
         """
-        
+
     api_key = "xxxxXXXXxxxxXXXXxxxxXXXXxxxxXXXXxxxxXXX" # This is the API-key of the 4CAT user
     youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -34,12 +34,42 @@ class SearchYouTube(Search):
 
     box = [['Name', 'Comment', 'Time', 'Likes', 'Reply Count']]
 
+    data = youtube.commentThreads().list(part='snippet', videoId=ID, maxResults='100', textFormat="plainText").execute()
 
-    def scrape_comments_with_replies():
-        data = youtube.commentThreads().list(part='snippet', videoId=ID, maxResults='100', textFormat="plainText").execute()
+    for i in data["items"]:
+
+        name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
+        comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
+        published_at = i["snippet"]['topLevelComment']["snippet"]['publishedAt']
+        likes = i["snippet"]['topLevelComment']["snippet"]['likeCount']
+        replies = i["snippet"]['totalReplyCount']
+
+        box.append([name, comment, published_at, likes, replies])
+
+        totalReplyCount = i["snippet"]['totalReplyCount']
+
+        if totalReplyCount > 0:
+
+            parent = i["snippet"]['topLevelComment']["id"]
+
+            data2 = youtube.comments().list(part='snippet', maxResults='100', parentId=parent,
+                                            textFormat="plainText").execute()
+
+            for i in data2["items"]:
+                name = i["snippet"]["authorDisplayName"]
+                comment = i["snippet"]["textDisplay"]
+                published_at = i["snippet"]['publishedAt']
+                likes = i["snippet"]['likeCount']
+                replies = ""
+
+                box.append([name, comment, published_at, likes, replies])
+
+    while ("nextPageToken" in data):
+
+        data = youtube.commentThreads().list(part='snippet', videoId=ID, pageToken=data["nextPageToken"],
+                                                maxResults='100', textFormat="plainText").execute()
 
         for i in data["items"]:
-
             name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
             comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
             published_at = i["snippet"]['topLevelComment']["snippet"]['publishedAt']
@@ -62,41 +92,9 @@ class SearchYouTube(Search):
                     comment = i["snippet"]["textDisplay"]
                     published_at = i["snippet"]['publishedAt']
                     likes = i["snippet"]['likeCount']
-                    replies = ""
+                    replies = ''
 
                     box.append([name, comment, published_at, likes, replies])
 
-        while ("nextPageToken" in data):
-
-            data = youtube.commentThreads().list(part='snippet', videoId=ID, pageToken=data["nextPageToken"],
-                                                maxResults='100', textFormat="plainText").execute()
-
-            for i in data["items"]:
-                name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
-                comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
-                published_at = i["snippet"]['topLevelComment']["snippet"]['publishedAt']
-                likes = i["snippet"]['topLevelComment']["snippet"]['likeCount']
-                replies = i["snippet"]['totalReplyCount']
-
-                box.append([name, comment, published_at, likes, replies])
-
-                totalReplyCount = i["snippet"]['totalReplyCount']
-
-                if totalReplyCount > 0:
-
-                    parent = i["snippet"]['topLevelComment']["id"]
-
-                    data2 = youtube.comments().list(part='snippet', maxResults='100', parentId=parent,
-                                                textFormat="plainText").execute()
-
-                    for i in data2["items"]:
-                        name = i["snippet"]["authorDisplayName"]
-                        comment = i["snippet"]["textDisplay"]
-                        published_at = i["snippet"]['publishedAt']
-                        likes = i["snippet"]['likeCount']
-                        replies = ''
-
-                        box.append([name, comment, published_at, likes, replies])
-
-        df = pd.DataFrame({'Name': [i[0] for i in box], 'Comment': [i[1] for i in box], 'Time': [i[2] for i in box],
-                        'Likes': [i[3] for i in box], 'Reply Count': [i[4] for i in box]})
+    df = pd.DataFrame({'Name': [i[0] for i in box], 'Comment': [i[1] for i in box], 'Time': [i[2] for i in box],
+                       'Likes': [i[3] for i in box], 'Reply Count': [i[4] for i in box]})
