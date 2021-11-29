1adc5fcde4f36e7396f1f70cd199c14b4d7e9b68
Dale Wahl <dalewahl@gmail.com>
2021-11-29 13:50:43 +0100

    Allow crawling additional subpages within domain

--- datasources/url_scraper/search_webpages.py
+++ datasources/url_scraper/search_webpages.py
@@ -1,6 +1,7 @@
 """
 Twitter keyword search via the Twitter API v2
 """
+from urllib.parse import urlparse
 import datetime
 
 from backend.abstract.selenium_scraper import SeleniumScraper
@@ -35,54 +36,99 @@ class SearchWithSelenium(SeleniumScraper):
             "type": UserInput.OPTION_TEXT_LARGE,
             "help": "List of urls"
         },
+        "subpages": {
+            "type": UserInput.OPTION_TEXT,
+            "help": "Crawl additional host links/subpages",
+            "min": 0,
+            "max": 5,
+            "default": 0
+        },
     }
 
     def get_items(self, query):
         """
-        Seperate and check urls, then loop through each and collects the HTML.
+        Separate and check urls, then loop through each and collects the HTML.
 
         :param query:
         :return:
         """
+        num_of_subpages = self.parameters.get("subpages")
         urls = [url.strip() for url in self.parameters.get("query", "").split('\n')]
+        # Holds additional links if crawling desired
+        additional_urls_groups_to_scrape = []
 
+        # First scrape all original urls
         for url in urls:
-            result = {
-                      "url": url,
-                      "final_url": None,
-                      "subject": None,
-                      "body": None,
-                      "detected_404": None,
-                      "timestamp": None,
-                      "error": False,
-                      }
-            if not validate_url(url):
-                # technically we have already validated, but best to inform user which urls are invalid
-                result['timestamp'] = int(datetime.datetime.now().timestamp())
-                result['error'] = 'Invalid URL format'
-            else:
-                # Try to scrape the url
-                try:
-                    self.dataset.log('Scraping url: %s' % url)
-                    scraped_page = self.simple_scrape_page(url)
-                except TimeoutException as e:
-                    result['timestamp'] = int(datetime.datetime.now().timestamp())
-                    result['error'] = 'Selenium TimeoutException: %s' % e
-                    yield result
-                    continue
+            result = self.collect_page_result(url)
 
-                # simple_scrape_page returns False if the browser did not load a new page
-                if scraped_page:
-                    result['final_url'] = scraped_page.get('final_url')
-                    result['body'] = scraped_page.get('page_source')
-                    result['subject'] = scraped_page.get('page_title')
-                    result['detected_404'] = scraped_page.get('detected_404')
-                    result['timestamp'] = int(datetime.datetime.now().timestamp())
-                else:
-                    result['timestamp'] = int(datetime.datetime.now().timestamp())
-                    result['error'] = 'Unable to scrape url' % e
+            # Add additional links to be scraped afterwards
+            if num_of_subpages > 0:
+                host = urlparse(url).netloc
+                links = self.collect_links()
+                additional_urls_groups_to_scrape.append((host, url, links))
 
             yield result
+
+        # Scrape additional subpages if requested
+        if num_of_subpages > 0:
+            for url_group in additional_urls_groups_to_scrape:
+                collected_pages = 0
+                host = url_group[0]
+                original_link = url_group[1]
+                links = url_group[2]
+                for url in links:
+                    # Break if adequate pages collected
+                    if collected_pages >= num_of_subpages:
+                        break
+                    # Check that url is in same host and not the original link
+                    if urlparse(url).netloc == host and url != original_link:
+                        # Attempt to scrape url
+                        result = self.collect_page_result(url)
+                        # Check that results was successfully scraped
+                        if result['error'] is False:
+                            collected_pages += 1
+                            yield result
+
+    def collect_page_result(self, url):
+        """
+        Crawls a url and creates a results object with necessary information
+        """
+        result = {
+            "url": url,
+            "final_url": None,
+            "subject": None,
+            "body": None,
+            "detected_404": None,
+            "timestamp": None,
+            "error": False,
+        }
+        if not validate_url(url):
+            # technically we have already validated, but best to inform user which urls are invalid
+            result['timestamp'] = int(datetime.datetime.now().timestamp())
+            result['error'] = 'Invalid URL format'
+        else:
+            # Try to scrape the url
+            try:
+                self.dataset.log('Scraping url: %s' % url)
+                scraped_page = self.simple_scrape_page(url)
+            except TimeoutException as e:
+                result['timestamp'] = int(datetime.datetime.now().timestamp())
+                result['error'] = 'Selenium TimeoutException: %s' % e
+                return result
+
+            # simple_scrape_page returns False if the browser did not load a new page
+            if scraped_page:
+                result['final_url'] = scraped_page.get('final_url')
+                result['body'] = scraped_page.get('page_source')
+                result['subject'] = scraped_page.get('page_title')
+                result['detected_404'] = scraped_page.get('detected_404')
+                result['timestamp'] = int(datetime.datetime.now().timestamp())
+            else:
+                result['timestamp'] = int(datetime.datetime.now().timestamp())
+                result['error'] = 'Unable to scrape url'
+
+        return result
+
 
     @staticmethod
     def validate_query(query, request, user):
@@ -109,4 +155,5 @@ class SearchWithSelenium(SeleniumScraper):
 
         return {
             "query": query.get("query"),
+            "subpages": query.get("subpages", 0)
             }
