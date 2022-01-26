4cfc044697a73abdd7f1528e2b17500a8770cd49
Dale Wahl <dalewahl@gmail.com>
2022-01-26 11:01:08 +0100

    new selenium options

--- backend/abstract/selenium_scraper.py
+++ backend/abstract/selenium_scraper.py
@@ -38,7 +38,7 @@ class SeleniumScraper(Search, metaclass=abc.ABCMeta):
         # try:
         self.driver.get(url)
         # except WebDriverException as e:
-        #     # restart selenium 
+        #     # restart selenium
 
         if self.check_for_movement():
             detected_404 = self.check_for_404(title_404_strings)
@@ -108,8 +108,11 @@ class SeleniumScraper(Search, metaclass=abc.ABCMeta):
         options = Options()
         options.headless = True
         options.add_argument('--headless')
+        options.add_argument("--remote-debugging-port=9222")
         options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-browser-side-navigation")
 
         try:
             self.driver = webdriver.Chrome(options=options)
