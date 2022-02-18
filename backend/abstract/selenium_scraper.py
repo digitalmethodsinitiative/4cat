a039b8788ea4cacbf5466b1ecbcfd193c9922e53
Dale Wahl <dalewahl@gmail.com>
2022-02-18 14:39:09 +0100

    Adding archive specific scraper

--- backend/abstract/selenium_scraper.py
+++ backend/abstract/selenium_scraper.py
@@ -1,10 +1,22 @@
 import abc
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException, SessionNotCreatedException
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+from bs4.element import Comment
 
 from backend.abstract.search import Search
 from common.lib.exceptions import ProcessorException
+import config
+
+if hasattr(config, 'SELENIUM_BROWSER') and hasattr(config, 'SELENIUM_EXECUTABLE_PATH'):
+    if config.SELENIUM_BROWSER == 'chrome':
+        from selenium.webdriver.chrome.options import Options
+    elif config.SELENIUM_BROWSER == 'firefox':
+        from selenium.webdriver.firefox.options import Options
+    else:
+        # TODO raise some sort of error; stop class from being used
+        pass
 
 
 class SeleniumScraper(Search, metaclass=abc.ABCMeta):
@@ -19,7 +31,7 @@ class SeleniumScraper(Search, metaclass=abc.ABCMeta):
     driver = None
     last_scraped_url = None
 
-    def simple_scrape_page(self, url, title_404_strings='default'):
+    def simple_scrape_page(self, url, extract_links=False, title_404_strings='default'):
         """
         Simple helper to scrape url. Returns a dictionary containing basic results from scrape including final_url,
         page_title, and page_source otherwise False if the page did not advance (self.check_for_movement() failed).
@@ -35,26 +47,41 @@ class SeleniumScraper(Search, metaclass=abc.ABCMeta):
                       Returns false if no movement was detected
         """
         self.reset_current_page()
-        # try:
+
         self.driver.get(url)
-        # except WebDriverException as e:
-        #     # restart selenium
 
         if self.check_for_movement():
-            detected_404 = self.check_for_404(title_404_strings)
-            page_title = self.driver.title
-            current_url = self.driver.current_url
-            page_source = self.driver.page_source
 
-            return {
-                    'original_url': url,
-                    'final_url': current_url,
-                    'page_title': page_title,
-                    'page_source': page_source,
-                    'detected_404': detected_404
-                    }
+            results = self.collect_results(extract_links, title_404_strings)
+            return results
+
         else:
             return False
+
+    def collect_results(self, extract_links=False, title_404_strings='default'):
+
+        result = {
+            'original_url': url,
+            'detected_404': self.check_for_404(title_404_strings),
+            'page_title': self.driver.title,
+            'final_url': self.driver.current_url,
+            'page_source': self.driver.page_source,
+            }
+
+        if extract_links:
+            try:
+                links = self.collect_links()
+            except Exception as e:
+                print(e)
+                print('trying to get links again')
+                try:
+                    links = self.collect_links()
+                except Exception as e:
+                    print(e)
+                    links = []
+            result['links'] = links
+
+        return result
 
     def collect_links(self):
         """
@@ -105,6 +132,7 @@ class SeleniumScraper(Search, metaclass=abc.ABCMeta):
         """
         Start a headless browser
         """
+        # TODO review and compare Chrome vs Firefox options 
         options = Options()
         options.headless = True
         options.add_argument('--headless')
@@ -115,7 +143,14 @@ class SeleniumScraper(Search, metaclass=abc.ABCMeta):
         options.add_argument("--disable-browser-side-navigation")
 
         try:
-            self.driver = webdriver.Chrome(options=options)
+            if hasattr(config, 'SELENIUM_BROWSER') and hasattr(config, 'SELENIUM_EXECUTABLE_PATH'):
+                if config.SELENIUM_BROWSER == 'chrome':
+                    self.driver = webdriver.Chrome(executable_path=config.SELENIUM_EXECUTABLE_PATH, options=options)
+                elif config.SELENIUM_BROWSER == 'firefox':
+                    self.driver = webdriver.Firefox(executable_path=config.SELENIUM_EXECUTABLE_PATH, options=options)
+                else:
+                    raise ProcessorException('Selenium Scraper not configured')
+
         except (SessionNotCreatedException, WebDriverException) as e:
             if "binary not found" in str(e):
                 raise ProcessorException("Chromium binary is not available.")
@@ -193,7 +228,7 @@ class SeleniumScraper(Search, metaclass=abc.ABCMeta):
         :param list stop_if_in_title:  List of strings representing possible 404 text
         """
         if stop_if_in_title == 'default':
-            stop_if_in_title = ["page not found", "directory not found", "file not found", "404 not found", "error 404"]
+            stop_if_in_title = ["page not found", "directory not found", "file not found", "404 not found", "error 404", "error page"]
 
         if any(four_oh_four.lower() in self.driver.title.lower() for four_oh_four in stop_if_in_title):
             return True
@@ -210,3 +245,80 @@ class SeleniumScraper(Search, metaclass=abc.ABCMeta):
 
         params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
         return self.driver.execute("send_command", params)
+
+    # Some BeautifulSoup helper functions
+    @staticmethod
+    def scrape_beautiful_text(page_source, beautiful_soup_parser='html.parser'):
+        """takes page source and uses BeautifulSoup to extract a list of all visible text items on page"""
+
+        # Couple of helper functions
+        def tag_visible(element):
+            """checks BeautifulSoup element to see if it is visible on webpage"""
+
+            """original list of elements:
+            ['style', 'script', 'head', 'title', 'meta', '[document]']
+            """
+            if element.parent.name in ['i:pgf', 'svg', 'img', 'script', 'style', 'script', 'head', 'title', 'meta', '[document]']:
+                return False
+            if isinstance(element, Comment):
+                return False
+            return True
+
+        def text_from_html(soup):
+            """take BeautifulSoup entity, finds all text blocks, and checks if block is visible on page"""
+            texts = soup.findAll(text=True)
+            visible_texts = filter(tag_visible, texts)
+            return visible_texts
+
+        def anyalpha(string):
+            """Check for any alpha"""
+            return any([c.isalpha() for c in string])
+
+        # Create soup
+        soup = BeautifulSoup(page_source, beautiful_soup_parser)
+
+        # I may be able to simplify this... just if t?
+        text = [t.strip() for t in text_from_html(soup) if t.strip()]
+        # Only return is there is at least some alphabetic info
+        text = [t for t in text if anyalpha(t)]
+        # Add the page title as the first entry to the text
+        if soup.title:
+            title = soup.title.text.strip()
+            return [title] + text
+        else:
+            return text
+
+    @staticmethod
+    def get_beautiful_links(page_source, domain, beautiful_soup_parser='html.parser'):
+        """
+        takes page_source and creates BeautifulSoup entity and url that was scraped, finds all links,
+        and returns the number of links and a list of all links in tuple of shown text, fixed link,
+        and original link.
+
+        Uses domain to attempt to fix links that are partial.
+        """
+        soup = BeautifulSoup(page_source, beautiful_soup_parser)
+        url_count = 0
+        all_links= soup.findAll('a')
+        links_to_return = []
+        for link in all_links:
+            link_url = link.get('href')
+            original_url = link_url
+            link_text = None
+            if link_url is not None:
+                url_count += 1
+                link_text = link.text
+                # If image in link, find alt text and add to link_text
+                for img in link.findAll('img'):
+                    alt_text = img.get('alt')
+                    if alt_text and type(alt_text) == str:
+                        link_text = ' '.join([link_text, alt_text])
+                # Fix URL if needed
+                if link_url.strip()[:4] == "http":
+                    pass
+                else:
+                    link_url = urljoin(domain, link_url)
+            else:
+                continue
+            links_to_return.append((link_text, link_url.rstrip('/'), original_url))
+        return url_count, links_to_return
---
+++ datasources/web_archive_scraper/search_webpages.py
@@ -0,0 +1,215 @@
+"""
+Twitter keyword search via the Twitter API v2
+"""
+from urllib.parse import urlparse
+import datetime
+import random
+
+from backend.abstract.selenium_scraper import SeleniumScraper
+from selenium.common.exceptions import TimeoutException
+from common.lib.exceptions import QueryParametersException, ProcessorInterruptedException
+from common.lib.helpers import validate_url
+from common.lib.user_input import UserInput
+
+class SearchWebArchiveWithSelenium(SeleniumScraper):
+    """
+    Get HTML page source from Web Archive (web.archive.org) via the Selenium webdriver and Chrome browser
+    """
+    type = "web_archive_scraper-search"  # job ID
+    max_workers = 1
+
+    options = {
+        "intro-1": {
+            "type": UserInput.OPTION_INFO,
+            "help": "This data source uses [Selenium](https://selenium-python.readthedocs.io/) in combination with "
+                    "a [Chrome webdriver](https://sites.google.com/chromium.org/driver/) and Google Chrome for linux "
+                    "to scrape the HTML source code. "
+                    "\n"
+                    "By mimicing a person using an actual browser, this method results in source code that closer "
+                    "resembles the source code an actual user receives when compared with simple HTML requests. It "
+                    "will also render JavaScript that starts as soon as a url is retrieved by a browser. "
+        },
+        "query-info": {
+            "type": UserInput.OPTION_INFO,
+            "help": "Please enter a list of urls one per line."
+        },
+        "query": {
+            "type": UserInput.OPTION_TEXT_LARGE,
+            "help": "List of urls"
+        },
+        "subpages": {
+            "type": UserInput.OPTION_TEXT,
+            "help": "Crawl additional host links/subpages",
+            "min": 0,
+            "max": 5,
+            "default": 0
+        },
+    }
+
+    def get_items(self, query):
+        """
+        Separate and check urls, then loop through each and collects the HTML.
+
+        :param query:
+        :return:
+        """
+        scrape_additional_subpages = self.parameters.get("subpages")
+        urls_to_scrape = [url.strip() for url in self.parameters.get("query", "").split('\n')]
+        urls_to_scrape = [{'url':url, 'num_additional_subpages': scrape_additional_subpages, 'subpage_links':[]} for url in urls_to_scrape]
+
+        # Do not scrape the same site twice
+        scraped_urls = set()
+
+        while urls_to_scrape:
+            # Grab first url
+            url_obj = urls_to_scrape.pop(0)
+            url = url_obj['url']
+            num_additional_subpages = url_obj['num_additional_subpages']
+            result = {
+                "url": url,
+                "final_url": None,
+                "subject": None,
+                "body": None,
+                "visible_text": None,
+                "detected_404": None,
+                "timestamp": None,
+                "error": False,
+            }
+
+            attempts = 0
+            success = False
+            while attempts < 5:
+                attempts += 1
+                scraped_page = scraper.simple_scrape_page(url)
+
+                if scraped_page:
+                    scraped_page['text'] = scraper.scrape_beautiful_text(scraped_page['page_source'])
+                else:
+                    # Hard Fail?
+                    self.dataset.log('Hard fail; no page source on url: %s' % url)
+                    continue
+
+                # Redirects require waiting on Internet Archive
+                if any([redirect_text in text for text in scraped_page['text']]):
+                    # Update last_scraped_url for movement check
+                    scraper.last_scraped_url = scraper.driver.current_url
+                    self.dataset.log('Redirect url: %s' % url)
+                    time.sleep(3)
+                    time_to_wait = 5
+                    try:
+                        while time_to_wait > 0:
+                            if scraper.check_for_movement():
+                                scraped_page = scraper.collect_results()
+                                if scraped_page:
+                                    scraped_page['text'] = scraper.scrape_beautiful_text(scraped_page['page_source'])
+                                    break
+                                else:
+                                    raise Exception('No page source on url: %s' % url)
+                            else:
+                                time_to_wait -= 1
+                                time.sleep(1)
+                    except Exception as e:
+                        # most likely something in Selenium "went stale"
+                        self.dataset.log('Redirect url unable to be scraped: %s' % url)
+                        self.dataset.log(e)
+                        scraped_page['error'] = e
+                        break
+
+                if any([bad_response_text in text for text in scraped_page['text']]):
+                    # Bad response from Internet Archive
+                    self.dataset.log('Internet Archive bad requests on url: %s' % url)
+                    # Try again; Internet Achive is mean
+                    time.sleep(1)
+                    continue
+                elif scraped_page['detected_404']:
+                    self.dataset.log('404 detected on url: %s' % url)
+                    scraped_page['error'] = '404 detected on url'
+                    break
+                else:
+                    success = True
+                    scraped_urls.add(url)
+                    break
+
+
+            if success:
+                self.dataset.log('Collected: %s' % url)
+                # Check is additional subpages need to be scraped
+                if num_additional_subpages > 0:
+                    # Check if any are available
+                    if not url_obj['subpage_links']:
+                        # If no, collect links
+                        domain = urlparse(url).scheme + '://' + urlparse(url).netloc
+                        num_of_links, links = scraper.get_beautiful_links(scraped_page['page_source'], domain)
+                        # Randomize links (else we end up with mostly menu items at the top of webpages)
+                        random.shuffle(links)
+                    else:
+                        links = url_obj['subpage_links']
+
+                    # Find the first link that has not been previously scraped
+                    while links:
+                        link = links.pop(0)
+                        if link[1] not in scraped_urls:
+                            # Add it to be scraped next
+                            urls_to_scrape.insert(0, {
+                                'url': link[1],
+                                'num_additional_subpages': num_additional_subpages - 1, # Make sure to request less additional pages
+                                'subpage_links':links,
+                            })
+                            break
+
+                # Update result and yield it
+                result['final_url'] = scraped_page.get('final_url')
+                result['body'] = scraped_page.get('page_source')
+                result['subject'] = scraped_page.get('page_title')
+                result['visible_text'] = scraped_page.get('text')
+                result['detected_404'] = scraped_page.get('detected_404')
+                result['timestamp'] = int(datetime.datetime.now().timestamp())
+                result['error'] = scraped_page.get('error') # This should be None...
+                yield result
+            else:
+                # Still need subpages?
+                if num_additional_subpages > 0:
+                    # Add the next one if it exists
+                    links = url_obj['subpage_links']
+                    while links:
+                        link = links.pop(0)
+                        if link[1] not in scraped_urls:
+                            # Add it to be scraped next
+                            urls_to_scrape.insert(0, {
+                                'url': link[1],
+                                'num_additional_subpages': num_additional_subpages - 1, # Make sure to request less additional pages
+                                'subpage_links':links,
+                            })
+                            break
+                # Unsure if we should return ALL failures, but certainly the originally supplied urls
+                result['timestamp'] = int(datetime.datetime.now().timestamp())
+                result['error'] = scraped_page.get('error')
+                yield result
+
+    @staticmethod
+    def validate_query(query, request, user):
+        """
+        Validate input for a dataset query on the Selenium Webpage Scraper.
+
+        Will raise a QueryParametersException if invalid parameters are
+        encountered. Parameters are additionally sanitised.
+
+        :param dict query:  Query parameters, from client-side.
+        :param request:  Flask request
+        :param User user:  User object of user who has submitted the query
+        :return dict:  Safe query parameters
+        """
+
+        # this is the bare minimum, else we can't narrow down the full data set
+        if not query.get("query", None):
+            raise QueryParametersException("Please provide a List of urls.")
+
+        urls = [url.strip() for url in query.get("query", "").split('\n')]
+        preprocessed_urls = [url for url in urls if validate_url(url)]
+        if not preprocessed_urls:
+            raise QueryParametersException("No Urls detected!")
+
+        return {
+            "query": query.get("query"),
+            "subpages": query.get("subpages", 0)
+            }
