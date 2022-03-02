c232084f6d80b17be2af936dfb4e64b934f468ac
Dale Wahl <dalewahl@gmail.com>
2022-03-02 13:45:51 +0100

    remove create first admin user from docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -13,16 +13,14 @@ trap exit_backend INT TERM
 python3 docker/docker_setup.py
 
 echo "Waiting for postgres..."
-
 while ! nc -z db 5432; do
   sleep 0.1
 done
-
 echo "PostgreSQL started"
 
-# Check if DB exists and creates admin is it does not
-user_created=false
+# Check if DB exists
 # Check for "jobs" table
+#TODO: move db creation to Dockerfile
 if [[ `psql --host=db --port=5432 --user=$POSTGRES_USER --dbname=$POSTGRES_DB -tAc "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'jobs')"` = 't' ]]; then
   # Table already exists
   echo "Database already created"
@@ -30,35 +28,10 @@ else
   echo 'Creating database'
   # Seed DB
   cd /usr/src/app && psql --host=db --port=5432 --user=$POSTGRES_USER --dbname=$POSTGRES_DB < backend/database.sql
-
-  echo 'Generating admin user'
-  # Generate password for admin user
-  admin_password=$(openssl rand -base64 12)
-  # Run python script to create new user
-  python3 /usr/src/app/helper-scripts/create_user.py -u admin -e -p "$admin_password" -a
-  echo 'Your admin username:' >> docker/shared/login.txt
-  echo 'admin' >> docker/shared/login.txt
-  echo 'Your admin password:' >> docker/shared/login.txt
-  echo "$admin_password" >> docker/shared/login.txt
-  user_created=true
-  # Make admin an actual admin user
-  psql --host=db --port=5432 --user=$POSTGRES_USER --dbname=$POSTGRES_DB -tAc "UPDATE users SET is_admin='t' WHERE name='admin'";
 fi
 
 echo 'Starting app'
 cd /usr/src/app
-
-# Inform user of admin password if created
-if [ $user_created = true ] ; then
-  echo '4CAT account information:'
-  echo 'Your admin username:'
-  echo 'admin'
-  echo 'Your admin password:'
-  echo "$admin_password"
-  echo 'This information has been saved in your Docker 4cat_backend container'
-  echo 'as login.txt'
-  echo ''
-fi
 
 echo "4CAT is accessible at:"
 echo "http://$SERVER_NAME:$PUBLIC_PORT"
