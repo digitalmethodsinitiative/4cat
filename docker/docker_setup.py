ad772fa4c805f0669fc325e7a35ffe37e85f5c9f
Dale Wahl <dalewahl@gmail.com>
2022-03-23 16:09:39 +0100

    strings for config parser

--- docker/docker_setup.py
+++ docker/docker_setup.py
@@ -18,7 +18,7 @@ if os.path.exists(DOCKER_CONFIG_FILE):
     docker_config['DATABASE']['db_password'] = os.environ['POSTGRES_PASSWORD']
 
     public_port = os.environ['PUBLIC_PORT']
-    docker_config['SERVER']['public_port'] = public_port
+    docker_config['SERVER']['public_port'] = str(public_port)
 
     # Save config file
     with open(DOCKER_CONFIG_FILE, 'w') as configfile:
@@ -52,7 +52,7 @@ docker_config['DATABASE']['db_port'] = '5432' # port exposed by postgres image
 
 docker_config.add_section('API')
 docker_config['API']['api_host'] = '4cat_backend' # backend container_name in docker-compose.py
-docker_config['API']['api_port'] = 4444 # backend port exposed in docker-compose.py
+docker_config['API']['api_port'] = '4444' # backend port exposed in docker-compose.py
 
 # These could perhaps be defined elsewhere
 docker_config.add_section('PATHS')
