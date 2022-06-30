5bedd64413fb533efae6f128c9bd6c608e273b9b
Stijn Peeters <stijn.peeters@uva.nl>
2022-06-30 21:16:17 +0200

    Circle pack layout option, community detection, colour by attribute

---
+++ webtool/static/js/color.js
@@ -0,0 +1,84 @@
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and v in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSV representation
+ */
+export function rgb2hsv(r, g, b) {
+    r /= 255, g /= 255, b /= 255;
+
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, v = max;
+
+    var d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if (max == min) {
+        h = 0; // achromatic
+    } else {
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+
+        h /= 6;
+    }
+
+    return [h, s, v];
+}
+
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+export function hsv2rgb(h, s, v) {
+    var r, g, b;
+
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+
+    switch (i % 6) {
+        case 0:
+            r = v, g = t, b = p;
+            break;
+        case 1:
+            r = q, g = v, b = p;
+            break;
+        case 2:
+            r = p, g = v, b = t;
+            break;
+        case 3:
+            r = p, g = q, b = v;
+            break;
+        case 4:
+            r = t, g = p, b = v;
+            break;
+        case 5:
+            r = v, g = p, b = q;
+            break;
+    }
+
+    return [r * 255, g * 255, b * 255];
+}
--- webtool/static/js/sigma_network.js
+++ webtool/static/js/sigma_network.js
@@ -1,10 +1,46 @@
+import * as color from './color.js';
+import {hsv2rgb, rgb2hsv} from "./color.js";
+
 const forceatlas2 = graphologyLibrary.layoutForceAtlas2;
+const circlepack = graphologyLibrary.layout.circlepack;
 const fa2worker = graphologyLibrary.FA2Layout;
+const palette = ["#0274e1", "#8fbd35", "#fe80ec", "#00aa4e", "#820017", "#00760e", "#c14218", "#ffac42"]
+const gradient = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0].map((fraction) => {
+    let base = rgb2hsv(206, 27, 40);  // 4cat red
+    base[1] = fraction;
+    return 'rgb(' + hsv2rgb(...base).map(v => parseInt(v)).join(', ') + ')';
+});
+console.log(gradient);
 
 const sigma_graph = {
     graph: null,
     sigma: null,
     stats: {},
+    top_communities: null,
+    attribute_map: {},
+
+    map_attribute: function (attribute) {
+        let values = {};
+        sigma_graph.graph.nodes().map((node) => {
+            let value = sigma_graph.graph.getNodeAttribute(node, attribute);
+            if (!(value in values)) {
+                values[value] = 0;
+            }
+            values[value] += 1;
+        });
+
+        // I hate javascript
+        let value_count = [...Object.values(values)];
+        value_count = value_count.sort((a, b) => a - b).reverse().slice(0, palette.length);
+        let top_values = []
+        let min_size = value_count.pop();
+        for (let num in values) {
+            if (values[num] >= min_size) {
+                top_values.push(String(num));
+            }
+        }
+        sigma_graph.attribute_map[attribute] = top_values.slice(0, palette.length);
+    },
 
     init: function () {
         let container = document.querySelector('#graph-container');
@@ -17,9 +53,13 @@ const sigma_graph = {
                 sigma_graph.graph = graphologyLibrary.gexf.parse(graphology.Graph, gexf);
 
                 graphologyLibrary.layout.random.assign(sigma_graph.graph);
-                let degrees = sigma_graph.graph.nodes().map(function(n) { return sigma_graph.graph.degree(n); });
+                let degrees = sigma_graph.graph.nodes().map(function (n) {
+                    return sigma_graph.graph.degree(n);
+                });
                 sigma_graph.stats.max_degree = Math.max(...degrees);
                 sigma_graph.stats.min_degree = Math.min(...degrees);
+                graphologyLibrary.communitiesLouvain.assign(sigma_graph.graph, {resolution: 1});
+                this.map_attribute('community');
 
                 sigma_graph.sigma = new Sigma(sigma_graph.graph, container, {
                     minCameraRatio: 0.1,
@@ -33,27 +73,41 @@ const sigma_graph = {
                             weight = settings.labelWeight,
                             color = settings.labelColor.color;
 
-                        if(document.querySelector('#label-size-type').checked) {
+                        if (document.querySelector('#label-size-type').checked) {
                             let range = size * 3;
                             let ratio = (data.degree - sigma_graph.stats.min_degree) / (sigma_graph.stats.max_degree - sigma_graph.stats.min_degree);
                             size += (ratio * range);
                         }
 
-                        context.font = `${weight} ${size}px ${font}`;
-                        const width = context.measureText(data.label).width + 8;
-
-                        context.fillStyle = "#ffffffcc";
-                        context.fillRect(data.x + data.size, data.y + size / 3 - 15, width, 20);
-
+                        context.font = `${weight} ${size}px sans-serif`;
                         context.fillStyle = color;
                         context.fillText(data.label, data.x + data.size + 3, data.y + size / 3);
                     },
-                    nodeReducer: function(node, data) {
+                    nodeReducer: function (node, data) {
+                        let mode = document.getElementById('node-colouring').value;
                         let threshold = parseFloat(document.querySelector('#min-degree').value);
                         let degree = sigma_graph.graph.degree(node);
                         threshold = sigma_graph.stats.min_degree + ((sigma_graph.stats.max_degree - sigma_graph.stats.min_degree) * threshold);
-                        if(degree < threshold) {
+                        if (degree < threshold) {
                             data.color = 'rgba(0, 0, 0, 255)';
+                        } else if (mode.indexOf('attribute-') === 0) {
+                            let attribute = mode.split('attribute-').pop();
+                            if (!(attribute in sigma_graph.attribute_map)) {
+                                sigma_graph.map_attribute(attribute);
+                            }
+                            let value = String(data[attribute]);
+                            let index = sigma_graph.attribute_map[attribute].indexOf(value);
+                            if (index !== -1) {
+                                console.log(palette[value])
+                                data.color = palette[index];
+                            } else {
+                                data.color = 'rgb(192, 192, 192)';
+                            }
+                        } else if (mode === 'degree') {
+                            let ratio = (degree - sigma_graph.stats.min_degree) / (sigma_graph.stats.max_degree - sigma_graph.stats.min_degree);
+                            let index = Math.round(ratio * gradient.length);
+                            console.log(gradient[index]);
+                            data.color = gradient[index];
                         }
                         data.degree = degree;
                         return data;
@@ -65,26 +119,46 @@ const sigma_graph = {
     },
 
     register_handlers: function () {
-        document.querySelector('#start-force').addEventListener('click', sigma_graph.fa2.toggle);
+        document.getElementById('start-layout').addEventListener('click', sigma_graph.layout.toggle);
         document.querySelectorAll('#network-settings input').forEach(function (control) {
             control.addEventListener('input', sigma_graph.fa2.update_settings);
         })
 
-        document.querySelectorAll('#visual-settings input').forEach(function (control) {
+        document.getElementById('graph-layout').addEventListener('input', sigma_graph.layout.change);
+
+        document.querySelectorAll('#visual-settings input, #visual-settings select').forEach(function (control) {
             control.addEventListener('input', sigma_graph.update_visual_settings);
         })
+
+        // figure out categories we can colour by
+        let extra_categories = [];
+        sigma_graph.graph.forEachNode((node) => {
+            let attributes = sigma_graph.graph.getNodeAttributes(node);
+            for (let attribute in attributes) {
+                if (['label', 'x', 'y', 'community'].indexOf(attribute) === -1 && extra_categories.indexOf(attribute) === -1) {
+                    extra_categories.push(attribute);
+                }
+            }
+        });
+        extra_categories.forEach((category) => {
+            let option = document.createElement('option');
+            option.setAttribute('value', 'attribute-' + category);
+            option.innerText = 'Node attribute: ' + category;
+            document.getElementById('node-colouring').appendChild(option);
+        })
+
 
         sigma_graph.update_visual_settings();
     },
 
-    update_visual_settings: function() {
-        sigma_graph.sigma.setSetting("labelRenderedSizeThreshold", document.querySelector('#label-threshold').value);
+    update_visual_settings: function () {
+        sigma_graph.sigma.setSetting("labelRenderedSizeThreshold", document.getElementById('label-threshold').value);
         //sigma_graph.sigma.setSetting("labelSize", document.querySelector('#label-size').value);
-        sigma_graph.sigma.setSetting("labelColor", { color: document.querySelector('#label-colour').value});
+        sigma_graph.sigma.setSetting("labelColor", {color: document.getElementById('label-colour').value});
 
         // node sizes
-        let min_size = parseFloat(document.querySelector('#min-node-size').value);
-        let max_size = parseFloat(document.querySelector('#max-node-size').value);
+        let min_size = parseFloat(document.getElementById('min-node-size').value);
+        let max_size = parseFloat(document.getElementById('max-node-size').value);
         sigma_graph.graph.forEachNode((node) => {
             let degree = sigma_graph.graph.degree(node);
             let range = max_size - min_size;
@@ -93,20 +167,65 @@ const sigma_graph = {
             sigma_graph.graph.setNodeAttribute(node, "size", size,);
         });
 
-        let show_edges = document.querySelector('#show-edges').checked;
-        let edge_colour = show_edges ? document.querySelector('#edge-colour').value : 'rgba(0, 0, 0, 255)';
+        let show_edges = document.getElementById('show-edges').checked;
+        let edge_colour = show_edges ? document.getElementById('edge-colour').value : 'rgba(0, 0, 0, 255)';
         sigma_graph.sigma.setSetting("defaultEdgeColor", edge_colour);
 
-        let show_nodes = document.querySelector('#show-nodes').checked;
-        let node_color = show_nodes ? document.querySelector('#node-colour').value : 'rgba(0, 0, 0, 255)';
+        let show_nodes = document.getElementById('show-nodes').checked;
+        let node_color = show_nodes ? document.getElementById('node-colour').value : 'rgba(0, 0, 0, 255)';
         sigma_graph.sigma.setSetting("defaultNodeColor", node_color);
 
         sigma_graph.sigma.refresh();
     },
 
+    layout: {
+        layout: 'fa2',
+
+        toggle: function () {
+            sigma_graph[sigma_graph.layout.layout].toggle();
+        },
+
+        change: function () {
+            let choice = document.getElementById('graph-layout').value;
+            if (choice !== sigma_graph.layout.layout) {
+                if (sigma_graph[sigma_graph.layout.layout].hasOwnProperty('uninit')) {
+                    sigma_graph[sigma_graph.layout.layout].uninit();
+                }
+                document.querySelectorAll('.layout-parameters').forEach((element) => {
+                    if (element.getAttribute('data-layout') !== choice) {
+                        element.style.display = 'none';
+                    } else {
+                        element.style.display = 'block';
+                    }
+                });
+                sigma_graph.layout.layout = choice;
+                sigma_graph[choice].init();
+            }
+
+        }
+    },
+
+    noverlap: {},
+
+    circlepack: {
+        have_init: false,
+        continuous: false,
+
+        init: function () {
+            document.querySelector('#start-layout span').innerHTML = '<i class="fa fa-shapes"></i> Apply Circle Pack';
+        },
+
+        toggle: function () {
+            circlepack.assign(sigma_graph.graph, {
+                hierarchyAttributes: ['degree', 'community']
+            });
+        }
+    },
+
     fa2: {
         have_init: false,
         layout: null,
+        continuous: true,
 
         init: function () {
             const settings = forceatlas2.inferSettings(sigma_graph.graph);
@@ -114,6 +233,7 @@ const sigma_graph = {
                 settings: sigma_graph.fa2.get_settings()
             });
             sigma_graph.fa2.have_init = true;
+            document.querySelector('#start-layout span').innerHTML = '<i class="fa fa-play"></i> Start ForceAtlas2';
         },
 
         start: function () {
@@ -122,12 +242,12 @@ const sigma_graph = {
             }
 
             sigma_graph.fa2.layout.start();
-            document.querySelector('#start-force span').innerHTML = '<i class="fa fa-sync fa-spin"></i> Stop ForceAtlas2';
+            document.querySelector('#start-layout span').innerHTML = '<i class="fa fa-sync fa-spin"></i> Stop ForceAtlas2';
         },
 
         stop: function () {
             sigma_graph.fa2.layout.stop();
-            document.querySelector('#start-force span').innerHTML = '<i class="fa fa-play"></i> Start ForceAtlas2';
+            document.querySelector('#start-layout span').innerHTML = '<i class="fa fa-play"></i> Start ForceAtlas2';
         },
 
         toggle: function () {
@@ -138,6 +258,13 @@ const sigma_graph = {
             }
         },
 
+        uninit: function () {
+            if (sigma_graph.fa2.have_init) {
+                sigma_graph.fa2.layout.kill();
+                sigma_graph.fa2.have_init = false;
+            }
+        },
+
         update_settings() {
             if (!sigma_graph.fa2.have_init) {
                 sigma_graph.fa2.init();
@@ -145,8 +272,7 @@ const sigma_graph = {
 
             let is_running = sigma_graph.fa2.layout.isRunning();
 
-            sigma_graph.fa2.layout.kill();
-            sigma_graph.fa2.have_init = false;
+            sigma_graph.fa2.uninit();
 
             if (is_running) {
                 sigma_graph.fa2.start();
@@ -157,16 +283,16 @@ const sigma_graph = {
 
         get_settings: function () {
             return {
-                adjustSizes: document.querySelector('#adjust-sizes').checked,
-                barnesHutOptimize: document.querySelector('#barnes-hut-optimise').checked,
-                barnesHutTheta: parseFloat(document.querySelector('#barnes-hut-theta').value),
-                edgeWeightInfluence: parseFloat(document.querySelector('#edge-weight-influence').value),
-                gravity: parseFloat(document.querySelector('#gravity').value),
-                linLogMode: document.querySelector('#linlog-mode').checked,
-                outboundAttractionDistribution: document.querySelector('#outbound-attraction-distribution').checked,
-                scalingRatio: parseFloat(document.querySelector('#scaling-ratio').value),
-                slowDown: parseFloat(document.querySelector('#slow-down').value),
-                strongGravityMode: document.querySelector('#strong-gravity').checked
+                adjustSizes: document.getElementById('adjust-sizes').checked,
+                barnesHutOptimize: document.getElementById('barnes-hut-optimise').checked,
+                barnesHutTheta: parseFloat(document.getElementById('barnes-hut-theta').value),
+                edgeWeightInfluence: parseFloat(document.getElementById('edge-weight-influence').value),
+                gravity: parseFloat(document.getElementById('gravity').value),
+                linLogMode: document.getElementById('linlog-mode').checked,
+                outboundAttractionDistribution: document.getElementById('outbound-attraction-distribution').checked,
+                scalingRatio: parseFloat(document.getElementById('scaling-ratio').value),
+                slowDown: parseFloat(document.getElementById('slow-down').value),
+                strongGravityMode: document.getElementById('strong-gravity').checked
             }
         }
     }
