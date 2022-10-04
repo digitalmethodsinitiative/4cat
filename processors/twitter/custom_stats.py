3b64b5a0b7a14decd6b5424c1fb3ee042a27d92f
Dale Wahl <dalewahl@gmail.com>
2022-10-04 12:55:22 +0200

    rearrange processor order (processors are sorted by type)

--- processors/twitter/custom_stats.py
+++ processors/twitter/custom_stats.py
@@ -15,7 +15,7 @@ class TwitterCustomStats(TwitterStatsBase):
     """
     Collect Twitter statistics. Build to emulate TCAT statistic.
     """
-    type = "twitter-custom-stats"  # job type ID
+    type = "twitter-1-custom-stats"  # job type ID
     category = "Twitter Analysis"  # category
     title = "Custom Statistics"  # title displayed in UI
     description = "Group tweets by category and count tweets per timeframe to collect aggregate group statistics.\nFor retweets and quotes, hashtags, mentions, URLs, and images from the original tweet are included in the retweet/quote. Data on public metrics (e.g., number of retweets or likes of tweets) are as of the time the data was collected."  # description displayed in UI
--- processors/twitter/twitter_stats.py
+++ processors/twitter/twitter_stats.py
@@ -14,7 +14,7 @@ class TwitterStats(TwitterStatsBase):
     """
     Collect Twitter statistics. Built to emulate TCAT statistic.
     """
-    type = "twitter-stats"  # job type ID
+    type = "twitter-0-stats"  # job type ID
     category = "Twitter Analysis"  # category
     title = "Twitter Statistics"  # title displayed in UI
     description = "Contains the number of tweets, number of tweets with links, number of tweets with hashtags, number of tweets with mentions, number of retweets, and number of replies"  # description displayed in UI
