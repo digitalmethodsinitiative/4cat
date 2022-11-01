d8b44a9cadd8bfe6c0f0145a5e4695d2247db44e
Dale Wahl <dalewahl@gmail.com>
2022-11-01 11:01:15 +0100

    Inform of LDA vis issue, add references, add option for other reductions

--- processors/text-analysis/visualise_lda.py
+++ processors/text-analysis/visualise_lda.py
@@ -8,7 +8,6 @@ from common.lib.exceptions import ProcessorInterruptedException
 
 import pickle
 import pyLDAvis.sklearn
-from bs4 import BeautifulSoup
 
 __author__ = ["Dale Wahl"]
 __credits__ = ["Dale Wahl"]
@@ -26,6 +25,25 @@ class VisualiseLDA(BasicProcessor):
     description = "Creates a visualisation of the chosen LDA model allowing exploration of the various words in each topic."  # description displayed in UI
     extension = "html"  # extension of result file, used internally and in UI
 
+    references = [
+        """Sievert, Carson, Shirley, Kenneth 2014. "LDAvis: A method for visualizing and interpreting topics" http://nlp.stanford.edu/events/illvi2014/papers/sievert-illvi2014.pdf""",
+    ]
+
+    options = {
+                  "dimensionality": {
+                      # https://pyldavis.readthedocs.io/en/latest/_modules/pyLDAvis/_prepare.html
+                      "type": UserInput.OPTION_CHOICE,
+                      "options": {
+                          "pcoa": "Principal Coordinate Analysis (aka Classical Multidimensional Scaling)",
+                          "mmds": "Metric Multidimensional Scaling",
+                          "tsne": "T-distributed Stochastic Neighbor Embedding",
+                      },
+                      "default": "pcoa",
+                      "help": "Multidimensionality reduction method",
+                      "tooltip": "Please see references for additional information."
+                  },
+    }
+
     @classmethod
     def is_compatible_with(cls, module=None):
         """
@@ -39,6 +57,7 @@ class VisualiseLDA(BasicProcessor):
         """
         Extracts topics per model and top associated words
         """
+        mds = self.parameters.get("dimensionality")
         self.dataset.update_status("Unpacking topic models")
         staging_area = self.unpack_archive_contents(self.source_file)
         results = []
@@ -63,9 +82,19 @@ class VisualiseLDA(BasicProcessor):
             with model_file.with_suffix(".vectoriser").open("rb") as infile:
                 vectoriser = pickle.load(infile)
 
-            LDAvis_prepared = pyLDAvis.sklearn.prepare(model, vectors, vectoriser, sort_topics=False)
+            LDAvis_prepared = pyLDAvis.sklearn.prepare(model, vectors, vectoriser, mds=mds, sort_topics=False)
             self.dataset.update_status("Saving LDA visualisation for '%s'" % model_file.stem)
-            pyLDAvis.save_html(LDAvis_prepared, str(staging_area.joinpath('%s.html' % model_file.stem)))
+            try:
+                pyLDAvis.save_html(LDAvis_prepared, str(staging_area.joinpath('%s.html' % model_file.stem)))
+            except TypeError as e:
+                if 'Object of type complex is not JSON serializable' in e:
+                    # Known issue: model contains complex (i.e. imaginary numbers). Possibly another type of dimensionality reduction can fix the issue or reducing number of topics.
+                    # https://github.com/bmabey/pyLDAvis/issues/69
+                    self.dataset.update_status("Data incompatible with dimensionality reduction method (appears to contain imaginary numbers). Try a different reduction method or decrease number of topics.", is_final=True)
+                    self.dataset.finish(0)
+                    return
+                else:
+                    raise e
 
         # Write HTML file
         with self.dataset.get_results_path().open("w", encoding="utf-8") as output_file:
