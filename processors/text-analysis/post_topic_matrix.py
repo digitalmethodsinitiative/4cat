e50d0eff6a2ff0ca4a55212ab8ef8ad89279c707
Dale Wahl <dalewahl@gmail.com>
2022-11-16 12:42:13 +0100

    add additional columns to post_topic_matrix; add top topic column

--- processors/text-analysis/post_topic_matrix.py
+++ processors/text-analysis/post_topic_matrix.py
@@ -22,7 +22,7 @@ class TopicModelWordExtractor(BasicProcessor):
     """
     type = "document_topic_matrix"  # job type ID
     category = "Text analysis"  # category
-    title = "Post/Topic matrix"  # title displayed in UI
+    title = "Post/Topic matrix (predict which posts belong to which topics)"  # title displayed in UI
     description = "Uses the LDA model to predict to which topic each item or sentence belongs and creates a CSV file showing this information. Each line represents one 'document'; if tokens are grouped per 'item' and only one column is used (e.g. only the 'body' column), there is one row per post/item, otherwise a post may be represented by multiple rows (for each sentence and/or column used)."  # description displayed in UI
     extension = "csv"  # extension of result file, used internally and in UI
 
@@ -32,8 +32,43 @@ class TopicModelWordExtractor(BasicProcessor):
             "default": False,
             "help": "Include top 5 words in topic header",
             "tooltip": 'This may be useful in better understanding your topics.',
-        }
+        },
+        "columns": {
+            "type": UserInput.OPTION_TEXT,
+            "help": "Extra column(s) to include from original data",
+            "default": "id",
+            "tooltip": "Note: 'id', 'thread_id', 'timestamp', 'author', 'body' and any tokenized columns are always included."
+        },
     }
+
+    @classmethod
+    def get_options(cls, parent_dataset=None, user=None):
+        """
+        Get processor options
+
+        This method by default returns the class's "options" attribute, or an
+        empty dictionary. It can be redefined by processors that need more
+        fine-grained options, e.g. in cases where the availability of options
+        is partially determined by the parent dataset's parameters.
+
+        :param DataSet parent_dataset:  An object representing the dataset that
+        the processor would be run on
+        :param User user:  Flask user the options will be displayed for, in
+        case they are requested for display in the 4CAT web interface. This can
+        be used to show some options only to privileges users.
+        """
+        options = cls.options
+
+        if parent_dataset:
+            top_dataset = parent_dataset.top_parent()
+            if top_dataset.get_columns():
+                columns = top_dataset.get_columns()
+                options["columns"]["type"] = UserInput.OPTION_MULTI
+                options["columns"]["inline"] = True
+                options["columns"]["options"] = {v: v for v in columns}
+                options["columns"]["default"] = ["body"]
+
+        return options
 
     @classmethod
     def is_compatible_with(cls, module=None):
@@ -74,8 +109,9 @@ class TopicModelWordExtractor(BasicProcessor):
         model_metadata_parameters = model_metadata.pop('parameters')
 
         # Collect column names of matrix
-        post_column_names = list(set(['id', 'thread_id', 'timestamp', 'author', 'body'] + token_metadata_parameters.get('columns')))
+        post_column_names = list(set(['id', 'thread_id', 'timestamp', 'author', 'body'] + self.parameters.get('columns', []) + token_metadata_parameters.get('columns')))
         model_column_names = ['post_id', 'document_id', 'interval']
+
         # Check if multiple documents exist per post/item and add 'document' column if needed
         if token_metadata_parameters.get('grouped_by') != 'item' or len(token_metadata_parameters.get('columns')) > 1:
             model_column_names.append('document')
@@ -129,9 +165,9 @@ class TopicModelWordExtractor(BasicProcessor):
                         combined_data['document'] = token_data['documents'][str(document_number)]
 
                     doc_predictions = model_data['predictions'][str(document_number)]
+                    combined_data['top_topic(s)'] = ', '.join([str(int(key) + 1) for key, value in doc_predictions.items() if value == max(doc_predictions.values())])  # add one to topic key here as well
                     for i, topic in enumerate(related_topic_columns):
                         combined_data[topic] = doc_predictions[str(i)]
-
 
                     writer.writerow(combined_data)
                     index += 1
