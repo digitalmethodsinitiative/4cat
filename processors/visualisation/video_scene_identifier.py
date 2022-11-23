f989cc34dd537ccc05405fddbf0ce2b0d4126bf6
Dale Wahl <dalewahl@gmail.com>
2022-11-23 12:29:32 +0100

    poor cp job

--- processors/visualisation/video_scene_identifier.py
+++ processors/visualisation/video_scene_identifier.py
@@ -213,18 +213,22 @@ class VideoSceneDetector(BasicProcessor):
 			self.dataset.update_status("Saving video scene results")
 			for url, video_data in video_metadata.items():
 				if video_data.get('success'):
-					# List types are not super fun for CSV
-					if 'post_ids' in video_data:
-						video_data['post_ids'] = ','.join(video_data['post_ids'])
+					files = video_data.get('files') if 'files' in video_data else [{"filename": video_data.get("filename"), "success":True}]
+					for file in files:
+						if not file.get("success"):
+							continue
+						# List types are not super fun for CSV
+						if 'post_ids' in video_data:
+							video_data['post_ids'] = ','.join(video_data['post_ids'])
 
-					for i, scene in enumerate(collected_scenes[video_data.get('filename')]):
-						rows.append({
-							'id': video_data.get('filename') + '_scene_' + str(i+1),  # best if all datasets have unique identifier
-							'url': url,
-							**scene,
-							**video_data,
-						})
-						num_posts += 1
+						for i, scene in enumerate(collected_scenes[file.get('filename')]):
+							rows.append({
+								'id': file.get('filename') + '_scene_' + str(i+1),  # best if all datasets have unique identifier
+								'url': url,
+								**scene,
+								**video_data,
+							})
+							num_posts += 1
 
 		self.dataset.update_status(
 			'Detected %i scenes in %i videos' % (num_posts, processed_videos))
