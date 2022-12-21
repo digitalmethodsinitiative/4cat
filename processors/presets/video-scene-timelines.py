1cf32386edb82610191894d9489c45ee5973060c
Stijn Peeters <stijn.peeters@uva.nl>
2022-12-21 14:43:08 +0100

    Add 'scene timelines' preset

---
+++ processors/presets/video-scene-timelines.py
@@ -0,0 +1,58 @@
+"""
+Extract neologisms
+"""
+import shutil
+
+import common.config_manager as config
+from backend.abstract.preset import ProcessorPreset
+
+
+class VideoSceneTimelineCreator(ProcessorPreset):
+    """
+    Run processor pipeline to create video scene stripes
+    """
+    type = "preset-scene-timelines"  # job type ID
+    category = "Visual"  # category. 'Combined processors' are always listed first in the UI.
+    title = "Create scene-by-scene timelines"  # title displayed in UI
+    description = "Creates a 'timeline' for each video, a horizontal collage of sequential frames. Each 'scene' in " \
+                  "the  video is visualised as a single frame. Scenes are detected by an algorithm. The timelines for " \
+                  "all videos are then stacked vertically and rendered as a single SVG file."  # description displayed in UI
+    extension = "svg"
+
+    @classmethod
+    def is_compatible_with(cls, module=None):
+        """
+        Determine compatibility
+
+        Compatible with 'Extract video frames'. Can in principle run on
+        anything that stores related images in separate folders in a zip
+        archive. Each folder will be rendered as a separate timeline.
+
+        :param str module:  Module ID to determine compatibility with
+        :return bool:
+        """
+        return module.type in ["video-downloader"] and \
+               config.get("video_downloader.ffmpeg-path") and \
+               shutil.which(config.get("video_downloader.ffmpeg-path"))
+
+    def get_processor_pipeline(self):
+        """
+        This queues a series of post-processors to visualise videos.
+        """
+
+        pipeline = [
+            # first, detect scenes (with the default settings)
+            {
+                "type": "video-scene-detector"
+            },
+            # then, extract frames per scene
+            {
+                "type": "video-scene-frames"
+            },
+            # and finally, render to a combined collage
+            {
+                "type": "video-timelines"
+            }
+        ]
+
+        return pipeline
