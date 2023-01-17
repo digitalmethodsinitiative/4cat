ecf30a9359020f473cb983bc58af51bd698fe501
Dale Wahl <dalewahl@gmail.com>
2023-01-17 10:45:30 +0100

    video hash as steps/preset
    should make the process more transparent for users. groundwork for visual processor to come...

--- processors/visualisation/video_hasher.py
+++ processors/visualisation/video_hasher.py
@@ -4,20 +4,68 @@ Hashes videos so they can be compared to others.
 This processor also requires ffmpeg to be installed in 4CAT's backend
 https://ffmpeg.org/
 """
+import csv
 import json
 import shutil
+import zipfile
 
 from videohash import VideoHash
 from videohash.exceptions import FFmpegNotFound
 
 from backend.abstract.processor import BasicProcessor
+from backend.abstract.preset import ProcessorPreset
 from common.lib.exceptions import ProcessorInterruptedException
 from common.lib.user_input import UserInput
+import common.config_manager as config
 
 __author__ = "Dale Wahl"
 __credits__ = ["Dale Wahl"]
 __maintainer__ = "Dale Wahl"
 __email__ = "4cat@oilab.eu"
+
+
+class VideoHasherPreset(ProcessorPreset):
+    """
+    Run processor pipeline to create video hashes
+    """
+    type = "preset-video-hashes"  # job type ID
+    category = "Visual"  # category. 'Combined processors' are always listed first in the UI.
+    title = "Create Video hashes to identify near duplicate videos"  # title displayed in UI
+    description = "Creates video hashes (64 bits/identifiers) to identify near duplicate videos in a dataset based on hash similarity. Uses video only (no audio; see references). This process can take a long time depending on video length, amount, and frames per second."
+    extension = "csv"
+
+    @classmethod
+    def is_compatible_with(cls, module=None):
+        """
+        Determine compatibility
+
+        Compatible with downloaded videos, and not really anything else!
+        Additionally ffmpeg needs to be available.
+
+        :param str module:  Module ID to determine compatibility with
+        :return bool:
+        """
+        return module.type in ["video-downloader"] and \
+               config.get("video_downloader.ffmpeg-path") and \
+               shutil.which(config.get("video_downloader.ffmpeg-path"))
+
+    def get_processor_pipeline(self):
+        """
+        This queues a series of post-processors to visualise videos.
+        """
+
+        pipeline = [
+            # first, create colleges (and hashes) with the default settings
+            {
+                "type": "video-hasher-1"
+            },
+            # then, extract hashes
+            {
+                "type": "video-hasher-2"
+            },
+        ]
+
+        return pipeline
 
 
 class VideoHasher(BasicProcessor):
@@ -39,37 +87,21 @@ class VideoHasher(BasicProcessor):
 	do not have much differentiating information particularly if there is little difference between frames (i.e. no
 	"scene" changes) and have lead to unwanted collision in tests.
 	"""
-	type = "video-hashes"  # job type ID
+	type = "video-hasher-1"  # job type ID
 	category = "Visual"  # category
-	title = "Create Video hashes to identify near duplicate videos"  # title displayed in UI
-	description = "Creates video hashes (64 bits/identifiers) to identify near duplicate videos in a dataset based on hash similarity. Uses video only (no audio; see references). This process can take a long time depending on video length, amount, and frames per second."  # description displayed in UI
-	extension = "csv"  # extension of result file, used internally and in UI
-
-	references = [
-		"[Video Hash](https://github.com/akamhy/videohash#readme)",
-	]
+	title = "Create Video colleges"  # title displayed in UI
+	description = "Creates video from frames. Can be used to create video hashes to detect similar videos."  # description displayed in UI
+	extension = "zip"  # extension of result file, used internally and in UI
 
 	options = {
 		"frame_interval": {
 			"type": UserInput.OPTION_TEXT,
 			"help": "Number of frames extracted per second to extract from video",
-			"tooltip": "The default value is 1 frame per second. For 1 frame per 5 seconds pass 0.2 (1/5). For 5 fps pass 5. For short videos, more frames per second lead to less collision (unsimilar videos being marked as similar), but require more time (2 fps is double the time of 1 fps).",
+			"tooltip": "The default value is 1 frame per second. For 1 frame per 5 seconds pass 0.2 (1/5). For 5 fps pass 5. For short videos, more frames per second lead to less collision when creating hashes (unsimilar videos being marked as similar), but require more time (2 fps is double the time of 1 fps).",
 			"coerce_type": float,
 			"default": 1,
 			"min": 0,
 			"max": 5,
-		},
-		"store_video_collages": {
-			"type": UserInput.OPTION_TOGGLE,
-			"help": "Store video collages",
-			"default": False,
-			"tooltip": "If enabled, the video collages will be stored. A seperate processor can be used to download them."
-		},
-		"update_parent": {
-			"type": UserInput.OPTION_TOGGLE,
-			"help": "Add video hashes to parent dataset",
-			"default": False,
-			"tooltip": "If enabled, the latest created video hashes will be added to the parent dataset and thus can be exported along with originating posts/items. This will overwrite previously created video_hashes added to dataset."
 		},
 	}
 
@@ -78,7 +110,7 @@ class VideoHasher(BasicProcessor):
 		"""
 		Allow on videos only
 		"""
-		return module.type in ["video-downloader", "video-downloader-plus"]
+		return module.type in ["video-downloader"]
 
 	def process(self):
 		"""
@@ -94,14 +126,10 @@ class VideoHasher(BasicProcessor):
 		# Collect parameters
 		frame_interval = self.parameters.get("frame_interval", 1)
 		self.dataset.log('Frames per seconds: %f' % frame_interval)
-		store_video_collages = self.parameters.get("store_video_collages", False)
-		update_parent = self.parameters.get("update_parent", False)
 
 		# Prepare staging area for videos and video tracking
 		staging_area = self.dataset.get_staging_area()
 		self.dataset.log('Staging directory location: %s' % staging_area)
-		if store_video_collages:
-			staging_area.joinpath('collages/').mkdir(exist_ok=True)
 
 		video_hashes = {}
 		video_metadata = None
@@ -135,27 +163,18 @@ class VideoHasher(BasicProcessor):
 
 			video_hashes[path.name] = {'videohash': videohash}
 
-			if store_video_collages:
-				shutil.copy(videohash.collage_path, staging_area.joinpath('collages/' + path.stem + '.jpg'))
-				video_hashes[path.name]['video_collage_filename'] = path.stem + '.jpg'
+			shutil.copy(videohash.collage_path, staging_area.joinpath(path.stem + '.jpg'))
+			video_hashes[path.name]['video_collage_filename'] = path.stem + '.jpg'
 
 			processed_videos += 1
 			self.dataset.update_status(
 				"Created %i/%i video hashes" % (processed_videos, total_possible_videos))
 			self.dataset.update_progress(processed_videos / total_possible_videos)
+			videohash.delete_storage_path()
 
-		if store_video_collages:
-			# Pack up the video collages somewhere we can retrieve them later
-			# TODO: This would be better as its own processor, but creating the collages twice seems counterproductive
-			# Perhaps we can instead somehow utilize the processor pipeline to feed results into a second processor?
-			self.dataset.update_status("Compressing video collages into archive")
-			from shutil import make_archive
-			make_archive(self.dataset.get_results_path().with_suffix(''), "zip", staging_area.joinpath('collages/'))
-			self.dataset.log('Video collages stored here: %s' % self.dataset.get_results_path().with_suffix('.zip'))
-
-		# Write output file
+		# Write hash file
+		# This file is held here and then copied as its own dataset via VideoHasherTwo
 		num_posts = 0
-		post_id_to_results = {}
 		rows = []
 		if video_metadata is None:
 			# Not good, but let's store the video_hashes and note the error
@@ -173,51 +192,93 @@ class VideoHasher(BasicProcessor):
 				num_posts += 1
 		else:
 			self.dataset.update_status("Saving video hash results")
-			with self.dataset.get_results_path().open("w", encoding="utf-8", newline="") as outfile:
-				for url, data in video_metadata.items():
-					if not data.get("success"):
+			for url, data in video_metadata.items():
+				if not data.get("success"):
+					continue
+				if "files" in data:
+					files = data.get('files')
+				elif "filename" in data:
+					files = [{"filename": data.get("filename"), "success": True}]
+				else:
+					self.dataset.log(f"Metadata Error: {url} with {data}")
+					continue
+
+				for file in files:
+					if not file.get("success"):
 						continue
-					files = data.get('files') if 'files' in data else [{"filename": data.get("filename"), "success":True}]
-					for file in files:
-						if not file.get("success"):
-							continue
-						video_hash = video_hashes[file.get('filename')].get('videohash')
-						row = {
-							'id': file.get('filename'),  # best if all datasets have unique identifier
-							'url': url,
-							"from_dataset": data.get("from_dataset"),
-							'video_hash': video_hash.hash,
-							'video_duration': video_hash.video_duration,
-							'video_count': len(data.get('post_ids', [])),
-							"post_ids": ','.join(data.get("post_ids", [])),
-							'video_collage_filename': video_hashes[file.get('filename')].get('video_collage_filename'),
-						}
+					if file.get('filename') not in video_hashes:
+						self.dataset.log(f"Metadata Error: {file.get('filename')} with {url} - {data}")
+						continue
+					video_hash = video_hashes[file.get('filename')].get('videohash')
+					rows.append({
+						'id': file.get('filename'),  # best if all datasets have unique identifier
+						'url': url,
+						"from_dataset": data.get("from_dataset"),
+						'video_hash': video_hash.hash,
+						'video_duration': video_hash.video_duration,
+						'video_count': len(data.get('post_ids', [])),
+						"post_ids": ','.join(data.get("post_ids", [])),
+						'video_collage_filename': video_hashes[file.get('filename')].get('video_collage_filename'),
+					})
+					num_posts += 1
 
-						if update_parent:
-							for post_id in data.get('post_ids', []):
-								# Posts can have multiple videos
-								if post_id in post_id_to_results.keys():
-									post_id_to_results[post_id].append((url, video_hash.hash))
-								else:
-									post_id_to_results[post_id] = [(url, video_hash.hash)]
-
-						rows.append(row)
-						num_posts += 1
-
-		if update_parent and video_metadata:
-			updated_rows = []
-			for post in self.dataset.top_parent().iterate_items(self):
-				video_hashes = post_id_to_results.get(post.get('id'), [])
-
-				# Combine multiple hashes along with specific url
-				updated_rows.append(','.join(['%s: %s' % (url, video_hash) for url, video_hash in video_hashes]))
-
-			self.add_field_to_parent(field_name='4CAT_video_hashes',
-									 new_data=updated_rows,
-									 which_parent=self.dataset.top_parent(),
-									 update_existing=True)
+		writer = None
+		with staging_area.joinpath("video_hashes.csv").open("w", encoding="utf-8", newline="") as outfile:
+			for row in rows:
+				if not writer:
+					writer = csv.DictWriter(outfile, fieldnames=row.keys())
+					writer.writeheader()
+				writer.writerow(row)
+				num_posts += 1
 
 		# Finish up
-		self.dataset.update_status(
-			'Created %i video hashes%s.' % (num_posts, ' and stored video collages' if store_video_collages else ''))
-		self.write_csv_items_and_finish(rows)
+		self.dataset.update_status(f'Created {num_posts} video hashes and stored video collages')
+		self.write_archive_and_finish(staging_area)
+
+
+class VideoHasherTwo(BasicProcessor):
+	"""
+	Video Hasher Two
+
+	This just grabs the output from the previous processor
+	"""
+	type = "video-hasher-2"  # job type ID
+	category = "Visual"  # category
+	title = "Create Video hashes to identify near duplicate videos"  # title displayed in UI
+	description = "Creates hashes from video colleges. These hashes can be compared to identify duplicate or similar videos."  # description displayed in UI
+	extension = "csv"  # extension of result file, used internally and in UI
+
+	references = [
+		"[Video Hash](https://github.com/akamhy/videohash#readme)",
+	]
+
+	@classmethod
+	def is_compatible_with(cls, module=None):
+		"""
+		Allow on videos only
+		"""
+		return module.type in ["video-hasher-1"]
+
+	def process(self):
+		"""
+		Copy the video hash csv file from the archive of the parent dataset.
+		"""
+		with zipfile.ZipFile(self.source_file, "r") as archive_file:
+			archive_contents = sorted(archive_file.namelist())
+
+			if "video_hashes.csv" not in archive_contents:
+				self.dataset.update_status("Unable to obtain hashes from video colleges.", is_final=True)
+				self.dataset.finish(0)
+				return
+
+			# Prepare staging area for videos and video tracking
+			staging_area = self.dataset.get_staging_area()
+			self.dataset.log('Staging directory location: %s' % staging_area)
+			# Extract file
+			archive_file.extract("video_hashes.csv", staging_area)
+			# Copy file to new dataset results path
+			shutil.copy(staging_area.joinpath("video_hashes.csv"), self.dataset.get_results_path())
+
+			self.dataset.update_status("Finished")
+			self.dataset.finish(self.source_dataset.num_rows)
+
