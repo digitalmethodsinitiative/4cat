795895a7189842549b5ac569470977c5714a4229
Dale Wahl <dalewahl@gmail.com>
2023-02-15 11:33:07 +0100

    minor docker changes to allow setting database and API hosts; fix so frontend changes to server_name not overwritten by env
    mostly because I made changes to run 4CAT in one container and needed a way to quickly change it back when working on something else!

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -24,19 +24,19 @@ do
 done
 
 echo "Waiting for postgres..."
-while ! nc -z db 5432; do
+while ! nc -z $POSTGRES_HOST 5432; do
   sleep 0.1
 done
 echo "PostgreSQL started"
 
 # Create Database if it does not already exist
-if [ `psql --host=db --port=5432 --user=$POSTGRES_USER --dbname=$POSTGRES_DB -tAc "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'jobs')"` = 't' ]; then
+if [ `psql --host=$POSTGRES_HOST --port=5432 --user=$POSTGRES_USER --dbname=$POSTGRES_DB -tAc "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'jobs')"` = 't' ]; then
   # Table already exists
   echo "Database already created"
 else
   echo "Creating Database"
   # Seed DB
-  cd /usr/src/app && psql --host=db --port=5432 --user=$POSTGRES_USER --dbname=$POSTGRES_DB < backend/database.sql
+  cd /usr/src/app && psql --host=$POSTGRES_HOST --port=5432 --user=$POSTGRES_USER --dbname=$POSTGRES_DB < backend/database.sql
   # No database exists, new build, no need to migrate so create .current-version file
   cp VERSION config/.current-version
 fi
@@ -49,11 +49,6 @@ python3 helper-scripts/migrate.py -y
 
 # Run docker_setup to update any environment variables if they were changed
 python3 docker/docker_setup.py
-
-echo 'Starting app'
-echo "4CAT is accessible at:"
-echo "http://$SERVER_NAME:$PUBLIC_PORT"
-echo ''
 
 # Start 4CAT backend
 python3 4cat-daemon.py start
