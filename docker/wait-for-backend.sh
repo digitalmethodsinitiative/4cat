e4d692bf0b9756185e042737909a1e2b51199d32
Dale Wahl <dalewahl@gmail.com>
2023-05-09 14:50:45 +0200

    wait for config.ini on new install

--- docker/wait-for-backend.sh
+++ docker/wait-for-backend.sh
@@ -3,6 +3,12 @@
 
 set -e
 
+# Ensure config.ini is created (backend will create on first run)
+while [ ! -e "${FOURCAT_DATA}/config/config.ini" ] ; do
+  sleep 0.1
+done
+
+# Use 4CAT internal API to check for 4CAT backend
 until python3 -m docker.wait-for-backend; do
   >&2 echo "Backend has not started - sleeping"
   sleep 1
