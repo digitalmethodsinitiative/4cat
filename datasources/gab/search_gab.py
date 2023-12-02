43d69b8031e3fd6dc4f4af9ee9e4cc0030357314
Parker-Kasiewicz <parkerkasiewicz@gmail.com>
2023-12-02 15:55:33 -0800

    Ingest Gab posts from Zeeschuimer

--- datasources/gab/search_gab.py
+++ datasources/gab/search_gab.py
@@ -47,80 +47,34 @@ class SearchGab(Search):
 
         mapped_item = {
             "id": post["id"],
-            "type": post["type"],
             "created_at": post["created_at"],
-            "thread_id": post["id"],
-            "timestamp": post_time.strftime("%Y-%m-%d %H:%M:%S"),
+            "body" : post["content"],
             "url": post["url"],
-            "post_id": "",
-            "body": "",
-            "reaction_count": "",
-            "reposts_count": "",
-            "replies_count": "",
-            "account_id": "",
-            "account_username": "",
-            "account_display_name": "",
-            "account_url": "",
-            "group_id": "",
-            "group_title": "",
-            "group_description": "",
-            "group_member_count": "",
-            "group_is_verified": "",
-            "group_is_private": "",
-            "group_category": "",
-            "account_note": "",
-            "account_followers_count": "",
-            "account_following_count": "",
-            "account_statuses_count": "",
-            "account_is_pro": "",
-            "account_is_verified": "",
-            "account_is_donor": "",
-            "account_is_investor": "",
-            "account_show_pro_life": "",
-            "account_is_parody": ""
-        }
-
-        
-        if post["type"] == "post":
-            mapped_item["post_id"] = post["id"]
-            mapped_item["body"] = post["content"]
-            mapped_item["reaction_count"] = post["reaction_count"]
-            mapped_item["reposts_count"] = post["reposts_count"]
-            mapped_item["replies_count"] = post["replies_count"]
-            
-            ### poster-specific
-            mapped_item["account_id"] = post["account"]["id"] 
-            mapped_item["account_username"] = post["account"]["username"]
-            mapped_item["account_display_name"] = post["account"]["display_name"]
-            mapped_item["account_url"] = post["account"]["url"]
-
-        if post["type"] == "group":
-            mapped_item["group_id"] = post["id"]
-            mapped_item["body"] = post["account"]["title"]
-            mapped_item["group_title"] = post["account"]["title"]
-            mapped_item["group_description"] = post["account"]["description"]
-            mapped_item["group_member_count"] = post["account"]["member_count"]
-            mapped_item["group_is_verified"] = post["account"]["is_verified"]
-            mapped_item["group_is_private"] = post["account"]["is_private"]
-            mapped_item["group_category"] = post["account"]["category"]
-
-
-        if post["type"] == "user":
-            mapped_item["account_id"] = post["id"]
-            mapped_item["account_username"] = post["account"]["username"]
-            mapped_item["account_display_name"] = post["account"]["display_name"]
-            mapped_item["account_url"] = post["account"]["url"]
-            mapped_item["account_note"] = post["account"]["note"]
-            mapped_item["account_avatar"] = post["account"]["avatar"]
-            mapped_item["account_followers_count"] = post["account"]["followers_count"]
-            mapped_item["account_following_count"] = post["account"]["following_count"]
-            mapped_item["account_statuses_count"] = post["account"]["statuses_count"]
-            mapped_item["account_is_pro"] = post["account"]["is_pro"]
-            mapped_item["account_is_verified"] = post["account"]["is_verified"]
-            mapped_item["account_is_donor"] = post["account"]["is_donor"]
-            mapped_item["account_is_investor"] = post["account"]["is_investor"]
-            mapped_item["account_show_pro_life"] = post["account"]["show_pro_life"]
-            mapped_item["account_is_parody"] = post["account"]["is_parody"]
-        
+            "reaction_count": post["reaction_count"],
+            "reposts_count": post["reposts_count"],
+            "replies_count": post["replies_count"],
+            "group_id": post["group"]["title"],
+            "group_description": post["group"]["description"],
+            "group_member_count": post["group"]["member_count"],
+            "group_is_private": post["group"]["is_private"],
+            "group_url": post["group"]["url"],
+            "group_created_at": post["group"]["created_at"],
+            "account_id": post["account"]["id"],
+            "account_username": post["account"]["username"],
+            "account_account": post["account"]["account"],
+            "account_display_name": post["account"]["display_name"],
+            "account_note": post["account"]["note"],
+            "link_id": post["link"]["id"],
+            "link_url": post["link"]["url"],
+            "link_title": post["link"]["title"],
+            "link_description": post["link"]["description"],
+            "link_type": post["link"]["type"] if "type" in post["link"] else None,
+            "link_image": post["link"]["image"],
+            "image_id": post["image"]["id"],
+            "image_url": post["image"]["url"],
+            "image_type": post["image"]["type"] if "type" in post["image"] else None,
+            "thread_id": post["id"],
+            "timestamp": post_time.strftime("%Y-%m-%d %H:%M:%S")
+        }        
     
         return mapped_item
