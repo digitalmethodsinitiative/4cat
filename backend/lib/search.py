0686e1aedcf7d6a3210ea95e14d65022f7221986
Stijn Peeters <stijn.peeters@uva.nl>
2024-02-09 15:37:14 +0100

    Keep track of import warnings in search.py

--- backend/lib/search.py
+++ backend/lib/search.py
@@ -48,7 +48,7 @@ class Search(BasicProcessor, ABC):
 	# Mandatory columns: ['thread_id', 'body', 'subject', 'timestamp']
 	return_cols = ['thread_id', 'body', 'subject', 'timestamp']
 
-	flawless = 0
+	import_issues = 0
 
 	def process(self):
 		"""
@@ -114,10 +114,11 @@ class Search(BasicProcessor, ABC):
 				# file exists somewhere, so we create it as an empty file
 				with open(query_parameters.get("copy_to"), "w") as empty_file:
 					empty_file.write("")
-		if self.flawless == 0:
+
+		if self.import_issues == 0:
 			self.dataset.finish(num_rows=num_items)
 		else:
-			self.dataset.update_status(f"Unexpected data format for {self.flawless} items. All data can be downloaded, but only data with expected format will be available to 4CAT processors; check logs for details", is_final=True)
+			self.dataset.update_status(f"{self.import_issues} item(s) in the dataset had an unexpected format. All data can be downloaded, but only data with the expected format will be available to 4CAT processors and in CSV exports; check the dataset log for details.", is_final=True)
 			self.dataset.finish(num_rows=num_items)
 
 	def search(self, query):
@@ -179,6 +180,8 @@ class Search(BasicProcessor, ABC):
 		if not path.exists():
 			return []
 
+		import_warnings = {}
+
 		# Check if processor and dataset can use map_item
 		check_map_item = self.map_item_method_available(dataset=self.dataset)
 		if not check_map_item:
@@ -199,17 +202,36 @@ class Search(BasicProcessor, ABC):
 					**item["data"],
 					"__import_meta": {k: v for k, v in item.items() if k != "data"}
 				}
+
 				# Check map item here!
 				if check_map_item:
 					try:
-						self.get_mapped_item(new_item)
+						mapped_item = self.get_mapped_item(new_item)
+
+						# keep track of items that raised a warning
+						# this means the item could be mapped, but there is
+						# some information the user should take note of
+						warning = mapped_item.get_message()
+						if warning:
+							if warning not in import_warnings:
+								import_warnings[warning] = 0
+							import_warnings[warning] += 1
+							self.import_issues += 1
+
 					except MapItemException as e:
 						# NOTE: we still yield the unmappable item; perhaps we need to update a processor's map_item method to account for this new item
-						self.flawless += 1
+						self.import_issues += 1
 						self.dataset.warn_unmappable_item(item_count=i, processor=self, error_message=e, warn_admins=unmapped_items is False)
 						unmapped_items = True
 
 				yield new_item
+
+		# warnings were raised about some items
+		# log these, with the number of items each warning applied to
+		if sum(import_warnings.values()) > 0:
+			self.dataset.log("While importing, the following issues were raised:")
+			for warning, num_items in import_warnings.items():
+				self.dataset.log(f"  {warning} (for {num_items:,} item(s))")
 
 		path.unlink()
 		self.dataset.delete_parameter("file")
