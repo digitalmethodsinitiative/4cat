14af493da41830cd2ce2d5df0c59297fe9f4a080
Dale Wahl <dalewahl@gmail.com>
2024-02-29 11:33:27 +0100

    add ui_only parameter to DataSet.get_available_processors() and BasicProcessor.display_in_ui()
    Allow using `display_in_ui` to hide processors from UI but allow them to be queued either via API or presets. This avoids issue of is_compatible_with() having to be used to hide processors with sometimes ill effects.

--- common/lib/dataset.py
+++ common/lib/dataset.py
@@ -1390,7 +1390,7 @@ class DataSet(FourcatModule):
 		processor_type = self.parameters.get("type", self.data.get("type"))
 		return backend.all_modules.processors.get(processor_type)
 
-	def get_available_processors(self, user=None):
+	def get_available_processors(self, user=None, ui_only=True):
 		"""
 		Get list of processors that may be run for this dataset
 
@@ -1401,11 +1401,15 @@ class DataSet(FourcatModule):
 
 		:param str|User|None user:  User to get compatibility for. If set,
 		use the user-specific config settings where available.
+		:param bool ui_only:  Only return processors that should be displayed
+		in the UI. If `False`, all processors are returned.
 
 		:return dict:  Available processors, `name => properties` mapping
 		"""
 		if self.available_processors:
-			return self.available_processors
+			# Update to reflect ui_only parameter which may be different from last call
+			# TODO: could children also have been created? Possible bug, but I have not seen anything effected by this
+			return [self.available_processors for processor in self.available_processors if not ui_only or processor.display_in_ui()]
 
 		processors = self.get_compatible_processors(user=user)
 
@@ -1414,6 +1418,9 @@ class DataSet(FourcatModule):
 				continue
 
 			if not processors[analysis.type].get_options():
+				del processors[analysis.type]
+
+			if ui_only and not processors[analysis.type].display_in_ui():
 				del processors[analysis.type]
 
 		self.available_processors = processors
