131a0eca0ad514b1ee57803e5c560ab0e56de42d
Stijn Peeters <stijn.peeters@uva.nl>
2024-04-08 18:28:04 +0200

    Do not attempt to load crashed file as module in Slack webhook. Fixes #422 (hopefully)

--- common/lib/logger.py
+++ common/lib/logger.py
@@ -10,7 +10,6 @@ import json
 from pathlib import Path
 
 from logging.handlers import RotatingFileHandler, HTTPHandler
-from importlib.machinery import SourceFileLoader
 
 from common.config_manager import config
 
@@ -130,17 +129,6 @@ class SlackLogHandler(WebHookLogHandler):
                 })
         except (IndexError, AttributeError):
             # the file is not readable, or the line number is out of bounds
-            pass
-
-        try:
-            module = SourceFileLoader(record.frame.filename.split("/")[-1].split(".")[0], record.frame.filename).load_module()
-            if "__maintainer__" in dir(module):
-                fields.append({
-                    "title": "Code maintainer:",
-                    "value": module.__maintainer__,
-                    "short": False
-                })
-        except ImportError:
             pass
 
         return {
