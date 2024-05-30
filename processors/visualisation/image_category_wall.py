e3c9ea57d46b32ba47b00a6047a278ddd530adc1
Dale Wahl <dalewahl@gmail.com>
2024-05-30 16:27:50 +0200

    image_category_wall convert None to str for category

--- processors/visualisation/image_category_wall.py
+++ processors/visualisation/image_category_wall.py
@@ -225,7 +225,7 @@ class ImageWallGenerator(BasicProcessor):
 
 				if category_type == str:
 					post_category = post.get(category_column)
-					if post_category == "":
+					if post_category == "" or post_category is None:
 						post_category = "None"
 					if post_category not in categories:
 						categories[post_category] = [{"id": post.get("id")}]
