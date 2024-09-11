fc909612c6f5767165494c86d055041e12f02584
Dale Wahl <dalewahl@gmail.com>
2024-09-11 12:55:00 +0200

    update install

--- extensions/web_studies/fourcat_install.py
+++ extensions/web_studies/fourcat_install.py
@@ -16,6 +16,9 @@ if __name__ == "__main__":
 					 action="store_true")
 	cli.add_argument("--component", "-c", default="backend",
 					 help="Which component of 4CAT to migrate. Nothing is installed when set to 'frontend'")  # Necessary to work with 4CAT migrate.py
+	cli.add_argument("--no-pip", "-p", default=False,
+					 help="Run pip to install any python requirements.",
+					 action="store_true")
 	args, extras = cli.parse_known_args()
 
 	def run_command(command, error_message):
@@ -34,7 +37,24 @@ if __name__ == "__main__":
 
 		return result
 
+	def pip_install():
+		"""
+		Install python requirements
+		"""
+		print("Installing python requirements")
+		interpreter = sys.executable
+		command = f"{interpreter} -m pip install -r requirements.txt"
+		run_command(command, "Error installing python requirements")
+
 	if args.component == "frontend":
+		# Frontend still needs packages though only to import modules successfully
+		if not args.no_pip:
+			try:
+				import selenium
+			except ImportError:
+				pip_install()
+				print("Installed required packages for extension.")
+				exit(0)
 		print("4CAT frontend component selected. No installation required.")
 		exit(0)
 	elif args.component not in ["backend", "both"]:
@@ -126,6 +146,9 @@ if __name__ == "__main__":
 
 		command = f"rm {FIREFOX_SETUP}"
 		run_command(command, "Error removing temp download files")
+
+	if not args.no_pip:
+		pip_install()
 
 	config.with_db()
 	config.set('selenium.installed', True)
