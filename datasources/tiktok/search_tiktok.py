1e2010af44817016c274c9ec9f7f9971deb57f66
Stijn Peeters <stijn.peeters@uva.nl>
2024-09-18 14:07:38 +0200

    Forgot TikTok and Douyin

--- datasources/tiktok/search_tiktok.py
+++ datasources/tiktok/search_tiktok.py
@@ -20,7 +20,7 @@ class SearchTikTok(Search):
     title = "Import scraped Tiktok data"  # title displayed in UI
     description = "Import Tiktok data collected with an external tool such as Zeeschuimer."  # description displayed in UI
     extension = "ndjson"  # extension of result file, used internally and in UI
-    is_from_extension = True
+    is_from_zeeschuimer = True
 
     # not available as a processor for existing datasets
     accepts = [None]
