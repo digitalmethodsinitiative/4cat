115a3c155f1c5edbce3d6c62c1fbbf66fa6a51ba
Dale Wahl <dalewahl@gmail.com>
2025-01-27 14:18:12 +0100

    bsky datasource

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ with open("VERSION") as versionfile:
 # Universal packages
 packages = set([
 	"anytree~=2.8.0",
+	"atproto==0.0.57",
 	"bcrypt~=3.2.0",
 	"beautifulsoup4",#~=4.11.0",
 	"clarifai-grpc~=9.0",
