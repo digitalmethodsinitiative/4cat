53c9134f461daee4810a5a5df81f0eae67a0df9c
Stijn Peeters <stijn.peeters@uva.nl>
2025-02-04 16:11:40 +0100

    Handle both JSON and HTML-sourced Pinterest data

--- datasources/pinterest/search_pinterest.py
+++ datasources/pinterest/search_pinterest.py
@@ -41,12 +41,30 @@ class SearchPinterest(Search):
         """
         Map Pinterest object to 4CAT item
 
-        Pretty simple, except posts sometimes don't have timestamps :|
+        Depending on whether the object was captured from JSON or HTML, treat it
+        differently. A lot of data is missing from HTML objects.
+
+        :param post:
+        :return:
+        """
+        if post.get("_zs-origin") == "html":
+            return SearchPinterest.map_item_from_html(post)
+        else:
+            return SearchPinterest.map_item_from_json(post)
+
+    @staticmethod
+    def map_item_from_json(post):
+        """
+        Map Pinterest object to 4CAT item
+
+        Pretty simple, except posts sometimes don't have timestamps :| but at
+        least these objects are more complete than the HTML data usually
 
         :param dict post:  Pinterest object
         :return MappedItem:  Mapped item
         """
         try:
+            # there are often no timestamps :'(
             timestamp = datetime.strptime(post.get("created_at", post.get("createdAt")), "%a, %d %b %Y %H:%M:%S %z")
             unix_timestamp = int(timestamp.timestamp())
             str_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
@@ -73,7 +91,7 @@ class SearchPinterest(Search):
             "board": post["board"]["name"],
             "board_pins": post["board"].get("pinCount", post["board"].get("pin_count")),
             "board_url": f"https://www.pinterest.com{post['board']['url']}",
-            "timestamp": str_timestamp,  # there are no timestamps :(
+            "timestamp": str_timestamp,
             "idea_tags": ",".join(post["pinJoin"]["visualAnnotation"]) if post.get("pinJoin") else "",
             "url": f"https://www.pinterest.com/pin/{post_id}",
             # these are not always available (shame)
@@ -84,4 +102,41 @@ class SearchPinterest(Search):
             "image_url": image_url,
             "dominant_colour": post.get("dominantColor", post.get("dominant_color")),
             "unix_timestamp": unix_timestamp
+        })
+
+    @staticmethod
+    def map_item_from_html(post):
+        """
+        Map Pinterest object to 4CAT item
+
+        These are from the HTML and have even less data than JSON objects...
+        but enough to be useful in some cases.
+
+        :param dict post:  Pinterest object
+        :return MappedItem:  Mapped item
+        """
+        return MappedItem({
+            "id": int(post["id"]),
+            "author": MissingMappedField(""),
+            "author_fullname": MissingMappedField(""),
+            "author_original": MissingMappedField(""),
+            "body": post["body"].strip(),
+            "subject": post["title"].strip(),
+            "ai_description": MissingMappedField(""),
+            "pinner_original": MissingMappedField(""),
+            "pinner_via": MissingMappedField(""),
+            "board": MissingMappedField(""),
+            "board_pins": MissingMappedField(""),
+            "board_url": MissingMappedField(""),
+            "timestamp": MissingMappedField(""),  # there are no timestamps :(
+            "idea_tags": ",".join(post["tags"]),
+            "url": f"https://www.pinterest.com/pin/{post['id']}",
+            # these are not always available (shame)
+            # "is_repin": "yes" if post["isRepin"] else "no",
+            # "is_unsafe": "yes" if post["isUnsafe"] else "no",
+            # "total_saves": post["aggregatedPinData"]["aggregatedStats"]["saves"],
+            "is_video": MissingMappedField(""),
+            "image_url": post["image"],
+            "dominant_colour": MissingMappedField(""),
+            "unix_timestamp": MissingMappedField("")
         })
