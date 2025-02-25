05d13f436933572445f36149ba1a9fa944beb7e6
Dale Wahl <dalewahl@gmail.com>
2025-02-25 18:57:13 +0100

    bsky: password input type and remove session_id from displayed properties

--- datasources/bsky/search_bsky.py
+++ datasources/bsky/search_bsky.py
@@ -75,8 +75,9 @@ class SearchBluesky(Search):
             "password": {
                 "type": UserInput.OPTION_TEXT,
                 "help": "Bluesky Password",
-                "cache": True,
-                "sensitive": True,
+                "cache": True, # tells the frontend to cache this value
+                "sensitive": True, # tells the backend to delete this value after use
+                "password": True, # tells the frontend this is a password type
             },
             "divider": {
                 "type": UserInput.OPTION_DIVIDER
