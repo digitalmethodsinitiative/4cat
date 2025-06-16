7bbf308feb82409e37024af05b0a67888fb80a80
Stijn Peeters <stijn.peeters@uva.nl>
2025-06-16 19:31:36 +0200

    prettier...

--- datasources/eightkun/__init__.py
+++ datasources/eightkun/__init__.py
@@ -1,7 +1,6 @@
 # Use default data source init function
 from common.lib.helpers import init_datasource as base_init_datasource  # noqa: F401
-from common.lib.helpers import init_datasource
-__all__ = ("init_datasource",)
+from common.lib.helpers import init_datasource as init_datasource
 
 # Internal identifier for this data source
 DATASOURCE = "eightkun"
--- datasources/media_import/__init__.py
+++ datasources/media_import/__init__.py
@@ -1,6 +1,5 @@
 # Use default data source init function
-from common.lib.helpers import init_datasource
-__all__ = ("init_datasource",)
+from common.lib.helpers import init_datasource as init_datasource
 
 # Internal identifier for this data source
 DATASOURCE = "media-import"
