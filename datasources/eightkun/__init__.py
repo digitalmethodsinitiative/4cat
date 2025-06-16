f1f1363c8b3f5cc19d20fcc28e762d189272c47d
Stijn Peeters <stijn.peeters@uva.nl>
2025-06-16 16:30:32 +0200

    noqa import in datasource init

--- datasources/eightkun/__init__.py
+++ datasources/eightkun/__init__.py
@@ -1,6 +1,6 @@
 # Use default data source init function
-from common.lib.helpers import init_datasource as base_init_datasource
-from datasources.fourchan import init_datasource
+from common.lib.helpers import init_datasource as base_init_datasource  # noqa: F401
+from datasources.fourchan import init_datasource  # noqa: F401
 
 # Internal identifier for this data source
 DATASOURCE = "eightkun"
--- datasources/media_import/__init__.py
+++ datasources/media_import/__init__.py
@@ -1,5 +1,5 @@
 # Use default data source init function
-from common.lib.helpers import init_datasource
+from common.lib.helpers import init_datasource  # noqa: F401
 
 # Internal identifier for this data source
 DATASOURCE = "media-import"
