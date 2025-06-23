3d3f8d663d27c83f30320109546b3e2dbaf713e1
Stijn Peeters <stijn.peeters@uva.nl>
2025-06-23 21:10:22 +0200

    Memcache server is a config.ini setting

--- common/lib/config_definition.py
+++ common/lib/config_definition.py
@@ -247,13 +247,6 @@ config_definition = {
         "tooltip": "Sphinx is used for full-text search for collected datasources (e.g., 4chan, 8kun, 8chan) and requires additional setup (see 4CAT wiki on GitHub).",
         "global": True
     },
-    "4cat.memcached_host": {
-        "type": UserInput.OPTION_TEXT,
-        "default": "",
-        "help": "Memcached server",
-        "tooltip": "Memcached server address, e.g. 'localhost' or '127.0.0.1:11211. If empty, memcached is not used.",
-        "global": True
-    },
     # proxy stuff
     "proxies.urls": {
         "type": UserInput.OPTION_TEXT_JSON,
