62e1c8137408d98f19eaba0fcccb7dbe16a9e530
Stijn Peeters <stijn.peeters@uva.nl>
2025-09-24 11:53:04 +0200

    Log recurring worker start as DEBUG2

--- backend/lib/manager.py
+++ backend/lib/manager.py
@@ -114,7 +114,8 @@ class WorkerManager:
 						job.claim()
 						worker = worker_class(logger=self.log, manager=self, job=job, modules=self.modules)
 						worker.start()
-						self.log.info(f"Starting new worker for job {job.data['jobtype']}/{job.data['remote_id']}")
+						log_level = self.log.levels["DEBUG"] if job.data["interval"] else self.log.levels["INFO"]
+						self.log.log(f"Starting new worker for job {job.data['jobtype']}/{job.data['remote_id']}", log_level)
 						self.worker_pool[jobtype].append(worker)
 					except JobClaimedException:
 						# it's fine
