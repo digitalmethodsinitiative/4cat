119693b7aa3b65bf396bfadc3c15a10d610f2609
Stijn Peeters <stijn.peeters@uva.nl>
2025-10-02 11:56:43 +0200

    Better Slack error messages
    Clickable stack trace, distinguish 4CAT code from library code

--- common/lib/logger.py
+++ common/lib/logger.py
@@ -12,6 +12,7 @@ from pathlib import Path
 from logging.handlers import RotatingFileHandler, HTTPHandler
 
 from common.config_manager import CoreConfigManager
+from common.lib.helpers import get_software_commit
 
 class WebHookLogHandler(HTTPHandler):
     """
@@ -91,6 +92,23 @@ class SlackLogHandler(WebHookLogHandler):
     """
     Slack webhook log handler
     """
+    _base_github_url = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor
+
+        We add an argument for the Github link to link to in the stack trace
+        for easier debugging.
+
+        :param args:
+        :param kwargs:
+        """
+        if "github_url" in kwargs:
+            self._base_github_url = kwargs["github_url"]
+            del kwargs["github_url"]
+
+        super().__init__(*args, **kwargs)
 
     def mapLogRecord(self, record):
         """
@@ -99,10 +117,13 @@ class SlackLogHandler(WebHookLogHandler):
         :param logging.LogRecord record: Log record
         """
         if record.levelno in (logging.ERROR, logging.CRITICAL):
+            emoji = ":rotating_light:"
             color = "#FF0000"  # red
         elif record.levelno == logging.WARNING:
+            emoji = ":bell:"
             color = "#DD7711"  # orange
         else:
+            emoji = ":information_source:"
             color = "#3CC619"  # green
 
         # simple stack trace
@@ -113,35 +134,78 @@ class SlackLogHandler(WebHookLogHandler):
             # the last 9 frames are not specific to the exception (general logging code etc)
             # the frame before that is where the exception was raised
             frames = traceback.extract_stack()[:-9]
-        location = "`%s`" % "` → `".join([frame.filename.split("/")[-1] + ":" + str(frame.lineno) for frame in frames])
+
+        nice_frames = []
+        root_path = Path(__file__).parent.parent.parent
+
+        # go through the traceback and distinguish 4CAT's own code from library
+        # or stdlib code, and annotate accordingly
+        highlight_frame = None
+        highlight_link = None
+        for frame in frames:
+            framepath = Path(frame.filename)
+            frame_location = frame.filename.split("/")[-1] + ":" + str(frame.lineno)
+
+            # make a link to the github if it is a file in the 4cat root and
+            # not in the venv (i.e. it is 4CAT's own code)
+            if framepath.is_relative_to(root_path) and "site-packages" not in framepath.parts and self._base_github_url:
+                sub_path = str(framepath.relative_to(root_path))
+                url = f"{self._base_github_url}{sub_path}#L{frame.lineno}"
+                frame_location = f"<{url}|`{frame_location}`>"
+                highlight_frame = frame
+                highlight_link = frame_location
+            else:
+                frame_location = f"_`{frame_location}`_"
+
+            if not highlight_frame:
+                highlight_frame = frame
+                highlight_link = frame_location
+
+            nice_frames.append(frame_location)
 
         # prepare slack webhook payload
-        fields = [{
-            "title": "Stack trace:",
-            "value": location,
-            "short": False
+        attachments = [{
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*Stack trace:*\n{' → '.join(nice_frames)}"
+            }
         }]
 
         # try to read some metadata from the offending file
         try:
-            with Path(record.frame.filename).open() as infile:
-                fields.append({
-                    "title": "Code (`" + record.frame.filename.split("/")[-1] + ":" + str(record.frame.lineno) + "`):",
-                    "value": "```" + infile.readlines()[record.frame.lineno - 1].strip() + "```",
-                    "short": False
+            with Path(highlight_frame.filename).open() as infile:
+                attachments.append({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Code* (" + highlight_link + "):\n```" + infile.readlines()[highlight_frame.lineno - 1].strip() + "```",
+                    }
                 })
         except (IndexError, AttributeError):
             # the file is not readable, or the line number is out of bounds
             pass
 
         return {
-            "text": ":bell: 4CAT %s logged on `%s`:" % (record.levelname.lower(), platform.uname().node),
-            "mrkdwn_in": ["text"],
-            "attachments": [{
-                "color": color,
-                "text": record.message,
-                "fields": fields
-            }]
+            "attachments": [
+                {
+                    "color": color,
+                    "blocks": [
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": f"{emoji} 4CAT {record.levelname.lower()} logged on `{platform.uname().node}`:",
+                            },
+                        },
+                        {
+                            "type": "section",
+                            "text": {"type": "mrkdwn", "text": record.message},
+                        },
+                        *attachments,
+                    ],
+                }
+            ]
         }
 
 
@@ -224,7 +288,20 @@ class Logger:
         :return:
         """
         if config.get("logging.slack.webhook"):
-            slack_handler = SlackLogHandler(config.get("logging.slack.webhook"))
+            # construct a github URL to link in slack alerts to easily review
+            # erroneous code
+            github_url = None
+            if config.get("4cat.github_url"):
+                github_url = config.get("4cat.github_url")
+                if github_url.endswith("/"):
+                    github_url = github_url[:-1]
+                github_url += "/blob/"
+
+                # use commit hash if available, else link to master
+                commit = get_software_commit()
+                github_url += f"{commit[0] or 'master'}/"
+                    
+            slack_handler = SlackLogHandler(config.get("logging.slack.webhook"), github_url=github_url)
             slack_handler.setLevel(self.levels.get(config.get("logging.slack.level"), self.alert_level))
             self.logger.addHandler(slack_handler)
 
