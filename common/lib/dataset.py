f41472b8921972a3facdba155c6ad31a20409e36
Dale Wahl <dalewahl@gmail.com>
2026-02-25 18:04:41 +0100

    fix: ensure files do not start with -
    if someone somewhere were to happen have a hanging space in front of a datasource name for example.

--- common/lib/dataset.py
+++ common/lib/dataset.py
@@ -1486,6 +1486,7 @@ class DataSet(FourcatModule):
             query_bit = self.data["query"].replace(" ", "-").lower()
             query_bit = re.sub(r"[^a-z0-9\-]", "", query_bit)
             query_bit = query_bit[:100]  # Crop to avoid OSError
+            query_bit = query_bit.strip("-")  # Remove leading/trailing dashes that might have been introduced by the previous steps
             file = query_bit + "-" + self.data["key"]
             file = re.sub(r"[-]+", "-", file)
 
