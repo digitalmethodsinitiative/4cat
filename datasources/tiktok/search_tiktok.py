655860bbc332b96fdb12be9e9ee07a0a1dee8a57
Stijn Peeters <stijn.peeters@uva.nl>
2026-04-30 16:00:08 +0200

    update

--- datasources/tiktok/search_tiktok.py
+++ datasources/tiktok/search_tiktok.py
@@ -22,6 +22,7 @@ class SearchTikTok(Search):
     description = "Import Tiktok data collected with an external tool such as Zeeschuimer."  # description displayed in UI
     extension = "ndjson"  # extension of result file, used internally and in UI
     is_from_zeeschuimer = True
+    icon = "brand-tiktok"
 
     # not available as a processor for existing datasets
     accepts = [None]
