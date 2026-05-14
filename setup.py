06eeef482949f9f31f0063551a7f1371597f8c0e
Dale Wahl <dalewahl@gmail.com>
2026-05-14 11:07:36 +0200

    rename download-telegram-videos to download_telegram_videos (for importability)

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ processor_packages = {
 	"razdel~=0.5",  # tokenise
 	"scenedetect[opencv]",  # video_scene_identifier
 	"scikit-learn",  # image_wall, histwords, tf_idf, topic_modeling, classification_evaluation, confusion_matrix
-	"Telethon~=1.36.0",  # search_telegram, download-telegram-videos, download-telegram-images
+	"Telethon~=1.36.0",  # search_telegram, download_telegram_videos, download-telegram-images
 	"unidecode~=1.3",  # accent_fold
 	"wordcloud~=1.8",  # word-cloud
 	# The https://github.com/akamhy/videohash is not being maintained anymore; these are two patches
