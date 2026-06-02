a6a8f11189f2ef447f5ab08040a31114f200ae26
Stijn Peeters <stijn.peeters@uva.nl>
2026-06-02 11:37:18 +0200

    Migrate script

---
+++ helper-scripts/migrate/migrate-1.54-1.55.py
@@ -0,0 +1,80 @@
+import json
+import sys
+import os
+
+from pathlib import Path
+
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), "../.."))
+from common.lib.database import Database
+from common.lib.logger import Logger
+
+import configparser  # noqa: E402
+
+log = Logger(output=True)
+ini = configparser.ConfigParser()
+ini.read(Path(__file__).parent.parent.parent.resolve().joinpath("config/config.ini"))
+db_config = ini["DATABASE"]
+
+db = Database(
+    logger=log,
+    dbname=db_config["db_name"],
+    user=db_config["db_user"],
+    password=db_config["db_password"],
+    host=db_config["db_host"],
+    port=db_config["db_port"],
+    appname="4cat-migrate",
+)
+
+# the separate LLM server settings were consolidated into one overarching 'llm.providers' setting
+print("  Checking if llm.providers setting exists...")
+has_setting = db.fetchone(
+    "SELECT COUNT(*) AS num FROM settings WHERE name = 'llm.providers'"
+)
+
+if has_setting["num"] > 0:
+    print("    ...exists, deleting old settings without overwriting")
+else:
+    print("    ...does not exist, filling with currently configured proviers")
+    provider_type = db.fetchone("SELECT value FROM settings WHERE name = 'llm.provider_type'")
+    providers = {}
+    if not provider_type:
+        print("    ...no provider currently configured")
+    else:
+        url = db.fetchone("SELECT value FROM settings WHERE name = 'llm.server'")
+        host = url.split("/")[2] if "://" in url else "localhost"
+        auth_header = db.fetchone("SELECT value FROM settings WHERE name = 'llm.auth_type'")
+        auth_key = db.fetchone("SELECT value FROM settings WHERE name = 'llm.auth_key'")
+        provider_name = db.fetchone("SELECT value FROM settings WHERE name = 'llm.host_name'")
+        provider_id = f"{provider_type}-{host}"
+
+        # vLLM and LM Studio are both openai-like
+        provider_type = {"ollama": "ollama"}.get(provider_type, "openai-like")
+        providers[provider_id] = {
+            "name": provider_name,
+            "type": provider_type,
+            "url": url,
+            "auth_header": auth_header,
+            "auth_key": auth_key,
+            "_id": provider_id
+        }
+
+    # add API models, always present
+    providers["thirdparty-models"] = {
+        "name": "Third-party models",
+        "type": "api",
+        "url": "",
+        "auth_header": "",
+        "auth_key": "",
+        "_id": "thirdparty-models"
+    }
+
+    db.insert("settings", {"name": "llm.providers", "value": json.dumps(providers)})
+    print(f"    ...added {len(providers)} providers")
+
+print("  Cleaning up old settings")
+db.execute("DELETE FROM settings WHERE name LIKE 'llm.%' AND name NOT IN ('llm.providers', 'llm.available_models', 'llm.access')")
+
+print("  Removing all known models (will be re-indexed on 4CAT restart)")
+db.upsert("settings", {"name": "llm.available_models", "value": "{}"})
+
+print("  - done!")
