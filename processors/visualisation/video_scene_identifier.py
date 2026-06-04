aa43f022eb64f34a87e0eb2033b4f7873e339547
Stijn Peeters <stijn.peeters@uva.nl>
2026-06-04 20:07:46 +0200

    Add ffmpeg-based scene detection to scene detect processor

--- processors/visualisation/video_scene_identifier.py
+++ processors/visualisation/video_scene_identifier.py
@@ -1,9 +1,13 @@
 """
 Detect scenes in videos
 """
+import shutil
+import oslex
 import json
 import os
-from scenedetect import open_video, SceneManager, VideoOpenFailure
+import re
+
+from scenedetect import open_video, SceneManager, VideoOpenFailure, FrameTimecode
 
 from backend.lib.processor import BasicProcessor
 from common.lib.exceptions import ProcessorInterruptedException, ProcessorException
@@ -13,6 +17,12 @@ __author__ = "Dale Wahl"
 __credits__ = ["Dale Wahl"]
 __maintainer__ = "Dale Wahl"
 __email__ = "4cat@oilab.eu"
+
+class SceneDetectionException(ProcessorException):
+	"""
+	Exception raised when a video cannot be opened or analysed
+	"""
+	pass
 
 
 class VideoSceneDetector(BasicProcessor):
@@ -33,7 +43,8 @@ class VideoSceneDetector(BasicProcessor):
 
 	references = [
 		"[PySceneDetect](https://github.com/Breakthrough/PySceneDetect)",
-		"[Detection Algorithms](https://scenedetect.com/projects/Manual/en/latest/api/detectors.html)"
+		"[Detection Algorithms](https://scenedetect.com/projects/Manual/en/latest/api/detectors.html)",
+		"ffmpeg's scene/shot detection algorithm is based on [ShotDetect](https://github.com/johmathe/Shotdetect) (see [here](https://github.com/FFmpeg/FFmpeg/commit/7286814))"
 	]
 
 	@classmethod
@@ -47,21 +58,54 @@ class VideoSceneDetector(BasicProcessor):
 		:param config ConfigManager|None config:  Configuration reader (context-aware)
 		:return dict:   Options for this processor
 		"""
-		return {
+		options = {
 			"detector_type": {
 				"help": "Type of detection algorithm",
 				"type": UserInput.OPTION_CHOICE,
-				"default": "adaptive_detector",
+				"default": "ffmpeg_select",
 				"tooltip": "See the processor's reference on Detection Algorithms.",
 				"options": {
-					"content_detector": "ContentDetector: frame by frame detection using color and intensity change; "
-										"mainly detects fast cuts",
-					"adaptive_detector": "AdaptiveDetector: ContentDetector with rolling average of frame changes to "
-										"mitigate fast camera motion falsely detected as scene changes",
-					"threshold_detector": "ThresholdDetector: compares multiple frame groups for both fast cuts and slow "
-										"fades, but only uses pixel intensity (i.e. only detects hard cut or fade to "
-										"black)",
+					"ffmpeg_select": "ffmpeg threshold filter",
+					"content_detector": "PySceneDetect ContentDetector",
+					"adaptive_detector": "PySceneDetect AdaptiveDetector",
+					"threshold_detector": "PySceneDetect ThresholdDetector",
 				},
+			},
+			"ffmpeg-info": {
+				"type": UserInput.OPTION_INFO,
+				"help": "A simple and fast algorithm that calculates a difference score between frames based on their "
+						"colour difference. More pixels with different colours is a higher difference score. A score "
+						"higher than 0.3 has a reasonable chance of indicating a new shot/scene.",
+				"requires": "detector_type==ffmpeg_select"
+			},
+			"cd-info": {
+				"type": UserInput.OPTION_INFO,
+				"help": "Frame by frame detection using color and intensity change; mainly detects fast cuts",
+				"requires": "detector_type==content_detector"
+			},
+			"ad-info": {
+				"type": UserInput.OPTION_INFO,
+				"help": "ContentDetector with rolling average of frame changes to  mitigate fast camera motion falsely "
+						"detected as scene changes",
+				"requires": "detector_type==adaptive_detector"
+			},
+			"td-info": {
+				"type": UserInput.OPTION_INFO,
+				"help": "Compares multiple frame groups for both fast cuts and slow PySceneDetect fades, but only uses "
+						"pixel intensity (i.e. only detects hard cut or fade to black)",
+				"requires": "detector_type==threshold_detector"
+			},
+			"ffmpeg_scene_threshold": {
+				"help": "Detection threshold",
+				"type": UserInput.OPTION_TEXT,
+				"default": 0.3,
+				"coerce_type": float,
+				"min": 0,
+				"max": 1,
+				"tooltip": "Scene detection threshold between 0 and 1. The higher this is, the more change is needed "
+						   "to consider a new scene to have started. See the ffmpeg documentation (in processor "
+						   "references) for more details.",
+				"requires": "detector_type==ffmpeg_select"
 			},
 			"min_scene_len": {
 				"help": "Minimum length of scene in frames",
@@ -71,17 +115,15 @@ class VideoSceneDetector(BasicProcessor):
 				"coerce_type": int,
 				"default": 15,
 				"min": 1,
+				"requires": "detector_type!=ffmpeg_select"
 			},
 			"luma_only": {
 				"type": UserInput.OPTION_TOGGLE,
 				"help": "Only consider changes in luminance/brightness of video",
 				"default": False,
 				"tooltip": "Applies to ContentDetector and AdaptiveDetector. If enabled, only considers changes in the "
-						"luminance channel of the video. If disabled, also consider changes in hue and saturation."
-			},
-			"cd_info": {
-				"type": UserInput.OPTION_INFO,
-				"help": "*Content Detector settings*"
+						"luminance channel of the video. If disabled, also consider changes in hue and saturation.",
+				"requires": ["detector_type!=ffmpeg_select", "detector_type!=threshold_detector"]
 			},
 			"cd_threshold": {
 				"type": UserInput.OPTION_TEXT,
@@ -92,10 +134,7 @@ class VideoSceneDetector(BasicProcessor):
 				"default": 27.0,
 				"min": 0,
 				"max": 5,
-			},
-			"ad_info": {
-				"type": UserInput.OPTION_INFO,
-				"help": "*Adaptive Detector settings*"
+				"requires": "detector_type==content_detector"
 			},
 			"ad_adaptive_threshold": {
 				"type": UserInput.OPTION_TEXT,
@@ -105,6 +144,7 @@ class VideoSceneDetector(BasicProcessor):
 				"coerce_type": float,
 				"default": 3.0,
 				"min": 0,
+				"requires": "detector_type==adaptive_detector"
 			},
 			"ad_min_delta_hsv": {
 				"type": UserInput.OPTION_TEXT,
@@ -114,6 +154,7 @@ class VideoSceneDetector(BasicProcessor):
 				"coerce_type": float,
 				"default": 15.0,
 				"min": 0,
+				"requires": "detector_type==adaptive_detector"
 			},
 			"ad_window_width": {
 				"type": UserInput.OPTION_TEXT,
@@ -123,10 +164,7 @@ class VideoSceneDetector(BasicProcessor):
 				"coerce_type": int,
 				"default": 2,
 				"min": 1,
-			},
-			"td_info": {
-				"type": UserInput.OPTION_INFO,
-				"help": "*Threshold Detector settings*"
+				"requires": "detector_type==adaptive_detector"
 			},
 			"td_threshold": {
 				"type": UserInput.OPTION_TEXT,
@@ -136,6 +174,7 @@ class VideoSceneDetector(BasicProcessor):
 				"coerce_type": float,
 				"default": 12.0,
 				"min": 0,
+				"requires": "detector_type==threshold_detector"
 			},
 			"td_fade_bias": {
 				"type": UserInput.OPTION_TEXT,
@@ -148,6 +187,7 @@ class VideoSceneDetector(BasicProcessor):
 				"default": 0.0,
 				"min": -1.0,
 				"max": 1.0,
+				"requires": "detector_type==threshold_detector"
 			},
 			"save_annotations": {
 				"type": UserInput.OPTION_ANNOTATION,
@@ -157,6 +197,14 @@ class VideoSceneDetector(BasicProcessor):
 				"default": False
 			}
 		}
+
+		# only offer ffmpeg option if we actually have ffmpeg
+		ffmpeg_path = shutil.which(config.get("video-downloader.ffmpeg_path"))
+		if not ffmpeg_path or not os.path.exists(ffmpeg_path):
+			del options["detector_type"]["options"]["ffmpeg_select"]
+			options["detector_type"]["default"] = list(options["detector_type"]["options"].values())[0]
+
+		return options
 
 	@classmethod
 	def is_compatible_with(cls, module=None, config=None):
@@ -170,20 +218,10 @@ class VideoSceneDetector(BasicProcessor):
 		This takes a zipped set of videos, uses https://github.com/Breakthrough/PySceneDetect to detect scene breaks in
 		videos
 		"""
-		# Check processor able to run; metadata files are seen in Windows, not in UNIX
-		if (os.name != "nt" and self.source_dataset.num_rows <= 1 or
-				os.name == "nt" and self.source_dataset.num_rows < 1):
-			# 1 because there is always a metadata file
-			self.dataset.finish_as_empty("No videos from which to extract scenes.")
-			return
-		deduct_metadata = 0
-		if os.name == "nt":
-			deduct_metadata = 1
-
 		save_annotations = self.parameters.get("save_annotations", False)
 
 		self.dataset.update_status("Detecting video scenes")
-		total_possible_videos = self.source_dataset.num_rows - deduct_metadata  # exclude metadata file on UNIX
+		skipped = 0
 		processed_videos = 0
 		video_metadata = None
 		collected_scenes = {}
@@ -201,61 +239,20 @@ class VideoSceneDetector(BasicProcessor):
 				# yt-dlp file
 				continue
 
-			# Get new scene detector
-			scene_manager = self.get_new_scene_manager()
+			detector = self.get_scenes_scenedetect if self.parameters.get("detector_type") != "ffmpeg_select" else self.get_scenes_ffmpeg
 
-			# Open video
+			self.dataset.update_progress(processed_videos / self.source_dataset.num_rows)
+			self.dataset.update_status(f"Detecting scenes in video {processed_videos+1:,} of {self.source_dataset.num_rows:,}")
 			try:
-				video = open_video(str(original_video.file))
-			except VideoOpenFailure as e:
-				self.dataset.update_status(f'Skipping video; Unable to open {video.file.name}: {e}')
+				scenes = detector(original_video)
+			except (VideoOpenFailure, SceneDetectionException) as e:
+				self.dataset.update_status(f'Skipping video; unable to open or parse {original_video.file.name}: {e}')
+				skipped += 1
 				continue
 
-			total_frames = video.duration.get_frames()
-			total_duration = video.duration.get_timecode()
-
-			# Run detector
-			scene_manager.detect_scenes(video)
-			scene_list = scene_manager.get_scene_list()
-			num_scenes_detected = len(scene_list)
-
-			# Collect scene information for mapping to metadata
-			collected_scenes[original_video.file.name] = []
-			if num_scenes_detected == 0:
-				# No scenes detected; record as one scene
-				# TODO: any reason to make this optional?
-				collected_scenes[original_video.file.name].append({
-					'start_frame': video.base_timecode.get_frames(),
-					'start_time': video.base_timecode.get_timecode(),
-					'start_fps': video.base_timecode.get_framerate(),
-					'end_frame': total_frames,
-					'end_time': total_duration,
-					'end_fps': video.duration.get_framerate(),
-					'scene_num': 1,
-					'num_scenes_detected': 1,
-					'total_video_frames': total_frames,
-					'total_video_duration': total_duration,
-				})
-
-			for i, scene in enumerate(scene_list):
-				collected_scenes[original_video.file.name].append({
-					'start_frame': scene[0].get_frames(),
-					'start_time': scene[0].get_timecode(),
-					'start_fps': scene[0].get_framerate(),
-					'end_frame': scene[1].get_frames(),
-					'end_time': scene[1].get_timecode(),
-					'end_fps': scene[1].get_framerate(),
-					'scene_num': i+1,
-					'num_scenes_detected': num_scenes_detected,
-					'total_video_frames': total_frames,
-					'total_video_duration': total_duration,
-				})
-
+			collected_scenes[original_video.file.name] = scenes
 
 			processed_videos += 1
-			self.dataset.update_status(
-				"Detected scenes for %i of %i videos" % (processed_videos, total_possible_videos))
-			self.dataset.update_progress(processed_videos / total_possible_videos)
 
 		# Finish up
 		self.dataset.update_status("Format data for output file")
@@ -313,17 +310,178 @@ class VideoSceneDetector(BasicProcessor):
 			self.save_annotations(annotations)
 
 		if rows:
-			self.dataset.update_status('Detected %i scenes in %i videos' % (num_posts, processed_videos))
-			warning = None if processed_videos < total_possible_videos else "Scenes could not be downloaded for all videos."
+			self.dataset.update_status(f"Detected {num_posts:,} scenes in {processed_videos:,} videos")
+			warning = None if not skipped else f"{skipped:,} videos were skipped"
 			self.write_csv_items_and_finish(rows, warning=warning)
 		else:
 			return self.dataset.finish_with_error("No distinct scenes could be detected in the videos. The videos may "
 												  "be too short for scenes to be detected.")
 
+	def get_scenes_ffmpeg(self, original_video):
+		"""
+		Detect scenes using ffmpeg
+
+		A quick if less sophisticated solution
+
+		:param original_video:  Video file
+		:return:
+		"""
+		threshold = self.parameters.get("ffmpeg_scene_threshold")
+
+		ffmpeg_path = shutil.which(self.config.get("video-downloader.ffmpeg_path"))
+		ffprobe_path = shutil.which("ffprobe".join(ffmpeg_path.rsplit("ffmpeg", 1)))
+
+		# first get some video metadata
+		probe_command = [ffprobe_path, "-v", "error", "-select_streams", "v:0", "-show_entries",
+		                 "stream=avg_frame_rate,duration,nb_frames", "-of", "csv=p=0",
+						 oslex.quote(str(original_video.file))]
+
+		probe = self.run_interruptable_process(probe_command)
+		if probe.stderr.decode("utf-8"):
+			raise SceneDetectionException("Could not read video metadata with ffprobe. The video may be unreadable.")
+
+		probe_output = probe.stdout.decode("utf-8")
+		fps, duration, total_frames = probe_output.split(",")
+		fps = float(fps.split("/")[0]) / float(fps.split("/")[1])
+		duration = float(duration)
+		total_frames = int(total_frames)
+		length_timecode = FrameTimecode(duration, fps).get_timecode()
+
+		command = [ffmpeg_path, "-y", "-hide_banner", "-loglevel", "error"]
+		command.extend(["-i", oslex.quote(str(original_video.file))])
+		# select all frames with a diff to the previous frame of > 0
+		# make sure to always select at least the very first frame (n=0)
+		command.extend(["-vf",
+                r"select=(gte(scene\,0)+eq(n\,0)),metadata=print:file='pipe\:1'",
+                "-an", "-f", "null", os.devnull
+		])
+
+		result = self.run_interruptable_process(command)
+		if not result.stdout:
+			raise SceneDetectionException("ffmpeg did not return any results. The video may be unreadable")
+
+		# parse ffmpeg output into a nice list of frame info dicts
+		frame = {}
+		frames = []
+		for line in result.stdout.splitlines():
+			line = line.decode("utf-8")
+			if line.startswith("frame:"):
+				info = re.match(
+						r"frame:(?P<frame>\d+)\s+pts:(?P<pts>[\d\.]+)\s+pts_time:(?P<pts_time>[\d\.]+)",
+						line,
+				)
+				matches = info.groupdict()
+				frame = {
+					"frame": int(matches["frame"]),
+					"pts": float(matches["pts"]),
+					"pts_time": matches["pts_time"]
+				}
+			elif line.startswith("lavfi.scene_score"):
+				frame.update({
+					"score": float(line.split("=")[1].strip()),
+				})
+				frames.append(frame)
+				frame = {}
+
+		if not frames:
+			raise SceneDetectionException("No frames were processed by ffmpeg. The video may be unreadable.")
+
+		if len(frames) < 2:
+			raise SceneDetectionException("No keyframes were detected in the video.")
+
+		# always include the very first and very last frames as keyframes
+		# this means even in a video with no transitions that meet the
+		# threshold, a single scene will be detected (i.e. the whole video)
+		key_frames = [f for i, f in enumerate(frames) if f["score"] >= threshold or i in (0, len(frames) - 1)]
+		scenes = []
+		previous = None
+
+		# consider each interval between key frames a 'scene'
+		for key_frame in key_frames:
+			if not previous:
+				previous = key_frame
+				continue
+
+			scenes.append({
+				"start_frame": previous["frame"],
+				"start_time": FrameTimecode(previous["pts_time"], fps).get_timecode(),
+				#"start_fps": None,
+				"end_frame": key_frame["frame"],
+				"end_time": FrameTimecode(key_frame["pts_time"], fps).get_timecode(),
+				#"end_fps": None,
+				"scene_num": len(scenes) + 1,
+				"num_scenes_detected": len(key_frames),
+				"total_video_frames": total_frames,
+				"total_video_duration": length_timecode
+			})
+
+			previous = key_frame
+
+		return scenes
+
+
+	def get_scenes_scenedetect(self, original_video):
+		"""
+		Detect scenes using PySceneDetect
+
+		Use one of three detectors as configured in the parameters
+
+		:param original_video:  Video to detect scenes in
+		:return:
+		"""
+		# Get new scene detector
+		scene_manager = self.get_new_scene_manager()
+
+		# Open video
+		video = open_video(str(original_video.file))
+
+		total_frames = video.duration.get_frames()
+		total_duration = video.duration.get_timecode()
+
+		# Run detector
+		scene_manager.detect_scenes(video)
+		scene_list = scene_manager.get_scene_list()
+
+		# Collect scene information for mapping to metadata
+		scenes = []
+		if len(scene_list) == 0:
+			# No scenes detected; record as one scene
+			# TODO: any reason to make this optional?
+			scenes[original_video.file.name].append({
+				'start_frame': video.base_timecode.get_frames(),
+				'start_time': video.base_timecode.get_timecode(),
+				#'start_fps': video.base_timecode.get_framerate(),
+				'end_frame': total_frames,
+				'end_time': total_duration,
+				#'end_fps': video.duration.get_framerate(),
+				'scene_num': 1,
+				'num_scenes_detected': 1,
+				'total_video_frames': total_frames,
+				'total_video_duration': total_duration,
+			})
+
+		for i, scene in enumerate(scene_list):
+			scenes.append({
+				'start_frame': scene[0].get_frames(),
+				'start_time': scene[0].get_timecode(),
+				#'start_fps': scene[0].get_framerate(),
+				'end_frame': scene[1].get_frames(),
+				'end_time': scene[1].get_timecode(),
+				#'end_fps': scene[1].get_framerate(),
+				'scene_num': i + 1,
+				'num_scenes_detected': len(scene_list),
+				'total_video_frames': total_frames,
+				'total_video_duration': total_duration,
+			})
+
+		return scenes
+
 	def get_new_scene_manager(self):
 		"""
-		Helper function to collect a new SceneManager object based on provided parameters. A new SceneManager is needed
-		for every video. If multiple videos are to be combined, one SceneManager could be used.
+		Helper function to collect a new SceneManager object
+
+		A new SceneManager is needed for every video. If multiple videos are
+		to be combined, one SceneManager could be used.
 		"""
 		scene_manager = SceneManager()
 		if self.parameters.get("detector_type") == 'content_detector':
@@ -348,4 +506,5 @@ class VideoSceneDetector(BasicProcessor):
 								  fade_bias=self.parameters.get("td_fade_bias"), ))
 		else:
 			raise ProcessorException('Scene detector type not available.')
+
 		return scene_manager
